972be995b05651ed07197cd37f161989dd4e1da3
Bonnie Jonkman <bonnie.jonkman@nrel.gov>
2012-02-18 06:01:08 +0000

    version 7.01.00a-bjj, as uploaded to web site 2/17/2012
    git-svn-id: https://windsvn2.nrel.gov/FAST/trunk@18 10acb478-4768-415a-8850-bacdb5912d4d
    
    Former-commit-id: c7ab849f06b41e91649183fa6ba310598c7ff32f

--- Archive.bat
+++ Archive.bat
@@ -1,55 +1,56 @@
 @ECHO OFF
 
-IF NOT "%1"==""  GOTO SetVars
+@SET ARCHPATH=Archive
+@SET PROGNAME=FAST
+@SET ARCHNAME=FAST_v%1
 
-ECHO 
-ECHO  The syntax for creating an archive is "Archive <version>"
-ECHO.
-ECHO  Example:  "archive 602 [all]"
-
-GOTO Done
-
-:SetVars
-SET ARCHPATH=Archive
-SET PROGNAME=FAST
-SET WINZIP="C:\Program Files\WinZip\WZZip"
-SET WINZIPSE="C:\Program Files\WinZip Self-Extractor\wzipse32.exe"
+@SET WINZIP="C:\Program Files\WinZip\WZZip"
+@SET WINZIPSE="C:\Program Files\WinZip Self-Extractor\wzipse32.exe"
 
 
-IF "%2"=="all"  GOTO ArchAll
+IF NOT "%1"==""  GOTO DeleteOld
 
-ECHO Archiving %PROGNAME% for distribution.
-SET ARCHNAME=FAST_v%1
-SET FILELIST=ArcFiles.txt
+@ECHO 
+@ECHO  The syntax for creating an archive is "Archive <version>"
+@ECHO.
+@ECHO  Example:  "archive 7.01.00"
 
-GOTO DeleteOld
-
-:ArchAll
-ECHO Archiving %PROGNAME% for internal use by including certification tests.
-SET ARCHNAME=FAST_v%1_all
-SET FILELIST=ArcAll.txt
-
+@GOTO Done
 
 :DeleteOld
-IF EXIST ARCHTMP.zip DEL ARCHTMP.zip
-IF EXIST %ARCHNAME%.exe DEL %ARCHNAME%.exe
+@IF EXIST ARCHTMP.zip DEL ARCHTMP.zip
+@IF EXIST %ARCHNAME%.exe DEL %ARCHNAME%.exe
 
 
 :DoIt
-%WINZIP% -a -o -P ARCHTMP @%FILELIST%
-ECHO Creating self-extracting archive...
-%WINZIPSE% ARCHTMP.zip -d. -y -win32 -le -overwrite -st"Unzipping %PROGNAME%" -m Disclaimer.txt
-REM IF you do not have access to NREL's NREL_Bug.ico file, please remove "-i Y:\Wind\Public\Projects\DesignCodes\NREL_Bug_32.ico" from the above command
-REM
-ECHO The self-extracting archive has been created.
-COPY ARCHTMP.exe %ARCHPATH%\%ARCHNAME%.exe
-DEL ARCHTMP.zip, ARCHTMP.exe
+@ECHO.
+@ECHO ------------------------------------------
+@ECHO Archiving %PROGNAME% for general distribution.
+@ECHO ------------------------------------------
+@ECHO.
+
+@%WINZIP% -a -o -P ARCHTMP @ArcFiles.txt
+@%WINZIPSE% ARCHTMP.zip -d. -y -win32 -le -overwrite -st"Unzipping %PROGNAME%" -m Disclaimer.txt
+
+@COPY ARCHTMP.exe %ARCHPATH%\%ARCHNAME%.exe
+@DEL ARCHTMP.zip, ARCHTMP.exe
+
+@ECHO.
+@ECHO ------------------------------------------
+@ECHO Archiving %PROGNAME% for internal use by including certification tests.
+@ECHO ------------------------------------------
+@ECHO.
+
+@%WINZIP% -a -o -P ARCHTMP @ArcAll.txt
+@%WINZIPSE% ARCHTMP.zip -d. -y -win32 -le -overwrite -st"Unzipping %PROGNAME%" -m Disclaimer.txt
+
+@COPY ARCHTMP.exe %ARCHPATH%\%ARCHNAME%_all.exe
+@DEL ARCHTMP.zip, ARCHTMP.exe
 
 
 :Done
-SET ARCHNAME=
-SET ARCHPATH=
-SET PROGNAME=
-SET FILELIST=
-SET WINZIP=
-SET WINZIPSE=
+@SET ARCHNAME=
+@SET ARCHPATH=
+@SET PROGNAME=
+@SET WINZIP=
+@SET WINZIPSE=
