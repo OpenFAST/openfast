c583ed4802f506d15758d4c0298d710311d9fdfe
Qi Wang <qi.wang2@nrel.gov>
2013-03-06 11:03:53 -0700

    Update 6 Mar, 2013, correct static results

--- Nonlinear_Static/RHS_element.f90
+++ Nonlinear_Static/RHS_element.f90
@@ -10,10 +10,12 @@ subroutine RHS_element(rhs_elem, dof_node, dof_total, &
    integer nnode, m, temp_id, i, j
    double precision tempATN(4,1), tempDChi(3,1)
    double precision Am(3,4), Dm(3,3), Chim(3,1)
-   double precision rhs_temp(dof_node*(norder+1))
+   double precision rhs_temp(dof_node*(norder+1)), hhp_temp(norder+1,norder+1)
    
    
    rhs_elem = 0.0d0
+   
+   hhp_temp = TRANSPOSE(hhp)
    
    do nnode=1, norder+1
    
@@ -24,7 +26,6 @@ subroutine RHS_element(rhs_elem, dof_node, dof_total, &
       
       
       call Chimatrix(Chim, dof_total, dof_node, norder, hhp, uf, nelem, nnode, Jacobian)
-      
          
       tempDChi = MATMUL(Dm, Chim)
       tempATN  = MATMUL(TRANSPOSE(Am), tempDChi)
@@ -33,9 +34,9 @@ subroutine RHS_element(rhs_elem, dof_node, dof_total, &
 
       do m=1, norder+1
          temp_id = (m-1)*dof_node
-         rhs_temp(temp_id+1) = wj(nnode)*hhp(nnode,m)*tempATN(1,1)
-         rhs_temp(temp_id+2) = wj(nnode)*hhp(nnode,m)*tempATN(2,1)
-         rhs_temp(temp_id+3) = hhp(nnode,m)*tempATN(3,1)
+         rhs_temp(temp_id+1) = wj(nnode)*hhp_temp(nnode,m)*tempATN(1,1)
+         rhs_temp(temp_id+2) = wj(nnode)*hhp_temp(nnode,m)*tempATN(2,1)
+         rhs_temp(temp_id+3) = hhp_temp(nnode,m)*tempATN(3,1)
          if(nnode==m) rhs_temp(temp_id+3) = rhs_temp(temp_id+3) + tempATN(4,1)
          rhs_temp(temp_id+3) = wj(nnode)*rhs_temp(temp_id+3)   
       enddo
