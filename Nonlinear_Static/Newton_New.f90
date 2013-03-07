b1b7364329088ac8161abbb004eaaba44ee00ad9
Mike Sprague <michael.a.sprague@nrel.gov>
2013-03-07 14:58:57 -0700

    added vtk output capabilities (needed asa directory)
    Former-commit-id: 4a744a59cd339a32bf8bb488c55420bfbd7435c2

--- Nonlinear_Static/Newton_New.f90
+++ Nonlinear_Static/Newton_New.f90
@@ -1,5 +1,5 @@
 subroutine Newton_New(dof_node,dof_total,norder,node_total,elem_total,&
-                     &hhp,uf,dmat,wj,niter,Jacobian)
+                     &hhp,uf,dmat,wj,niter,Jacobian, xloc)
    
    integer dof_node,dof_total,norder,node_total,elem_total,niter
    double precision hhp(norder+1,norder+1),wj(norder+1),dmat(node_total,3)
@@ -11,18 +11,33 @@ subroutine Newton_New(dof_node,dof_total,norder,node_total,elem_total,&
    double precision ui_old(dof_total)!mas ????
    double precision rel_change(dof_total)
 
-   parameter(TOLF  = 1.0d-6)  ! Xiao & Zhong use 1d-5
+   double precision xloc(node_total)
+   double precision position(dof_total)
+
+   parameter(TOLF  = 1.0d-5)  ! Xiao & Zhong use 1d-5
    integer i,j,m
    double precision temp1,temp2,FmL,errf,errx
+
+   character*4 filename
+
+   external filename
    
    ui=0.0d0
-!   FmL=3.14d-5 
 
+   yloc = 0.
+
+   position = 0.
+
+!   FmL=3.14d-5 
+   do i = 1, node_total
+      position(3*(i-1)+1) = xloc(i)
+   enddo
    
+   call outputvtk(position, dof_total, 'output'//filename(0) )         
      
    do i=1, niter
    
-       write(*,*) "niter=",i
+       write(*,*) "starting Newton iteration ",i
               
        call AssembleRHS(RHS, dof_node, dof_total, uf, &
                   & norder, hhp, wj, node_total, dmat, &
@@ -74,16 +89,21 @@ subroutine Newton_New(dof_node,dof_total,norder,node_total,elem_total,&
        call Norm(dof_total, uf, temp1)
        call Norm(dof_total, rel_change, temp2) 
        
-       errx = temp2 - tolx*temp1
+       errx = temp2 !- tolx*temp1
 
 !        call Norm(dof_total, RHS, errx)
 
         write(*,*) "errx",errx
-        if(errx .le. TOLF) return
-        
+ 
+        if (i .eq. 10) stop
+   
+        if(errx .lt. TOLF*temp1) return
         
         uf = uf + ui
         
+        !position = position + uf
+ 
+        call outputvtk(position + uf, dof_total, 'output'//filename(i) )         
                
 !       write(*,*) "Norm"
 !       write(*,*) temp1
