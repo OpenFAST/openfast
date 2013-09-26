6dfefaa64e0ceea0ce09c255bf50864e73f7dd02
Marshall Buhl <marshall.buhl@nrel.gov>
2013-09-26 16:05:22 +0000

    UpdateAllSVN.bat is a script that will do an "svn update" of all the modules used in this project.  Users should set the paths for any computer they will use for this.  Windows stored the computer name in the %HOSTNAME% environment variable so that the paths for that computer are used in the script.
    git-svn-id: https://windsvn2.nrel.gov/FAST/branches/BJonkman@515 10acb478-4768-415a-8850-bacdb5912d4d
    
    Former-commit-id: a5b9fca26d343719db5bf9f21b694e75e3254fb4

---
+++ UpdateAllSVN.bat
@@ -0,0 +1,71 @@
+@ECHO OFF
+
+::=======================================================================================================
+IF "%COMPUTERNAME%"=="APLATT-21846S" GOTO APLATT-21846S
+IF "%COMPUTERNAME%"=="BJONKMAN-23080S" GOTO BJONKMAN-23080S
+IF "%COMPUTERNAME%"=="MBUHL-20665S" GOTO MBUHL-20665S
+
+:APLATT-21846S
+@SET AeroDyn=
+@SET ElastoDyn=
+@SET FAST=
+@SET HydroDyn=
+@SET InflowWind=
+@SET MAP=
+@SET NWTC_Library=
+@SET ServoDyn=
+@SET SubDyn=
+GOTO UpdateSVN
+
+:BJONKMAN-23080S
+@SET AeroDyn=
+@SET ElastoDyn=
+@SET FAST=
+@SET HydroDyn=
+@SET InflowWind=
+@SET MAP=
+@SET NWTC_Library=
+@SET ServoDyn=
+@SET SubDyn=
+GOTO UpdateSVN
+
+:MBUHL-20665S
+@SET AeroDyn="M:\CAEtools\Simulators\AeroDyn\branches\Framework"
+@SET ElastoDyn="M:\CAEtools\Simulators\FAST\branches\BJonkman"
+@SET FAST="M:\CAEtools\Simulators\FAST\branches\BJonkman"
+@SET HydroDyn="M:\CAEtools\Simulators\HydroDyn\branches\HydroDyn_Modularization"
+@SET InflowWind="M:\CAEtools\Simulators\InflowWind\branches\modularization"
+@SET MAP="M:\CAEtools\Simulators\MAP\trunk"
+@SET NWTC_Library="M:\CAEtools\Miscellaneous\NWTC_Library\trunk"
+@SET ServoDyn="M:\CAEtools\Simulators\FAST\branches\BJonkman"
+@SET SubDyn="M:\CAEtools\Simulators\SubDyn\branches\v0.4"
+GOTO UpdateSVN
+
+:: Update all the SVN repositories used for this project.
+
+:UpdateSVN
+@ECHO.
+@ECHO Using folder definitions for %HOSTNAME% to update all modules in this project.
+@ECHO.
+
+SVN update %AeroDyn%
+SVN update %ElastoDyn%
+SVN update %FAST%
+SVN update %HydroDyn%
+SVN update %InflowWind%
+SVN update %MAP%
+SVN update %NWTC_Library%
+SVN update %ServoDyn%
+SVN update %SubDyn%
+
+:: Delete temporary environment variables.
+
+@SET AeroDyn=
+@SET ElastoDyn=
+@SET FAST=
+@SET HydroDyn=
+@SET InflowWind=
+@SET MAP=
+@SET NWTC_Library=
+@SET ServoDyn=
+@SET SubDyn=
