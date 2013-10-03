3aeea373b9022e792a1e4506c2145857362a2941
Andrew Platt <andrew.d.platt@gmail.com>
2013-10-03 20:56:45 +0000

    Updated for release on the web as version 2.00.00b-adp (http://wind.nrel.gov/designcodes/simulators/inflowwind/alpha/).
    git-svn-id: https://windsvn2.nrel.gov/InflowWind/branches/modularization@98 96535b8f-fecd-4773-a067-ac893715d582

--- update_web.bat
+++ update_web.bat
@@ -1,25 +1,32 @@
 @ECHO OFF
 
-@IF NOT "%1"==""  GOTO Copy
+set LOC=y:\wind\WindWeb\designcodes\simulators\inflowwind\alpha
+
+@IF /I "%1"=="-NOFILES" GOTO UpdateWeb
+@IF NOT "%1"=="" GOTO CopyFiles
 
 @ECHO 
 @ECHO  The syntax for updating files on the InflowWind page is "update_web <version>"
 @ECHO.
 @ECHO  Example:  "update_web 1.03.01"
+@ECHO.
+@ECHO  The syntax for updating the web page without copying the other files is "update_web -nofiles"
+@ECHO.
 
 @GOTO Done
 
-:Copy
-
-set LOC=y:\wind\WindWeb\designcodes\miscellaneous\inflowwind
-
-copy CreatePage.pl                 %LOC%\CreatePage.pl
+:CopyFiles
 copy ChangeLog.txt                 %LOC%\ChangeLog.txt
 copy Archive\InflowWind_v%1.exe    %LOC%\InflowWind_v%1.exe
+copy Archive\InflowWind_v%1.tar.gz %LOC%\InflowWind_v%1.tar.gz
 copy InflowWind.pdf                %LOC%\InflowWind.pdf
 
+
+:UpdateWeb
+copy CreatePage.pl                 %LOC%\CreatePage.pl
 perl %LOC%\CreatePage.pl
 
+:Done
 set LOC=
 
-:Done
+
