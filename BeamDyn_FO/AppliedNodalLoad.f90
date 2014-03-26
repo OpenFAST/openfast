13a0d3611e16e32739613d5422650a7c9aff9e0d
Qi Wang <qi.wang2@nrel.gov>
2014-03-26 15:29:02 -0600

    26MAR14,3
    Former-commit-id: 0f652e6cbf475ea3aa273abdf9461ddd39ecb3c6

--- BeamDyn_FO/AppliedNodalLoad.f90
+++ BeamDyn_FO/AppliedNodalLoad.f90
@@ -1,21 +1,16 @@
-!*********************************************************************************************************************************
-! This is the subroutine where the external force is input.
-!**********************************************************************************************************************************
 
-  SUBROUTINE AppliedNodalLoad(F_ext,time,dof_total)
+   SUBROUTINE AppliedNodalLoad(F_ext,time,dof_total)
+   !***************************************************************************************
+   ! This is the subroutine where the external force is input.
+   !***************************************************************************************
 
-   REAL(ReKi),INTENT(INOUT)::F_ext(:) ! Applied nodal force
-   REAL(DbKi),INTENT(IN)::time ! Current time
-   INTEGER(IntKi),INTENT(IN)::dof_total ! Total degrees of freedom
+   REAL(ReKi),    INTENT(INOUT)::F_ext(:)    ! Applied nodal force
+   REAL(DbKi),    INTENT(IN)::   time        ! Current time
+   INTEGER(IntKi),INTENT(IN)::   dof_total   ! Total degrees of freedom
 
-!   INTEGER::i
-!   WRITE(*,*) "time = ", time
    
    F_ext = 0.0D0
 
 !   F_ext(dof_total-3) = 1.0D+02 * SIN(10.0D+00 * time) ! Input external force here
-
-!   WRITE(*,*) F_ext(dof_total-3)
-!   STOP
 
    END SUBROUTINE AppliedNodalLoad
