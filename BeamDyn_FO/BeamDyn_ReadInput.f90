29e08e9752b149b58410cfadd81f28f514509bff
Qi Wang <qi.wang2@nrel.gov>
2014-04-30 11:07:38 -0600

    30APR14

---
+++ BeamDyn_FO/BeamDyn_ReadInput.f90
@@ -0,0 +1,35 @@
+   SUBROUTINE BeamDyn_ReadInput(InputFileName,&
+!             &BladeFileName,
+             &InputFileData,ErrStat,ErrMsg)
+
+   ! Passed Variables:
+   CHARACTER(*),                 INTENT(IN   )  :: InputFileName    ! Name of the input file
+!   CHARACTER(*),                 INTENT(IN   )  :: BladeFileName    ! File that contains the blade properties information
+
+   TYPE(BD_InputFile),           INTENT(  OUT)  :: InputFileData    ! Data stored in the module's input file
+   INTEGER(IntKi),               INTENT(  OUT)  :: ErrStat          ! The error status code
+   CHARACTER(*),                 INTENT(  OUT)  :: ErrMsg           ! The error message, if an error occurred
+
+
+   ! Local variables:
+!   INTEGER(4)         :: allo_stat
+!   CHARACTER(1024)    :: FilePath
+   LOGICAL            :: Echo
+   INTEGER(IntKi)                               :: UnEcho
+   INTEGER(IntKi)                               :: ErrStat2
+   CHARACTER(LEN(ErrMsg))                       :: ErrMsg2
+!   CHARACTER(1024)                              :: BldFile(MaxBl)
+
+   INTEGER(IntKi)     :: i
+
+   ErrStat = ErrID_None
+   ErrMsg = ''
+
+   CALL ReadPrimaryFile(InputFileName,InputFileData,&
+!                       &BldFile,
+                       &UnEcho,ErrStat2,ErrMsg2)
+
+   
+   END SUBROUTINE BeamDyn_ReadInput
+
+
