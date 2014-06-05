786fcfb0428f0ff40ab863a1aa9ff636ad152b32
Qi Wang <qi.wang2@nrel.gov>
2014-06-05 16:10:12 -0600

    05JUN14,modified time inter-extrapolation
    Former-commit-id: 0a90bea096d26b3d6827ac56e4587dd6f2340f14

--- BeamDyn_FO/BeamDyn.f90
+++ BeamDyn_FO/BeamDyn.f90
@@ -278,7 +278,7 @@ INCLUDE 'BeamDyn_ApplyBoundaryCondition.f90'
    WRITE(*,*) "Mass0_GL: ", p%Mass0_GL(4,:,2)
 !   WRITE(*,*) "Stiff0_GL: ", p%Stif0_GL(4,:,3)
 !   WRITE(*,*) "Stiff0_GL: ", p%Stif0_GL(4,:,4)
-   STOP
+!   STOP
    ! Define parameters here:
 
    p%node_total  = p%elem_total*(p%node_elem-1) + 1         ! total number of node  
