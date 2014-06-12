682b316bae82da96e6d3b762e40acd451345b04b
Bonnie Jonkman <bonnie.jonkman@nrel.gov>
2014-06-12 18:57:46 +0000

    updated SubDyn input files based on some minor modifications to SubDyn source. Added template for SubDyn input files. NOTE that Tests19 and 21 will not run with the executables (I'm going to update HydroDyn source/input files before updating these executables)
    git-svn-id: https://windsvn2.nrel.gov/FAST/branches/BJonkman@712 10acb478-4768-415a-8850-bacdb5912d4d
    
    Former-commit-id: 6d6f14ab87209d75f995091e6b206ee0dce5e3d7

--- CopyFilesForRelease.bat
+++ CopyFilesForRelease.bat
@@ -21,6 +21,7 @@ COPY "%CRUNCH%"                      "%bin_dir%"
 :dependencies
 
 :NWTC_Library
+ECHO NWTC_Library
 SET src_folder=%NWTC_Lib_Loc%\..
 SET dst_folder=%depend_dir%\NWTC_Library
 SET list_of_files=%src_folder%\SourceFiles.txt
@@ -29,6 +30,7 @@ if exist "%dst_folder%\*" DEL "%dst_folder%\*"
 for /f %%i in (%list_of_files%) DO copy /Y "%src_folder%\%%i" "%dst_folder%"
 
 :NetLib
+ECHO NetLib
 SET src_folder=%NETLIB_Loc%\..
 SET dst_folder=%depend_dir%\NetLib
 SET list_of_files=%src_folder%\SourceFiles.txt
@@ -38,6 +40,7 @@ for /f %%i in (%list_of_files%) DO copy /Y "%src_folder%\%%i" "%dst_folder%"
 
 
 :ElastoDyn
+ECHO ElastoDyn
 SET src_folder=%ED_Loc%\..
 SET dst_folder=%depend_dir%\ElastoDyn
 SET list_of_files=%src_folder%\FAST_SourceFiles_ED.txt
@@ -46,6 +49,7 @@ if exist "%dst_folder%\*" DEL "%dst_folder%\*"
 for /f %%i in (%list_of_files%) DO copy /Y "%src_folder%\%%i" "%dst_folder%"
 
 :ServoDyn
+ECHO ServoDyn
 SET src_folder=%SrvD_Loc%\..
 SET dst_folder=%depend_dir%\ServoDyn
 SET list_of_files=%src_folder%/FAST_SourceFiles_SrvD.txt
@@ -54,6 +58,7 @@ if exist "%dst_folder%\*" DEL "%dst_folder%\*"
 for /f %%i in (%list_of_files%) DO copy /Y "%src_folder%\%%i" "%dst_folder%"
 
 :InflowWind
+ECHO InflowWind
 SET src_folder=%IfW_Loc%\..
 SET dst_folder=%depend_dir%\InflowWind
 SET list_of_files=%src_folder%/FAST_SourceFiles.txt
@@ -62,6 +67,7 @@ if exist "%dst_folder%\*" DEL "%dst_folder%\*"
 for /f %%i in (%list_of_files%) DO copy /Y "%src_folder%\%%i" "%dst_folder%"
 
 :AeroDyn
+ECHO AeroDyn
 SET src_folder=%AD_Loc%\..
 SET dst_folder=%depend_dir%\AeroDyn
 SET list_of_files=%src_folder%\FAST_SourceFiles.txt
@@ -70,6 +76,7 @@ if exist "%dst_folder%\*" DEL "%dst_folder%\*"
 for /f %%i in (%list_of_files%) DO copy  "%src_folder%\%%i" "%dst_folder%"
 
 :HydroDyn
+ECHO HydroDyn
 SET src_folder=%HD_Loc%\..
 SET dst_folder=%depend_dir%\HydroDyn
 SET list_of_files=%src_folder%\FAST_SourceFiles.txt
@@ -78,6 +85,7 @@ if exist "%dst_folder%\*" DEL "%dst_folder%\*"
 for /f %%i in (%list_of_files%) DO copy /Y "%src_folder%\%%i" "%dst_folder%"
 
 :SubDyn
+ECHO SubDyn
 SET src_folder=%SD_Loc%\..
 SET dst_folder=%depend_dir%\SubDyn
 SET list_of_files=%src_folder%\FAST_SourceFiles.txt
@@ -86,6 +94,7 @@ if exist "%dst_folder%\*" DEL "%dst_folder%\*"
 for /f %%i in (%list_of_files%) DO copy /Y "%src_folder%\%%i" "%dst_folder%"
 
 :IceFloe
+ECHO IceFloe
 SET src_folder=%IceF_Loc%\..
 SET dst_folder=%depend_dir%\IceFloe
 SET list_of_files=%src_folder%\FAST_SourceFiles.txt
@@ -94,12 +103,16 @@ if exist "%dst_folder%\*" DEL "%dst_folder%\*"
 for /f %%i in (%list_of_files%) DO copy /Y "%src_folder%\%%i" "%dst_folder%"
 
 :MAP
+ECHO MAP
 SET src_folder=%MAP_Loc%\..
 SET dst_folder=%depend_dir%\MAP
 SET list_of_files=%src_folder%\FAST_SourceFiles.txt
 
 if exist "%dst_folder%\*" DEL "%dst_folder%\*"
 for /f %%i in (%list_of_files%) DO copy /Y "%src_folder%\%%i" "%dst_folder%"
+
+:FEAMooring
+ECHO Skipping FEAMooring
 
 
 :ClearVars
