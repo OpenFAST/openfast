694b069124fa1d1c2d9b71896435fc86d8374b9f
Bonnie Jonkman <bonnie.jonkman@nrel.gov>
2014-07-01 20:52:07 +0000

    updated web site with "CreatePage.pl" and removed old CreateAlphaPage.pl. We are only going to update one web site from this trunk/branch.
    git-svn-id: https://windsvn2.nrel.gov/AeroDyn/trunk@112 9add3c13-68b6-420a-9438-b3321c957034

--- update_web.bat
+++ update_web.bat
@@ -1,28 +1,35 @@
 @ECHO OFF
 
-@IF NOT "%1"==""  GOTO Copy
+set LOC=Y:\Wind\WindWeb\designcodes\simulators\aerodyn\alpha
+
+
+@IF /I "%1"=="-NOFILES" GOTO UpdateWeb
+@IF NOT "%1"=="" GOTO CopyFiles
+
 
 @ECHO 
 @ECHO  The syntax for updating files on the AeroDyn page is "update_web <version>"
 @ECHO.
 @ECHO  Example:  "update_web 13.01.00"
+@ECHO.
+@ECHO  The syntax for updating the web page without copying the other files is "update_web -nofiles"
+@ECHO.
 
 @GOTO Done
 
-:Copy
 
-rem set LOC=\\netapp01\websites\wind\designcodes\simulators\aerodyn
-set LOC=Y:\Wind\WindWeb\designcodes\simulators\aerodyn
-
-copy CreatePage.pl             %LOC%\CreatePage.pl
+:CopyFiles
 copy ChangeLog.txt             %LOC%\ChangeLog.txt
 copy Archive\AD_v%1.exe        %LOC%\AD_v%1.exe
 copy Archive\AD_v%1.tar.gz     %LOC%\AD_v%1.tar.gz
 
 rem copy UserGuideAddendum_AeroDyn13Interface.pdf %LOC%\UserGuideAddendum_AeroDyn13Interface.pdf
 
+:UpdateWeb
+copy CreatePage.pl             %LOC%\CreatePage.pl
+
 perl %LOC%\CreatePage.pl
 
-set LOC=
 
 :Done
+set LOC=
