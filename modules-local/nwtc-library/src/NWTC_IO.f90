5c8503b8446198ef8621d178d36ce6a902b4f291
Bonnie Jonkman <bonnie.jonkman@nrel.gov>
2014-09-27 01:57:49 +0000

    + replaced web development files with scripts for new website + updated version numbers
    git-svn-id: https://windsvn2.nrel.gov/NWTC_Library/trunk@261 33a3f72e-afca-4cba-af91-dee07167dc2f

--- modules-local/nwtc-library/src/NWTC_IO.f90
+++ modules-local/nwtc-library/src/NWTC_IO.f90
@@ -35,7 +35,7 @@ MODULE NWTC_IO
 !=======================================================================
 
    TYPE(ProgDesc), PARAMETER    :: NWTC_Ver = &                               ! The name, version, and date of the NWTC Subroutine Library.
-                                    ProgDesc( 'NWTC Subroutine Library', 'v2.03.03b-bjj', '30-June-2014')
+                                    ProgDesc( 'NWTC Subroutine Library', 'v2.04.00a-bjj', '30-Sep-2014')
 
    TYPE, PUBLIC                 :: FNlist_Type                                ! This type stores a linked list of file names.
       CHARACTER(1024)                        :: FileName                      ! A file name.
