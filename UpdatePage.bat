58aa16e31ca00cfde3c493bebf22b4ce66fbaa09
Bonnie Jonkman <bonnie.jonkman@nrel.gov>
2014-09-27 01:57:49 +0000

    + replaced web development files with scripts for new website + updated version numbers
    git-svn-id: https://windsvn2.nrel.gov/NWTC_Library/trunk@261 33a3f72e-afca-4cba-af91-dee07167dc2f

--- UpdatePage.bat
+++
@@ -1,2 +0,0 @@
-@CALL UpdateWeb -nofiles
-
--- UpdateWeb.bat
+++ UpdateWeb.bat
@@ -1,29 +1,12 @@
 @ECHO OFF
 
-set LOC=Y:\Wind\WindWeb\designcodes\miscellaneous\NWTC_Library
+set LOC=Y:\Wind\WindWeb\nwtc\docs
 
-@IF /I "%1"=="-NOFILES" GOTO UpdateWeb
-@IF NOT "%1"=="" GOTO CopyFiles
-
-@ECHO 
-@ECHO  The syntax for updating files on the NWTC Subroutine Library page is "updateweb <version>"
-@ECHO.
-@ECHO  Example:  "updateweb 1.01.08"
-@ECHO.
-@ECHO  The syntax for updating the web page without copying the other files is "updateweb -nofiles"
-@ECHO.
-
-@GOTO Done
 
 :CopyFiles
-copy ChangeLog.txt                            %LOC%\ChangeLog.txt
+copy ChangeLog.txt                            %LOC%\NWTC_Library_ChangeLog.txt
 copy Documentation\NWTC_Library_Routines.txt  %LOC%\NWTC_Library_Routines.txt
-copy Archive\NWTC_Lib_v%1.exe                 %LOC%\NWTC_Lib_v%1.exe
-copy Archive\NWTC_Lib_v%1.tar.gz              %LOC%\NWTC_Lib_v%1.tar.gz
 
-:UpdateWeb
-copy CreatePage.pl                            %LOC%\CreatePage.pl
-perl %LOC%\CreatePage.pl
 
 :Done
 set LOC=
