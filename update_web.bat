12e36fe47597c01bb4189da5292ba8dd97073877
Bonnie Jonkman <bonnie.jonkman@nrel.gov>
2014-10-03 17:29:35 +0000

    + updated documents for web site + fixed some spacing in source code
    git-svn-id: https://windsvn2.nrel.gov/AeroDyn/trunk@162 9add3c13-68b6-420a-9438-b3321c957034

--- update_web.bat
+++ update_web.bat
@@ -1,35 +1,9 @@
 @ECHO OFF
 
-set LOC=Y:\Wind\WindWeb\designcodes\simulators\aerodyn\alpha
-
-
-@IF /I "%1"=="-NOFILES" GOTO UpdateWeb
-@IF NOT "%1"=="" GOTO CopyFiles
-
-
-@ECHO 
-@ECHO  The syntax for updating files on the AeroDyn page is "update_web <version>"
-@ECHO.
-@ECHO  Example:  "update_web 13.01.00"
-@ECHO.
-@ECHO  The syntax for updating the web page without copying the other files is "update_web -nofiles"
-@ECHO.
-
-@GOTO Done
-
+set LOC=Y:\Wind\WindWeb\nwtc\docs
 
 :CopyFiles
-copy ChangeLog.txt             %LOC%\ChangeLog.txt
-copy Archive\AD_v%1.exe        %LOC%\AD_v%1.exe
-copy Archive\AD_v%1.tar.gz     %LOC%\AD_v%1.tar.gz
-
-rem copy UserGuideAddendum_AeroDyn13Interface.pdf %LOC%\UserGuideAddendum_AeroDyn13Interface.pdf
-
-:UpdateWeb
-copy CreatePage.pl             %LOC%\CreatePage.pl
-
-perl %LOC%\CreatePage.pl
-
+copy ChangeLog.txt             %LOC%\AeroDyn-ChangeLog.txt
 
 :Done
 set LOC=
