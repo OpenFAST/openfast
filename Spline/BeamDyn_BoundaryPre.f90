039a342db625965ed57570fd11c71296710d5a79
Qi Wang <qi.wang2@nrel.gov>
2014-11-13 17:16:59 -0700

    13NOV14
    Former-commit-id: a4e049bedb04b76adea4900d249e5cacc68c3081

--- Spline/BeamDyn_BoundaryPre.f90
+++ Spline/BeamDyn_BoundaryPre.f90
@@ -18,7 +18,7 @@
 
    pi = ACOS(-1.0D0)
    temp_pp(:) = 0.0D0
-   temp_pp(2) = 4.0D0*TAN((-pi*t*1.0D0/3.0D0+(2.0D0*pi*counter))/4.0D0)
+!   temp_pp(2) = 4.0D0*TAN((-pi*t*1.0D0/3.0D0+(2.0D0*pi*counter))/4.0D0)
    temp_qq(:) = 0.0D0
    x%q(4:6) = temp_pp(:)
 
