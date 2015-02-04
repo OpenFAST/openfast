3527e4e7a65ae021a5e2bd7e91fa1e7fa75f7c72
Bonnie Jonkman <bonnie.jonkman@nrel.gov>
2015-02-04 19:37:31 +0000

    + updated switches in ServoDyn (user routines from subroutines are option 3 for ALL control types) + added two lines in ServoDyn input file for future use with TMD model + added scripts to convert input files, and ran them to update CertTest files + added Lidar files to FAST_Gateway.
    git-svn-id: https://windsvn2.nrel.gov/FAST/branches/BJonkman@902 10acb478-4768-415a-8850-bacdb5912d4d
    
    Former-commit-id: e35395a54a211e301f6d74856f61a428c55a5c0f

--- glue-codes/simulink/src/create_FAST_SFunc.m
+++ glue-codes/simulink/src/create_FAST_SFunc.m
@@ -1,7 +1,14 @@
-mex -L../../bin -lFAST_Library_x64.lib -compatibleArrayDims -outdir ../../bin FAST_SFunc.c 
-%mex -L../../bin -lFAST_Library_x64.lib -largeArrayDims -outdir ../../bin FAST_SFunc.c 
+mex -L../../bin -lFAST_Library_Win32 -outdir ../../bin FAST_SFunc.c 
+%mex -L../../bin -lFAST_Library_x64 -outdir ../../bin FAST_SFunc.c 
 
 %%
- addpath('C:\Users\bjonkman\Documents\CAETools\FASTv8\bin');
- addpath('C:\Users\bjonkman\Documents\CAETools\FASTv8\Simulink\Samples');
- 
+ %addpath('C:\Users\bjonkman\Documents\CAETools\FASTv8\bin');
+ %addpath('C:\Users\bjonkman\Documents\CAETools\FASTv8\Simulink\Samples');
+  
+% *************************************************************************** 
+%   Warning: MEX-files generated using Microsoft Visual C++ 2010 require 
+%            that Microsoft Visual Studio 2010 run-time libraries be  
+%            available on the computer they are run on. 
+%            If you plan to redistribute your MEX-files to other MATLAB 
+%            users, be sure that they have the run-time libraries. 
+% ***************************************************************************  
