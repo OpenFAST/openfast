629c9bed79ccd6116db341da9896ffa8f8d142d6
Qi Wang <qi.wang2@nrel.gov>
2015-02-19 18:10:05 -0700

    Finished GA2, working on global-local blade coordinates transformation
    Former-commit-id: 035535fc8e26751420bac8ae9d72928a5b17af62

--- AM2/BeamDyn_BoundaryGA2.f90
+++ AM2/BeamDyn_BoundaryGA2.f90
@@ -10,7 +10,8 @@
    REAL(ReKi)                                   :: pi
 
    pi = ACOS(-1.0D0)
-!   x%q(1:3) = u%RootMotion%TranslationDisp(1:3,1)
+   x%q(1:3) = u%RootMotion%TranslationDisp(1:3,1)
+   x%q(6) = 4.0D0*TAN((3.1415926D0*t*1.0D0/3.0D0+(2.0D0*pi*OtherState%Rescale_Counter))/4.0D0)
 !   x%q(6) = 4.0D0*TAN((3.1415926D0*t*1.0D0/3.0D0)/4.0D0)
 !   IF(ABS(x%q(6)) .GT. 4.0D0) THEN
 !       x%q(6) = 4.0D0*TAN((3.1415926D0*t*1.0D0/3.0D0+(2.0D0*pi))/4.0D0)
@@ -18,18 +19,18 @@
 !------------------
 !  Rotating beam
 !------------------
-!   x%dqdt(1:3) = u%RootMotion%TranslationVel(1:3,1)
-!   x%dqdt(4:6) = u%Rootmotion%RotationVel(1:3,1)
-!   OtherState%acc(1:3) = u%RootMotion%TranslationAcc(1:3,1)
-!   OtherState%acc(4:6) = u%RootMotion%RotationAcc(1:3,1)
+   x%dqdt(1:3) = u%RootMotion%TranslationVel(1:3,1)
+   x%dqdt(4:6) = u%Rootmotion%RotationVel(1:3,1)
+   OtherState%acc(1:3) = u%RootMotion%TranslationAcc(1:3,1)
+   OtherState%acc(4:6) = u%RootMotion%RotationAcc(1:3,1)
 !------------------
 ! End rotating beam
 !------------------
 
 !-------DEBUG---------
-x%q(1:6) = 0.0D0
-x%dqdt(1:6) = 0.0D0
-OtherState%acc(1:6) = 0.0D0
+!x%q(1:6) = 0.0D0
+!x%dqdt(1:6) = 0.0D0
+!OtherState%acc(1:6) = 0.0D0
 !x%q(5) = -4.0D0*TAN((3.1415926D0*t/3.0D0)/4.0D0)
 !IF(ABS(x%q(5)) .GT. 4.0D0) THEN
 !    x%q(5) = -4.0D0*TAN((3.1415926D0*t/3.0D0+2.0D0*3.1415926D0)/4.0D0)
