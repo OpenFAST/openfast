0437ce766075cf792defd1f4bb44258dfaa1bfde
Bonnie Jonkman <bonnie.jonkman@nrel.gov>
2015-03-02 16:27:15 +0000

    added comment about possible fix for profiling
    git-svn-id: https://wind-dev2.nrel.gov/svn/SubDyn/trunk@342 3fcc0dcb-640f-2d4c-99c7-f8943d75394e

--- modules-local/subdyn/src/SubDyn.f90
+++ modules-local/subdyn/src/SubDyn.f90
@@ -744,7 +744,7 @@ SUBROUTINE SD_CalcOutput( t, u, p, x, xd, z, OtherState, y, ErrStat, ErrMsg )
                 
              !STATIC IMPROVEMENT METHOD  ( modify UL )
       IF (p%SttcSolve) THEN
-         FLt  = MATMUL(p%PhiL_T,                  OtherState%UFL + p%FGL)  ! -> bjj: todo: this line takes up A LOT of time. is PhiL sparse???? no
+         FLt  = MATMUL(p%PhiL_T,                  OtherState%UFL + p%FGL)  ! -> bjj: todo: this line takes up A LOT of time. is PhiL sparse???? no (solution: don't call this routine thousands of time to calculate the jacobian)
          ULS  = MATMUL(p%PhiLInvOmgL2,            FLt                   )  ! -> bjj: todo: this line takes up A LOT of time. is PhiL sparse????
          OtherState%UL  = OtherState%UL + ULS 
           
