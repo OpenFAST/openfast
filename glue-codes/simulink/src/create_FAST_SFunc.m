23263c5f88a87784c5b4d294ee42b5ace66ceac0
Bonnie Jonkman <bonnie.jonkman@nrel.gov>
2015-03-23 15:52:23 +0000

    added option to compile static libraries instead of dynamic libraries in the MATLAB mex files (so we don't need to redistribute Microsoft Visual Studio libraries)
    git-svn-id: https://windsvn2.nrel.gov/FAST/branches/BJonkman@949 10acb478-4768-415a-8850-bacdb5912d4d
    
    Former-commit-id: d4180f287e5d3d7c17a489c442b92e77ea8df7f1

--- glue-codes/simulink/src/create_FAST_SFunc.m
+++ glue-codes/simulink/src/create_FAST_SFunc.m
@@ -1,5 +1,5 @@
-mex -L../../bin -lFAST_Library_Win32 -outdir ../../bin LINKFLAGS='$LINKFLAGS /STACK:999999999 /LARGEADDRESSAWARE' FAST_SFunc.c 
-% mex -v -L../../bin -lFAST_Library_x64 -outdir ../../bin FAST_SFunc.c 
+% mex -L../../bin -lFAST_Library_Win32 -outdir ../../bin COMPFLAGS='$COMPFLAGS /MT' LINKFLAGS='$LINKFLAGS /STACK:999999999 /LARGEADDRESSAWARE' FAST_SFunc.c 
+mex -v -L../../bin -lFAST_Library_x64 -outdir ../../bin COMPFLAGS='$COMPFLAGS /MT' FAST_SFunc.c 
 
 %%
  %addpath('C:\Users\bjonkman\Documents\CAETools\FASTv8\bin');
