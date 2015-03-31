9c14f9177a5184f5d48b87dfa972e5f5b30ecb19
Bonnie Jonkman <bonnie.jonkman@nrel.gov>
2015-03-31 20:44:23 +0000

    updated FAST mex functions using MATLAB 2014b
    git-svn-id: https://windsvn2.nrel.gov/FAST/branches/BJonkman@956 10acb478-4768-415a-8850-bacdb5912d4d
    
    Former-commit-id: aa928f217904c1ff05130f5f35b96eae4474eeaa

--- glue-codes/simulink/src/create_FAST_SFunc.m
+++ glue-codes/simulink/src/create_FAST_SFunc.m
@@ -1,14 +1,6 @@
-% mex -L../../bin -lFAST_Library_Win32 -outdir ../../bin COMPFLAGS='$COMPFLAGS /MT' LINKFLAGS='$LINKFLAGS /STACK:999999999 /LARGEADDRESSAWARE' FAST_SFunc.c 
-mex -v -L../../bin -lFAST_Library_x64 -outdir ../../bin COMPFLAGS='$COMPFLAGS /MT' FAST_SFunc.c 
+mex -L../../bin -lFAST_Library_Win32 -outdir ../../bin COMPFLAGS='$COMPFLAGS /MT' LINKFLAGS='$LINKFLAGS /STACK:999999999 /LARGEADDRESSAWARE' FAST_SFunc.c 
+% mex -v -L../../bin -lFAST_Library_x64 -outdir ../../bin COMPFLAGS='$COMPFLAGS /MT' FAST_SFunc.c 
 
 %%
  %addpath('C:\Users\bjonkman\Documents\CAETools\FASTv8\bin');
  %addpath('C:\Users\bjonkman\Documents\CAETools\FASTv8\Simulink\Samples');
-  
-% *************************************************************************** 
-%   Warning: MEX-files generated using Microsoft Visual C++ 2010 require 
-%            that Microsoft Visual Studio 2010 run-time libraries be  
-%            available on the computer they are run on. 
-%            If you plan to redistribute your MEX-files to other MATLAB 
-%            users, be sure that they have the run-time libraries. 
-% ***************************************************************************  
