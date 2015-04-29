deb698330ce6459b79683dbd05b7b91e40fcd156
Qi Wang <qi.wang2@nrel.gov>
2015-04-29 17:15:40 -0600

    29APR15, combined all files into BeamDyn.f90

--- BeamDyn/BD_Norm.f90
+++
@@ -1,8 +0,0 @@
-   FUNCTION BD_Norm(vector)
-   
-   REAL(ReKi),INTENT(IN):: vector(:)
-   REAL(ReKi)           :: BD_Norm
-   
-   BD_Norm = SQRT(DOT_PRODUCT(vector,vector))
-   
-   END FUNCTION BD_Norm
