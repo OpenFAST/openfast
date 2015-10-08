34bb46fb09eb7166d2391dee17b4848b05a94627
Bonnie Jonkman <bonnie.jonkman@nrel.gov>
2015-10-08 04:53:37 +0000

    + updated FAST_Library and SFunc (both 32-bit and 64-bit versions) + updated CertTest/TstFiles + updated ChangeLog and other documentation
    git-svn-id: https://windsvn2.nrel.gov/FAST/branches/BJonkman@1154 10acb478-4768-415a-8850-bacdb5912d4d
    
    Former-commit-id: d222d6d734895e0b2da830f16449ddb82436c394

--- glue-codes/simulink/src/create_FAST_SFunc.m
+++ glue-codes/simulink/src/create_FAST_SFunc.m
@@ -1,6 +1,11 @@
-% mex -L../../bin -lFAST_Library_Win32 -I../../Source -I../../Source/dependencies/OpenFOAM -outdir ../../bin COMPFLAGS='$COMPFLAGS /MT' LINKFLAGS='$LINKFLAGS /STACK:999999999 /LARGEADDRESSAWARE' FAST_SFunc.c 
+%% 32-bit Matlab:
+% mex -L../../bin -lFAST_Library_Win32 ...
+%     LINKFLAGS='$LINKFLAGS /STACK:999999999 /LARGEADDRESSAWARE' ...
+%     -I../../Source -I../../Source/dependencies/OpenFOAM -outdir ../../bin COMPFLAGS='$COMPFLAGS /MT' FAST_SFunc.c
+
+%% 64-bit Matlab:
 mex -v -L../../bin -lFAST_Library_x64 ...
-    -I../../Source -I../../Source/dependencies/OpenFOAM -outdir ../../bin COMPFLAGS='$COMPFLAGS /MT' FAST_SFunc.c 
+    -I../../Source -I../../Source/dependencies/OpenFOAM -outdir ../../bin COMPFLAGS='$COMPFLAGS /MT' FAST_SFunc.c
 
 %%
  %addpath('C:\Users\bjonkman\Documents\CAETools\FASTv8\bin');
