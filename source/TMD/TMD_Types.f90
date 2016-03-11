259602f41368d4ea383f3431862efaaadd02d339
Bonnie Jonkman <bonnie.jonkman@nrel.gov>
2016-03-11 13:32:41 -0700

    updated types files so I wouldn't have to keep seeing that they were generated with an older version of the registry

--- source/TMD/TMD_Types.f90
+++ source/TMD/TMD_Types.f90
@@ -1,9 +1,9 @@
 !STARTOFREGISTRYGENERATEDFILE 'TMD_Types.f90'
 !
-! WARNING This file is generated automatically by the FAST registry
+! WARNING This file is generated automatically by the FAST registry.
 ! Do not edit.  Your changes to this file will be lost.
 !
-! FAST Registry (v3.00.00, 13-Nov-2015)
+! FAST Registry (v3.01.00, 11-Jan-2016)
 !*********************************************************************************************************************************
 ! TMD_Types
 !.................................................................................................................................
