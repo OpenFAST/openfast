5534ef089ce880f9c51afa16692717a7e992a092
Bonnie Jonkman <bonnie.jonkman@nrel.gov>
2016-04-08 17:25:44 +0000

    removed file (that no longer exists) from UpdateWeb.bat
    git-svn-id: https://windsvn2.nrel.gov/NWTC_Library/trunk@370 33a3f72e-afca-4cba-af91-dee07167dc2f

--- UpdateWeb.bat
+++ UpdateWeb.bat
@@ -5,7 +5,6 @@ set LOC=Y:\Wind\WindWeb\nwtc\docs
 
 :CopyFiles
 copy ChangeLog.txt                            %LOC%\NWTC_Library_ChangeLog.txt
-copy Documentation\NWTC_Library_Routines.txt  %LOC%\NWTC_Library_Routines.txt
 
 
 :Done
