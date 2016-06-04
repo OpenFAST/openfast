8c6716872807a68c5a50bab439572a741936eef9
Bonnie Jonkman <bonnie.jonkman@nrel.gov>
2016-06-03 23:00:32 -0600

    updated IceFloe_Types_notFAST.f90 with apparently updated code from the zip file Tim sent me
    Former-commit-id: 66b4fe110a23b964b73a9a057d5e5b6651edcc38

--- modules-ext/icefloe/src/icefloe/IceFloe_Types_notFAST.f90
+++ modules-ext/icefloe/src/icefloe/IceFloe_Types_notFAST.f90
@@ -1,6 +1,6 @@
 !*********************************************************************************************************************************
 MODULE IceFloe_Types
-! this is a simplified version for use with IceFloe not in FAST
+! this is a simplified version for use with IceFloe with codes other than FAST
 !---------------------------------------------------------------------------------------------------------------------------------
 use precision
 IMPLICIT NONE
@@ -11,11 +11,12 @@ IMPLICIT NONE
     REAL(ReKi)  :: iceDirection      ! ice floe direction [degrees]
     REAL(ReKi)  :: minStrength      ! minimum dynamic ice strength [Pa]
     REAL(ReKi)  :: minStrengthNegVel      ! minimum dynamic ice strength for negative velocity [Pa]
-    REAL(ReKi)  :: defaultArea      ! structure width to use in cpld crushin [m]
+    REAL(ReKi)  :: defaultArea       ! width to use for coupled crushing
     REAL(ReKi)  :: crushArea      ! cross sectional area of ice against tower [m^2]
     REAL(ReKi)  :: coeffStressRate      ! coefficient to calc stress rate from relative vellocity [Pa/m]
     REAL(ReKi)  :: C(4)      ! coefficient of cubic transition curve for negative stress rates [-]
-    REAL(ReKi)  :: dt      ! time step [sec]
+    REAL(ReKi)  :: dt        ! time step [sec]
+    REAL(ReKi)  :: rampTime  ! ramp time for forces at beginning of simulation [sec]
     REAL(ReKi) , DIMENSION(:), ALLOCATABLE  :: legX      ! - [x position of each leg relative to structure center]
     REAL(ReKi) , DIMENSION(:), ALLOCATABLE  :: legY      ! - [y position of each leg relative to structure center]
     REAL(ReKi) , DIMENSION(:), ALLOCATABLE  :: ks      ! - [shelter factor due to upstream leg]
@@ -26,4 +27,10 @@ IMPLICIT NONE
     LOGICAL  :: initFlag      ! Flag for successful initialization [-]
   END TYPE IceFloe_ParameterType
 ! =======================
+
+! Some constants that would otherwise come from NREL code base
+   real(ReKi), parameter :: Pi  = 3.141592654_ReKi
+   real(ReKi), parameter :: D2R = Pi/180.0_ReKi
+   real(ReKi), parameter :: R2D = 180.0_ReKi/Pi
+
 END MODULE IceFloe_Types
