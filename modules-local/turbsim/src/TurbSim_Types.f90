d3f140103db74b8e6b73ba164ab863350dbe0514
Bonnie Jonkman <bonnie.jonkman@nrel.gov>
2016-06-07 18:35:18 +0000

    v2.00.006-bjj 6-Jun-2016 on the web.
    git-svn-id: https://windsvn2.nrel.gov/TurbSim/branches/Modularization@133 c49df35f-aca6-4ea5-bb27-7fcdbeda79b9

--- modules-local/turbsim/src/TurbSim_Types.f90
+++ modules-local/turbsim/src/TurbSim_Types.f90
@@ -3,7 +3,7 @@ MODULE TurbSim_Types
 
 use NWTC_Library
 
-   TYPE(ProgDesc), PARAMETER :: TurbSim_Ver = ProgDesc( 'TurbSim', 'v2.00.05c-bjj', '25-Feb-2016' )
+   TYPE(ProgDesc), PARAMETER :: TurbSim_Ver = ProgDesc( 'TurbSim', 'v2.00.06a-bjj', '6-Jun-2016' )
 
    LOGICAL,        PARAMETER :: MVK         = .FALSE.                       ! This parameter has been added to replace the NON-STANDARD compiler directive previously used
    LOGICAL,        PARAMETER :: PeriodicY   = .FALSE. !.TRUE.
