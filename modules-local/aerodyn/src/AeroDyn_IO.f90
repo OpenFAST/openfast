56407a50dbdde9e66aeb517e4d7dcf535bc2eb47
Bonnie Jonkman <bonnie.jonkman@nrel.gov>
2016-07-27 03:17:47 -0600

    added change log, re-ran certTest, removed unused dependencies and updated NWTC_Library (which should be replaced with submodule soon)

--- modules-local/aerodyn/src/AeroDyn_IO.f90
+++ modules-local/aerodyn/src/AeroDyn_IO.f90
@@ -30,7 +30,7 @@ MODULE AeroDyn_IO
    
    implicit none
    
-   type(ProgDesc), parameter  :: AD_Ver = ProgDesc( 'AeroDyn', 'v15.03.00', '23-Jun-2016' )
+   type(ProgDesc), parameter  :: AD_Ver = ProgDesc( 'AeroDyn', 'v15.03.00', '27-Jul-2016' )
    character(*),   parameter  :: AD_Nickname = 'AD'
       
 ! ===================================================================================================
