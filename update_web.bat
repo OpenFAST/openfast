cb833d594ab58c2196f9f11149ceae3f82a6ce97
Greg Hayman <gregory.hayman@nrel.gov>
2016-12-16 05:48:58 -0700

    removed files and directories we do not want in the OpenFAST repo

--- update_web.bat
+++
@@ -1,8 +0,0 @@
-
-set LOC=Y:\Wind\WindWeb\nwtc\docs
-
-rem copy Documentation\SubDyn_Manual.pdf                                 %LOC%\SubDyn_Manual.pdf
-copy ChangeLog.txt                                                   %LOC%\SubDyn_ChangeLog.txt
-
-set LOC=
-
