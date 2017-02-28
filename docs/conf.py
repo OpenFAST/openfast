204282f7aa1c7ae02d1cd52a8aef5e324ad15e18
Rood <jon.rood@nrel.gov>
2017-02-28 10:37:41 -0700

    Adding OpenFAST logo to documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -148,7 +148,7 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 # html_theme = 'bizstyle'
-#html_logo = 'naluLowMach.jpg'
+html_logo = 'openfastlogo.jpg'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
