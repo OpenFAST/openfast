ed20dadb8b39b484fc90ee402341efa4c782f31b
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-05-01 15:14:02 -0600

    cleanup the build/reg_tests directory structure

--- reg_tests/CTestList.cmake
+++ reg_tests/CTestList.cmake
@@ -3,19 +3,19 @@
 #===============================================================================
 
 # Standard regression test
-function(add_test_r testname)
+function(add_test_r testname )
     add_test(
       ${testname} bash -c "
-      echo ${CMAKE_CURRENT_SOURCE_DIR} &&
-      echo ${CMAKE_BINARY_DIR} &&
-      mkdir -p ${testname}-local &&
-      cp -r ${CMAKE_CURRENT_SOURCE_DIR}/AWT27 ./${testname}-local/AWT27 &&
-      cp ${CMAKE_CURRENT_SOURCE_DIR}/test_files/${testname}/*.fst ${testname}-local &&
-      ${CMAKE_BINARY_DIR}/../install/bin/openfast ${testname}-local/${testname}.fst &&
-      ${CMAKE_CURRENT_SOURCE_DIR}/pass_fail.sh ${testname} ${CMAKE_BINARY_DIR}/reg_tests/${testname}-local/${testname}.outb ${TOLERANCE}"
+        #cp ${CMAKE_CURRENT_SOURCE_DIR}/test_files/${testname}/*.fst ${testname} &&
+        #${CMAKE_INSTALL_PREFIX}/bin/openfast ${testname}/${testname}.fst &&
+        #${CMAKE_CURRENT_SOURCE_DIR}/pass_fail.sh ${testname} ${CMAKE_BINARY_DIR}/reg_tests/${testname}/${testname}.outb ${TOLERANCE}
+
+        cp ${CMAKE_CURRENT_SOURCE_DIR}/test_files/${testname}/*.fst . &&
+        ${CMAKE_INSTALL_PREFIX}/bin/openfast ${testname}.fst &&
+        ${CMAKE_CURRENT_SOURCE_DIR}/pass_fail.sh ${testname} ${CMAKE_BINARY_DIR}/reg_tests/${testname}.outb ${TOLERANCE}
+      "
     )
-    #set_tests_properties(${testname} PROPERTIES TIMEOUT 1000 PROCESSORS ${np} WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/test_files/${testname}" LABELS "regression")
-    #file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/test_files/${testname})
+    set_tests_properties(${testname} PROPERTIES TIMEOUT 1000 WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}" LABELS "regression")
 endfunction(add_test_r)
 
 #===============================================================================
@@ -23,3 +23,4 @@ endfunction(add_test_r)
 #===============================================================================
 
 add_test_r(Test01)
+add_test_r(Test02)
