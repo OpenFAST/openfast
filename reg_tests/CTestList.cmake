9aaed62dac50420b2e3ee3713a01f7732abffb20
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-05-01 17:02:35 -0600

    include more required files for CertTest cases

--- reg_tests/CTestList.cmake
+++ reg_tests/CTestList.cmake
@@ -6,12 +6,8 @@
 function(add_test_r testname )
     add_test(
       ${testname} bash -c "
-        #cp ${CMAKE_CURRENT_SOURCE_DIR}/test_files/${testname}/*.fst ${testname} &&
-        #${CMAKE_INSTALL_PREFIX}/bin/openfast ${testname}/${testname}.fst &&
-        #${CMAKE_CURRENT_SOURCE_DIR}/pass_fail.sh ${testname} ${CMAKE_BINARY_DIR}/reg_tests/${testname}/${testname}.outb ${TOLERANCE}
-
         cp ${CMAKE_CURRENT_SOURCE_DIR}/test_files/${testname}/*.fst . &&
-        ${CMAKE_INSTALL_PREFIX}/bin/openfast ${testname}.fst &&
+        ${CMAKE_INSTALL_PREFIX}/bin/openfast ${testname}.fst > ${testname}.log &&
         ${CMAKE_CURRENT_SOURCE_DIR}/pass_fail.sh ${testname} ${CMAKE_BINARY_DIR}/reg_tests/${testname}.outb ${TOLERANCE}
       "
     )
@@ -22,5 +18,19 @@ endfunction(add_test_r)
 # Regression tests
 #===============================================================================
 
-add_test_r(Test01)
-add_test_r(Test02)
+#add_test_r(Test01)
+#add_test_r(Test02)
+#add_test_r(Test03)
+#add_test_r(Test04)
+add_test_r(Test05)
+add_test_r(Test06)
+add_test_r(Test07)
+add_test_r(Test08)
+add_test_r(Test09)
+add_test_r(Test10)
+add_test_r(Test11)
+add_test_r(Test12)
+add_test_r(Test13)
+add_test_r(Test14)
+add_test_r(Test15)
+add_test_r(Test16)
