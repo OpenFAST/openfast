b6109aaaa0b7cbc946d40d7698036e77ba42189a
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-05-08 08:48:47 -0600

    add support for multiple solution sets, and automatically choose the appropriate one based on machine type and compiler

--- reg_tests/executeRegressionTestCase.py
+++ reg_tests/executeRegressionTestCase.py
@@ -1,13 +1,15 @@
 """
     This script executes openfast and a regression test for a single case.
 
-    Usage: python executeRegressionTestCase.py CMAKE_BINARY_DIR CMAKE_CURRENT_SOURCE_DIR TESTNAME TOLERANCE
-    Example: python executeRegressionTestCase.py openfast/build openfast/reg_tests Test02 0.000001
+    Usage: python executeRegressionTestCase.py CMAKE_BINARY_DIR CMAKE_CURRENT_SOURCE_DIR TESTNAME TOLERANCE SYSTEM_NAME COMPILER_ID
+    Example: python executeRegressionTestCase.py openfast/build openfast/reg_tests Test02 0.000001 Darwin INTEL
 """
 
 import sys
 import os
 import subprocess
+
+##### Helper functions
 
 def exitWithError(error):
     print error
@@ -19,8 +21,10 @@ def exitWithDirNotFound(dir):
 def exitWithFileNotFound(file):
     exitWithError("File does not exist: {}\n".format(file))
 
+##### Main program
+
 # Verify input arguments
-if len(sys.argv) != 5:
+if len(sys.argv) != 7:
     exitWithError("Invalid arguments: {}\n".format(" ".join(sys.argv)) +
     "Usage: python executeRegressionTestCase.py CMAKE_BINARY_DIR CMAKE_CURRENT_SOURCE_DIR TESTNAME TOLERANCE")
 
@@ -28,32 +32,43 @@ binaryDirectory = sys.argv[1]
 sourceDirectory = sys.argv[2]
 caseName = sys.argv[3]
 tolerance = sys.argv[4]
+systemName = sys.argv[5]
+compilerId = sys.argv[6]
 
-if not os.path.isdir(binaryDirectory):
-    exitWithDirNotFound(binaryDirectory)
+if not os.path.isdir(binaryDirectory): exitWithDirNotFound(binaryDirectory)
+if not os.path.isdir(sourceDirectory): exitWithDirNotFound(sourceDirectory)
 
-if not os.path.isdir(sourceDirectory):
-    exitWithDirNotFound(sourceDirectory)
+# Map the system and compiler configurations to a solution set
+# Internal names -> Human readable names
+systemName_map = {
+    "Darwin": "macos",
+    "RHEL": "rhel"
+}
+compilerId_map = {
+    "GNU": "gnu",
+    "INTEL": "intel"
+}
+
+# Build the target output directory name or choose the default
+targetSystem = systemName_map.get(systemName, "rhel")
+targetCompiler = compilerId_map.get(compilerId, "intel")
+targetOutputDirectory = sourceDirectory + "/RegressionTestData/{}-{}".format(targetSystem, targetCompiler)
+if not os.path.isdir(targetOutputDirectory): exitWithDirNotFound(targetOutputDirectory)
 
 # build, verify, and run the openfast command based on input arguments
 executable = binaryDirectory + "/glue-codes/fast/openfast"
-if not os.path.isfile(executable):
-    exitWithFileNotFound(executable)
+if not os.path.isfile(executable): exitWithFileNotFound(executable)
+
 fast_command = "{} {}.fst > {}.log".format(executable, caseName, caseName)
 fast_return_code = subprocess.call(fast_command, shell=True)
 
 # build, verify, and run the regression test command based on input arguments
 testscript = sourceDirectory + "/pass_fail.py"
-if not os.path.isfile(testscript):
-    exitWithFileNotFound(testscript)
-
 output1 = binaryDirectory + "/reg_tests/{}.outb".format(caseName)
-if not os.path.isfile(output1):
-    exitWithFileNotFound(output1)
-
-output2 = sourceDirectory + "/RegressionTestData/outputs/{}.outb".format(caseName)
-if not os.path.isfile(output2):
-    exitWithFileNotFound(output2)
+output2 = targetOutputDirectory + "/outputs/{}.outb".format(caseName)
+if not os.path.isfile(testscript): exitWithFileNotFound(testscript)
+if not os.path.isfile(output1): exitWithFileNotFound(output1)
+if not os.path.isfile(output2): exitWithFileNotFound(output2)
 
 test_command = " ".join(["python", testscript, output1, output2, tolerance])
 test_return_code = subprocess.call(test_command, shell=True)
