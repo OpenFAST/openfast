6bff4536b6e50140476d7453a9ec35fabd57977c
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-05-10 15:41:35 -0600

    update test list file

--- ctest/CTestTestfile.cmake
+++ ctest/CTestTestfile.cmake
@@ -1,4 +1,6 @@
-add_test(Test01 python ${CMAKE_CURRENT_SOURCE_DIR}/reg_tests/executeRegressionTestCase.py ${CMAKE_CURRENT_SOURCE_DIR}/build ${CMAKE_CURRENT_SOURCE_DIR}/reg_tests Test01 0.001)
-#add_test(Test02 python ${CMAKE_CURRENT_SOURCE_DIR}/reg_tests/executeRegressionTestCase.py ${CMAKE_CURRENT_SOURCE_DIR}/build ${CMAKE_CURRENT_SOURCE_DIR}/reg_tests Test02 0.001)
-#add_test(Test03 python ${CMAKE_CURRENT_SOURCE_DIR}/reg_tests/executeRegressionTestCase.py ${CMAKE_CURRENT_SOURCE_DIR}/build ${CMAKE_CURRENT_SOURCE_DIR}/reg_tests Test03 0.001)
-#add_test(Test04 python ${CMAKE_CURRENT_SOURCE_DIR}/reg_tests/executeRegressionTestCase.py ${CMAKE_CURRENT_SOURCE_DIR}/build ${CMAKE_CURRENT_SOURCE_DIR}/reg_tests Test04 0.001)
+
+add_test(Test01 python ${CTEST_SOURCE_DIRECTORY}/reg_tests/executeRegressionTestCase.py Test01 ${CTEST_SOURCE_DIRECTORY}/install/bin/openfast ${CTEST_SOURCE_DIRECTORY} 0.001)
+#set_tests_properties(Test01 PROPERTIES ENVIRONMENT "CTEST_OUTPUT_ON_FAILURE=1" TIMEOUT 1000 WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}" LABELS "regression")
+#add_test(Test02 python ${CTEST_SOURCE_DIRECTORY}/reg_tests/executeRegressionTestCase.py Test02 ${CTEST_SOURCE_DIRECTORY}/install/bin/openfast ${CTEST_SOURCE_DIRECTORY} 0.001)
+#add_test(Test03 python ${CTEST_SOURCE_DIRECTORY}/reg_tests/executeRegressionTestCase.py Test03 ${CTEST_SOURCE_DIRECTORY}/install/bin/openfast ${CTEST_SOURCE_DIRECTORY} 0.001)
+#add_test(Test04 python ${CTEST_SOURCE_DIRECTORY}/reg_tests/executeRegressionTestCase.py Test04 ${CTEST_SOURCE_DIRECTORY}/install/bin/openfast ${CTEST_SOURCE_DIRECTORY} 0.001)
