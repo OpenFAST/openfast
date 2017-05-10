30083a281075b465f0d08c284540e0d4bbf00306
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-05-10 15:37:39 -0600

    add configuration for a local cdash to ctest

--- ctest/CTestConfig.cmake
+++ ctest/CTestConfig.cmake
@@ -1,6 +1,15 @@
-set(CTEST_PROJECT_NAME         "Test-Project")
-set(CTEST_NIGHTLY_START_TIME   "01:00:00 MST")
-set(CTEST_DROP_METHOD          "http")
-set(CTEST_DROP_SITE            "raf.mudaf")
-set(CTEST_DROP_LOCATION        "/cdash/submit.php?project=TestProject")
-set(CTEST_DROP_SITE_CDASH      FALSE)
+## This file should be placed in the root directory of your project.
+## Then modify the CMakeLists.txt file in the root directory of your
+## project to incorporate the testing dashboard.
+##
+## # The following are required to submit to the CDash dashboard:
+##   ENABLE_TESTING()
+##   INCLUDE(CTest)
+
+set(CTEST_PROJECT_NAME "openfast")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "localhost")
+set(CTEST_DROP_LOCATION        "/cdash/public/submit.php?project=openfast")
+set(CTEST_DROP_SITE_CDASH TRUE)
