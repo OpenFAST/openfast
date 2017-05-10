a6d301903fa944041e3bc9d6f47a278f90540987
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-05-10 15:36:54 -0600

    add more genericness to the python scripts to ensure cross platform and cross installation compatibility

--- reg_tests/executeRegressionTestCase.py
+++ reg_tests/executeRegressionTestCase.py
@@ -1,12 +1,16 @@
 """
-    This script executes openfast and a regression test for a single case.
+    This script executes openfast and a regression test for a single test case.
+    The test case must be one of the CertTest cases, and the r-test submodule must be initialized.
+    r-test is initialized with `git submodule update --init --recursive` or
+    updated with `git submodule update`
 
-    Usage: python executeRegressionTestCase.py CMAKE_BINARY_DIR CMAKE_CURRENT_SOURCE_DIR TESTNAME TOLERANCE
-    Example: python executeRegressionTestCase.py openfast/build openfast/reg_tests Test02 0.000001
+    Usage: python executeRegressionTestCase.py testname openfast_executable tolerance
+    Example: python executeRegressionTestCase.py Test02 openfast 0.000001
 """
 
-import sys
 import os
+import sys
+import shutil
 import subprocess
 
 def exitWithError(error):
@@ -19,43 +23,61 @@ def exitWithDirNotFound(dir):
 def exitWithFileNotFound(file):
     exitWithError("File does not exist: {}\n".format(file))
 
+print sys.argv
+
 # Verify input arguments
 if len(sys.argv) != 5:
     exitWithError("Invalid arguments: {}\n".format(" ".join(sys.argv)) +
     "Usage: python executeRegressionTestCase.py CMAKE_BINARY_DIR CMAKE_CURRENT_SOURCE_DIR TESTNAME TOLERANCE")
 
-binaryDirectory = sys.argv[1]
-sourceDirectory = sys.argv[2]
-caseName = sys.argv[3]
+caseName = sys.argv[1]
+executable = sys.argv[2]
+sourceDirectory = sys.argv[3]
 tolerance = sys.argv[4]
 
-if not os.path.isdir(binaryDirectory):
-    exitWithDirNotFound(binaryDirectory)
-
 if not os.path.isdir(sourceDirectory):
-    exitWithDirNotFound(sourceDirectory)
+    exitWithError("The given source directory, {}, does not exist.".format(sourceDirectory))
 
-# build, verify, and run the openfast command based on input arguments
-executable = binaryDirectory + "/glue-codes/fast/openfast"
-if not os.path.isfile(executable):
-    exitWithFileNotFound(executable)
-fast_command = "{} {}.fst > {}.log".format(executable, caseName, caseName)
-fast_return_code = subprocess.call(fast_command, shell=True)
+# the r-test submodule, /inputs and /outputs subdirectories are required to run the regression test
+testDataDirectory = os.path.join(sourceDirectory, "reg_tests", "r-test")
+if not os.path.isdir(testDataDirectory):
+    exitWithError("The test data directory, {}, does not exist. If you haven't already, run `git submodule update --init --recursive`".format(testDataDirectory))
+inputsDirectory = os.path.join(testDataDirectory, "inputs")
+if not os.path.isdir(inputsDirectory):
+    exitWithError("The test data inputs directory, {}, does not exist. Try running `git submodule update`".format(inputsDirectory))
+outputsDirectory = os.path.join(testDataDirectory, "outputs")
+if not os.path.isdir(outputsDirectory):
+    exitWithError("The test data outputs directory, {}, does not exist. Try running `git submodule update`".format(outputsDirectory))
+
+# create the local output directory if it does not already exist
+# and initialize it with input files for all test cases
+localDirectory = "outputs-local"
+if not os.path.isdir(localDirectory):
+    shutil.copytree(inputsDirectory, localDirectory)
+
+# execute the given case locally
+caseInputFile = os.path.join(localDirectory, caseName) + ".fst"
+executionScript = os.path.join(sourceDirectory, "reg_tests", "executeOpenfastCase.py")
+command = "python {} {} {}".format(executionScript, caseInputFile, executable)
+print "'{}' - running".format(command)
+return_code = subprocess.call(command, shell=True)
+print "'{}' - finished with exit code {}".format(command, return_code)
+# TODO check the return code here
 
 # build, verify, and run the regression test command based on input arguments
-testscript = sourceDirectory + "/pass_fail.py"
-if not os.path.isfile(testscript):
-    exitWithFileNotFound(testscript)
+regTestScript = os.path.join(sourceDirectory, "reg_tests", "pass_fail.py")
+if not os.path.isfile(regTestScript):
+    exitWithFileNotFound(regTestScript)
 
-output1 = binaryDirectory + "/reg_tests/{}.outb".format(caseName)
-if not os.path.isfile(output1):
-    exitWithFileNotFound(output1)
+caseOutputFile = os.path.join(localDirectory, caseName) + ".outb"
+if not os.path.isfile(caseOutputFile):
+    exitWithFileNotFound(caseOutputFile)
 
-output2 = sourceDirectory + "/RegressionTestData/outputs/{}.outb".format(caseName)
-if not os.path.isfile(output2):
-    exitWithFileNotFound(output2)
+caseGoldStandardFile = os.path.join(outputsDirectory, caseName) + ".outb"
+if not os.path.isfile(caseGoldStandardFile):
+    exitWithFileNotFound(caseGoldStandardFile)
 
-test_command = " ".join(["python", testscript, output1, output2, tolerance])
+test_command = " ".join(["python", regTestScript, caseOutputFile, caseGoldStandardFile, tolerance])
 test_return_code = subprocess.call(test_command, shell=True)
 
 # return pass/fail
