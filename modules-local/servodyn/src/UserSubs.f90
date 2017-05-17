1411f63ff218b16208aaf676b937454b62e65396
Jason Jonkman <jason.jonkman@nrel.gov>
2017-05-17 16:21:41 -0600

    Added a yaw-step controller for FAST.Farm calibration

--- modules-local/servodyn/src/UserSubs.f90
+++ modules-local/servodyn/src/UserSubs.f90
@@ -426,6 +426,7 @@ SUBROUTINE UserYawCont ( YawPos, YawRate, WindDir, YawError, NumBl, ZTime, DT, D
 
 
 USE                             Precision
+USE                             NWTC_Library
 
 
 IMPLICIT                        NONE
@@ -451,6 +452,21 @@ CHARACTER(1024), INTENT(IN ) :: DirRoot                                         
 YawPosCom  = 0.0
 YawRateCom = 0.0
 
+!JASON: IMPOSE YAW STEP FOR FAST.Farm CALIBRATION CASE - START
+IF (      ( ZTime >= 298.0_DbKi ) .AND. ( ZTime < 300.0_DbKi ) ) THEN
+   YawRateCom = ( 10.0_ReKi/2.0_ReKi )*D2R 
+   YawPosCom  = 0.0           + YawRateCom*( ZTime - 298.0_DbKi )
+ELSE IF ( ( ZTime >= 300.0_DbKi ) .AND. ( ZTime < 598.0_DbKi ) ) THEN    
+   YawRateCom = 0.0
+   YawPosCom  = 10.0_ReKi*D2R
+ELSE IF ( ( ZTime >= 598.0_DbKi ) .AND. ( ZTime < 600.0_DbKi ) ) THEN    
+   YawRateCom = ( 15.0_ReKi/2.0_ReKi )*D2R 
+   YawPosCom  = 10.0_ReKi*D2R + YawRateCom*( ZTime - 598.0_DbKi )
+ELSE IF ( ( ZTime >= 600.0_DbKi )                               ) THEN
+   YawRateCom = 0.0
+   YawPosCom  = 25.0_ReKi*D2R
+END IF
+!JASON: IMPOSE YAW STEP FOR FAST.Farm CALIBRATION CASE - END
 
 
 RETURN
