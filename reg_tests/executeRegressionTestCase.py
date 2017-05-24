a1a8abf68cf33bdb3c4ea8cbe9ec7fa214818566
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-05-24 14:36:26 -0600

    update python program after changes to cmake driver; accept more explicit input arguments and build fewer directory paths internally

--- reg_tests/executeRegressionTestCase.py
+++ reg_tests/executeRegressionTestCase.py
@@ -4,8 +4,8 @@
     r-test, which must be initialized prior to running. r-test can be initialized
     with `git submodule update --init --recursive` or updated with `git submodule update`.
 
-    Usage: `python3 executeRegressionTestCase.py testname openfast_executable source_directory tolerance system_name compiler_id`
-    Example: `python3 executeRegressionTestCase.py Test02 openfast path/to/openfast_repo 0.000001 [Darwin,Linux,Windows] [Intel,GNU]`
+    Usage: `python3 executeRegressionTestCase.py testname openfast_executable source_directory build_directory tolerance system_name compiler_id`
+    Example: `python3 executeRegressionTestCase.py Test02 openfast path/to/openfast_repo path/to/openfast_repo/build 0.000001 [Darwin,Linux,Windows] [Intel,GNU]`
 """
 
 import os
@@ -27,20 +27,21 @@ def exitWithFileNotFound(file):
 
 ##### Main program
 
-# determine python version
+### Determine python version
 pythonCommand = "python3"
 if sys.version_info < (3, 0):
      pythonCommand = "python"
 
-# Verify input arguments
-if len(sys.argv) < 5 or len(sys.argv) > 7:
+### Verify input arguments
+if len(sys.argv) < 6 or len(sys.argv) > 8:
     exitWithError("Invalid arguments: {}\n".format(" ".join(sys.argv)) +
     "Usage: {} executeRegressionTestCase.py testname openfast_executable source_directory tolerance system_name compiler_id".format(pythonCommand))
 
 caseName = sys.argv[1]
 executable = sys.argv[2]
 sourceDirectory = sys.argv[3]
-tolerance = sys.argv[4]
+buildDirectory = sys.argv[4]
+tolerance = sys.argv[5]
 
 # verify executable
 try:
@@ -56,6 +57,13 @@ except OSError as e:
 if not os.path.isdir(sourceDirectory):
     exitWithError("The given source directory, {}, does not exist.".format(sourceDirectory))
 
+# verify build directory
+if not os.path.isdir(buildDirectory):
+    os.mkdir(buildDirectory)
+
+if not os.path.isdir(buildDirectory):
+    exitWithError("The given build directory, {}, does not exist.".format(buildDirectory))
+
 # verify tolerance
 try:
     float(tolerance)
@@ -64,18 +72,18 @@ except ValueError:
 
 systemcompiler_given = True
 try:
-    systemName = sys.argv[5]
+    systemName = sys.argv[6]
 except IndexError:
     systemcompiler_given = False
     systemName = "not_given"
 
 try:
-    compilerId = sys.argv[6]
+    compilerId = sys.argv[7]
 except IndexError:
     systemcompiler_given = False
     compilerId = "not_given"
 
-# Map the system and compiler configurations to a solution set
+### Map the system and compiler configurations to a solution set
 # Internal names -> Human readable names
 systemName_map = {
     "darwin": "macos",
@@ -93,58 +101,57 @@ else:
     targetSystem = systemName_map.get(systemName.lower())
     targetCompiler = compilerId_map.get(compilerId.lower())
 
-targetOutput = os.path.join(targetSystem+"-"+targetCompiler)
+outputType = os.path.join(targetSystem+"-"+targetCompiler)
 if not systemcompiler_given:
     print("\nThe gold standard files are machine-compiler dependent.\n" +
     "Defaulting to {}-{}.\n".format(targetSystem, targetCompiler))
 
-# the r-test submodule, inputs subdirectory, and corresponding machine-compiler outputs subdirectorie are required to run the regression test
-testDataDirectory = os.path.join(sourceDirectory, "reg_tests", "r-test")
-if not os.path.isdir(testDataDirectory):
-    exitWithError("The test data directory, {}, does not exist. If you haven't already, run `git submodule update --init --recursive`".format(testDataDirectory))
-outputsDirectory = os.path.join(testDataDirectory, targetOutput)
-if not os.path.isdir(outputsDirectory):
-    exitWithError("The test data outputs directory, {}, does not exist. Try running `git submodule update`".format(outputsDirectory))
-inputsDirectory = os.path.join(testDataDirectory, "inputs")
+### Build the filesystem navigation variables for running openfast on the test case
+regtests = os.path.join(sourceDirectory, "reg_tests")
+rtest = os.path.join(regtests, "r-test")
+targetOutputDirectory = os.path.join(rtest, outputType)
+inputsDirectory = os.path.join(rtest, "inputs")
+testBuildDirectory = os.path.join(buildDirectory, "outputs-local")
+
+# verify all the required directories exist
+if not os.path.isdir(rtest):
+    exitWithError("The test data directory, {}, does not exist. If you haven't already, run `git submodule update --init --recursive`".format(rtest))
+if not os.path.isdir(targetOutputDirectory):
+    exitWithError("The test data outputs directory, {}, does not exist. Try running `git submodule update`".format(targetOutputDirectory))
 if not os.path.isdir(inputsDirectory):
     exitWithError("The test data inputs directory, {}, does not exist. Verify your local repository is up to date.".format(inputsDirectory))
 
 # create the local output directory if it does not already exist
 # and initialize it with input files for all test cases
-localDirectory = os.path.join(sourceDirectory, "ctest-build", "outputs-local")
-if not os.path.isdir(localDirectory):
-    shutil.copytree(inputsDirectory, localDirectory)
+if not os.path.isdir(testBuildDirectory):
+    shutil.copytree(inputsDirectory, testBuildDirectory)
 
-# execute the given case locally
-caseInputFile = os.path.join(localDirectory, caseName) + ".fst"
-executionScript = os.path.join(sourceDirectory, "reg_tests", "executeOpenfastCase.py")
-command = " ".join([pythonCommand, executionScript, caseInputFile, executable])
-print("'{}' - running".format(command))
+### Run openfast on the test case
+caseInputFile = os.path.join(testBuildDirectory, caseName + ".fst")
+executionScript = os.path.join(regtests, "executeOpenfastCase.py")
+executionCommand = " ".join([pythonCommand, executionScript, caseInputFile, executable])
+print("'{}' - running".format(executionCommand))
 sys.stdout.flush()
-return_code = subprocess.call(command, shell=True)
-print("'{}' - finished with exit code {}".format(command, return_code))
+executionReturnCode = subprocess.call(executionCommand, shell=True)
+print("'{}' - finished with exit code {}".format(executionCommand, executionReturnCode))
 
-if return_code != 0:
+if executionReturnCode != 0:
     exitWithError("")
 
-# build, verify, and run the regression test command based on input arguments
-regTestScript = os.path.join(sourceDirectory, "reg_tests", "pass_fail.py")
-if not os.path.isfile(regTestScript):
-    exitWithFileNotFound(regTestScript)
+### Build the filesystem navigation variables for running the regression test
+passFailScript = os.path.join(regtests, "pass_fail.py")
+localOutputFile = os.path.join(testBuildDirectory, caseName + ".outb")
+goldStandardFile = os.path.join(targetOutputDirectory, caseName + ".outb")
 
-caseOutputFile = os.path.join(localDirectory, caseName) + ".outb"
-if not os.path.isfile(caseOutputFile):
-    exitWithFileNotFound(caseOutputFile)
+if not os.path.isfile(passFailScript): exitWithFileNotFound(passFailScript)
+if not os.path.isfile(localOutputFile): exitWithFileNotFound(localOutputFile)
+if not os.path.isfile(goldStandardFile): exitWithFileNotFound(goldStandardFile)
 
-caseGoldStandardFile = os.path.join(outputsDirectory, caseName) + ".outb"
-if not os.path.isfile(caseGoldStandardFile):
-    exitWithFileNotFound(caseGoldStandardFile)
-
-test_command = " ".join([pythonCommand, regTestScript, caseOutputFile, caseGoldStandardFile, tolerance])
-print("'{}' - running".format(test_command))
+passfailCommand = " ".join([pythonCommand, passFailScript, localOutputFile, goldStandardFile, tolerance])
+print("'{}' - running".format(passfailCommand))
 sys.stdout.flush()
-test_return_code = subprocess.call(test_command, shell=True)
-print("'{}' - finished with exit code {}".format(test_command, test_return_code))
+passfailReturnCode = subprocess.call(passfailCommand, shell=True)
+print("'{}' - finished with exit code {}".format(passfailCommand, passfailReturnCode))
 
 # return pass/fail
-sys.exit(test_return_code)
+sys.exit(passfailReturnCode)
