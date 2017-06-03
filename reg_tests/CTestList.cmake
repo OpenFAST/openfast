ad3f9144db1f320889c6e9707f79ff1736cfe8ed
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-06-02 19:52:45 -0600

    increase the default timeout for each test; test case 21 takes a while

--- reg_tests/CTestList.cmake
+++ reg_tests/CTestList.cmake
@@ -36,8 +36,8 @@ function(of_regression testname)
        ${CMAKE_SYSTEM_NAME}             # [Darwin,Linux,Windows]
        ${CMAKE_Fortran_COMPILER_ID}     # [Intel,GNU]
   )
-  # limit each test to 25 minutes: 1500s
-  set_tests_properties(${testname} PROPERTIES TIMEOUT 1500 WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}" LABELS "regression")
+  # limit each test to 45 minutes: 2700s
+  set_tests_properties(${testname} PROPERTIES TIMEOUT 2700 WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}" LABELS "regression")
 endfunction(of_regression)
 
 function(bd_regression testname)
