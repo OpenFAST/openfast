90802a65c02aaeef4f7b088d267fc91d5dfd3ff6
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-07-30 22:23:04 -0600

    change markdown to html for viewing in browser

--- reg_tests/lib/errorPlotting.py
+++ reg_tests/lib/errorPlotting.py
@@ -41,14 +41,14 @@ def validateAndExpandInputs(argv):
     rtl.validateFileOrExit(testSolution)
     rtl.validateFileOrExit(baselineSolution)
     return (testSolution, baselineSolution, attribute)
-    
+
 def parseSolution(solution):
     try:
         data, info = load_output(solution)
         return (data, info)
     except Exception as e:
         rtl.exitWithError("Error: {}".format(e))
-    
+
 def plotError(xseries, y1series, y2series, title, xlabel, y1label, y2label):
     diff = y1series - y2series
     plt.figure()
@@ -68,47 +68,73 @@ def plotError(xseries, y1series, y2series, title, xlabel, y1label, y2label):
 
 def savePlot(plt, path, foutname):
     plt.savefig(os.path.join(path, foutname+".png"))
-    
+
 def plotOpenfastError(testSolution, baselineSolution, attribute):
     testSolution, baselineSolution, attribute = validateAndExpandInputs([testSolution, baselineSolution, attribute])
     dict1, info1 = parseSolution(testSolution)
     dict2, info2 = parseSolution(baselineSolution)
-    
+
     try:
         channel = info1['attribute_names'].index(attribute)
     except Exception as e:
         rtl.exitWithError("Error: Invalid channel name--{}".format(e))
-        
+
     # get test name -- this could break if .outb file is not used
     testname = testSolution.split("/")[-1]
     testname = testname.split(".")[-2]
     xlabel = 'Time (s)'
     y1label = attribute + " (" + info1["attribute_units"][channel] + ")"
     y2label = "Baseline - Local (" + info1['attribute_units'][channel] + ")"
-    
+
     timevec = dict1[:, 0]
     y1series = np.array(dict1[:, channel], dtype = np.float)
     y2series = np.array(dict2[:, channel], dtype = np.float)
     plt = plotError(timevec, y1series, y2series, testname, xlabel, y1label, y2label)
-    
+
     basePath = os.path.sep.join(testSolution.split(os.path.sep)[:-1])
     plotPath = os.path.join(basePath, "plots")
     rtl.validateDirOrMkdir(plotPath)
     savePlot(plt, plotPath, attribute)
-    
+
 def initializePlotDirectory(testSolution, plotList):
     basePath = os.path.sep.join(testSolution.split(os.path.sep)[:-1])
     plotPath = os.path.join(basePath, "plots")
     rtl.validateDirOrMkdir(plotPath)
-    with open(os.path.join(plotPath, "Plots.md"), "w") as plotsmd:
-        plotsmd.write("These plots were generated automatically.  ")
-        plotsmd.write("The plots included are:  ")
-        for plot in plotList:
-            plotsmd.write(plot + "  \n")
-        for plot in plotList:
-            plotsmd.write("![alt text][{}]  ".format(plot) + "  \n")
-        for plot in plotList:
-            plotsmd.write("[{}]: {}".format(plot, os.path.join(plot+".png")) + "\n")
+    with open(os.path.join(plotPath, "plots.html"), "w") as plotshtml:
+        plotshtml.write('<!DOCTYPE html>' + "\n")
+        plotshtml.write('<html>' + "\n")
+        plotshtml.write('<head>' + "\n")
+        plotshtml.write('<title>YEEEAA</title>' + "\n")
+        plotshtml.write('</head>' + "\n")
+        plotshtml.write('<body>' + "\n")
+        plotshtml.write('<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">')
+        plotshtml.write('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">' + "\n")
+        plotshtml.write('<section>' + "\n")
+        plotshtml.write('<div class="container gal-container">' + "\n")
 
-    plotsmd.close()
-        
+        for plot in plotList:
+            plotshtml.write('<div class="col-md-8 col-sm-12 co-xs-12 gal-item">' + "\n")
+            plotshtml.write('<div class="box">' + "\n")
+            plotshtml.write('<a href="#" data-toggle="modal" data-target="#1">' + "\n")
+            plotshtml.write('<img src="{}">'.format(plot+".png") + "\n")
+            plotshtml.write('</a>' + "\n")
+            plotshtml.write('<div class="modal fade" id="1" tabindex="-1" role="dialog">' + "\n")
+            plotshtml.write('<div class="modal-dialog" role="document">' + "\n")
+            plotshtml.write('<div class="modal-content">' + "\n")
+            plotshtml.write('<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>' + "\n")
+            plotshtml.write('<div class="modal-body">' + "\n")
+            plotshtml.write('<img src="{}">'.format(plot+".png") + "\n")
+            plotshtml.write('</div>' + "\n")
+            plotshtml.write('<div class="col-md-12 description">' + "\n")
+            plotshtml.write('<h4>This is the first one on my Gallery</h4>' + "\n")
+            plotshtml.write('</div>' + "\n")
+            plotshtml.write('</div>' + "\n")
+            plotshtml.write('</div>' + "\n")
+            plotshtml.write('</div>' + "\n")
+            plotshtml.write('</div>' + "\n")
+            plotshtml.write('</div>' + "\n")
+
+        plotshtml.write('</body>' + "\n")
+        plotshtml.write('</html>' + "\n")
+
+    plotshtml.close()
