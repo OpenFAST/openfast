234458dc7fa4e2345fa76b17a72ddb6cc15f91a3
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-09-11 14:48:43 -0600

    bug fixes in checkrotmat unit test

--- modules-local/beamdyn/tests/test_BD_CheckRotMat.F90
+++ modules-local/beamdyn/tests/test_BD_CheckRotMat.F90
@@ -19,14 +19,14 @@ subroutine test_BD_CheckRotMat()
     call SetConstants()
     
     ! known valid rotation matrix: pi about x-axis
-    call calcRotationMatrix(testR, Pi_D, 1)
+    call calcRotationMatrix(testR, Pi, 1)
     call BD_CheckRotMat(testR, ErrStat, ErrMsg)
     @assertEqual(0, ErrStat)
 
     ! known invalid rotation matrix: halve the angle of the diagonal elements
     ! this should produce a fatal error (ErrStat = 4)
-    testR(:,2) = (/ testR(1,2),  cos(Pi_D/2), testR(3,2) /)
-    testR(:,2) = (/ testR(1,2), testR(2,2),  cos(Pi_D/2) /)
+    testR(:,2) = (/ testR(1,2),  cos(Pi/2), testR(3,2) /)
+    testR(:,3) = (/ testR(1,2), testR(2,2),  cos(Pi/2) /)
     call BD_CheckRotMat(testR, ErrStat, ErrMsg)    
     @assertEqual(4, ErrStat)
 
