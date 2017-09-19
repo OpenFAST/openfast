0a14f37d04aa6ebd3f52bac8564942b51b29b592
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-09-19 09:53:16 -0600

    clean up for consistency, expand test coverage
    test branches
     - both rotation angles 0, no transpose of input rotations (flag = 0)
     - delta2 > 0, no transpose of input rotations (flag = 0)
     - delta2 < 0, no transpose of input rotations (flag = 0)
     - flag = 1
     - flag = 2
     - flag = 3

--- modules-local/beamdyn/tests/test_BD_CrvCompose.F90
+++ modules-local/beamdyn/tests/test_BD_CrvCompose.F90
@@ -1,24 +1,31 @@
 @test
 subroutine test_BD_CrvCompose()
-    ! branches to test
-    ! delta2 > 0, no transpose of input rotations (flag = 0)
-    ! delta2 < 0, no transpose of input rotations (flag = 0)
-    ! flag = 1
-    ! flag = 2
-    ! flag = 3
+    ! test branches
+    ! - both rotation angles 0, no transpose of input rotations (flag = 0)
+    ! - delta2 > 0, no transpose of input rotations (flag = 0)
+    ! - delta2 < 0, no transpose of input rotations (flag = 0)
+    ! - flag = 1
+    ! - flag = 2
+    ! - flag = 3
     
     use pFUnit_mod
     use BeamDyn_Subs
     use NWTC_Num
+    use test_tools
     
     implicit none
-
+    
+    ! input rotation axis and angle
     real(BDKi), dimension(3)   :: n1, n2
     real(BDKi)                 :: angle1, angle2
-    real(BDKi), dimension(3)   :: rotparams1, rotparams2, composedparams
-    real(BDKi), dimension(3,3) :: testrotation, rotationmat1, rotationmat2, baselinerotation
-    real(BDKi), dimension(3)   :: testaxis, baselineaxis
-    real(BDKi)                 :: testangle, baselineangle
+    
+    ! result rotations, verification vectors
+    real(BDKi), dimension(3,3) :: testrotation, r1, r2
+    real(BDKi), dimension(3)   :: composedparams
+    real(BDKi), dimension(3)   :: initialvector, testvector, baselinevector
+    
+    ! other test settings
+    integer                    :: flag
     character(1024)            :: testname
     real(BDKi)                 :: tolerance
     
@@ -27,89 +34,138 @@ subroutine test_BD_CrvCompose()
     
     tolerance = 1e-14
     
-    ! set the rotation axes    
+    ! set the rotation axes and initial vector for all tests
     n1 = (/ 1, 0, 0 /) ! x axis
     n2 = (/ 0, 0, 1 /) ! z axis
+    initialvector = (/ 0, 0, 1 /)
     
     
+    ! --------------------------------------------------------------------------
+    testname = "both rotation angles 0, no transpose of input rotations (flag = 0)"
+    angle1 = 0 ! 0 degrees
+    angle2 = 0 ! 0 degrees
+    flag = 0
+    
+    ! both rotations should return an identity matrix    
+    r1 = calcRotationMatrix(angle1, n1)
+    r2 = calcRotationMatrix(angle2, n2)
+    baselinevector = matmul(r1, initialvector)
+    baselinevector = matmul(r2, baselinevector)
+    
+    call BD_CrvCompose(composedparams, 4*tan(angle1/4)*n1, 4*tan(angle2/4)*n2, flag)
+    call BD_CrvMatrixR(composedparams, testrotation)
+    testvector = matmul(testrotation, initialvector)
+    
+    @assertEqual(testvector, baselinevector, tolerance, testname)
+    
+    
+    ! --------------------------------------------------------------------------
     testname = "delta2 > 0, no transpose of input rotations (flag = 0)"
-    angle1 = 0.5*Pi_D ! 90 degrees
-    angle2 = Pi_D    ! 180 degrees
+    angle1 = PiBy2_D ! 90 degrees
+    angle2 = PiBy2_D ! 90 degrees
+    flag = 0
     
     ! a point initially at 0,0,1 is at
     ! 0,-1,0 after rotation 1
-    ! 0,1,0 after rotation 2
+    ! 1,0,0 after rotation 2
     
-    call parameterizedRotation(testrotation, angle1, angle2, n1, n2, 0)
-    call standardRotation(baselinerotation, angle1, angle2, 1, 3)
+    r1 = calcRotationMatrix(angle1, n1)
+    r2 = calcRotationMatrix(angle2, n2)
+    baselinevector = matmul(r1, initialvector)
+    baselinevector = matmul(r2, baselinevector)
     
-    testaxis = getRotationAxis(testrotation)
-    baselineaxis = getRotationAxis(baselinerotation)
-    testangle = getRotationAngle(testrotation)
-    baselineangle = getRotationAngle(baselinerotation)
+    call BD_CrvCompose(composedparams, 4*tan(angle1/4)*n1, 4*tan(angle2/4)*n2, flag)
+    call BD_CrvMatrixR(composedparams, testrotation)
+    testvector = matmul(testrotation, initialvector)
     
-    @assertEqual(testaxis, baselineaxis, tolerance, testname)
-    @assertEqual(testangle, baselineangle, tolerance, testname)
+    @assertEqual(testvector, baselinevector, tolerance, testname)
+      
     
-    
+    ! --------------------------------------------------------------------------
     testname = "delta2 < 0, no transpose of input rotations (flag = 0)"
     angle1 = PiBy2_D ! 90 degrees
-    angle2 = 1.5*Pi_D ! 270 degrees
+    angle2 = 1.5*Pi  ! 270 degrees
+    flag = 0
     
     ! a point initially at 0,0,1 is at
     ! 0,-1,0 after rotation 1
     ! -1,0,0 after rotation 2
     
-    call parameterizedRotation(testrotation, angle1, angle2, n1, n2, 0)
-    call standardRotation(baselinerotation, angle1, angle2, 1, 3)
+    r1 = calcRotationMatrix(angle1, n1)
+    r2 = calcRotationMatrix(angle2, n2)
+    baselinevector = matmul(r1, initialvector)
+    baselinevector = matmul(r2, baselinevector)
     
-    testaxis = getRotationAxis(testrotation)
-    baselineaxis = getRotationAxis(baselinerotation)
-    testangle = getRotationAngle(testrotation)
-    baselineangle = getRotationAngle(baselinerotation)
+    call BD_CrvCompose(composedparams, 4*tan(angle1/4)*n1, 4*tan(angle2/4)*n2, flag)
+    call BD_CrvMatrixR(composedparams, testrotation)
+    testvector = matmul(testrotation, initialvector)
     
-    @assertEqual(testaxis, baselineaxis, tolerance, testname)
-    @assertEqual(testangle, baselineangle, tolerance, testname)
+    @assertEqual(testvector, baselinevector, tolerance, testname)
     
-    !TODO flag = 1
-    !TODO flag = 2
-    !TODO flag = 3
     
-end subroutine
+    ! --------------------------------------------------------------------------
+    testname = "delta2 > 0, transpose of first rotation (flag = 1)"
+    angle1 = PiBy2_D ! 90 degrees
+    angle2 = PiBy2_D ! 90 degrees
+    flag = 1
+    
+    ! a point initially at 0,0,1 is at
+    ! 0,1,0 after rotation 1 <- transpose of the first rotation
+    ! -1,0,0 after rotation 2
+    
+    r1 = calcRotationMatrix(-angle1, n1)
+    r2 = calcRotationMatrix(angle2, n2)
+    baselinevector = matmul(r1, initialvector)
+    baselinevector = matmul(r2, baselinevector)
 
-subroutine parameterizedRotation(r, angle1, angle2, n1, n2, flag)
-    use BeamDyn_Subs
-    use test_tools
-    implicit none
+    call BD_CrvCompose(composedparams, 4*tan(angle1/4)*n1, 4*tan(angle2/4)*n2, flag)
+    call BD_CrvMatrixR(composedparams, testrotation)
+    testvector = matmul(testrotation, initialvector)
     
-    real(BDKi), intent(  out), dimension(3,3) :: r
-    real(BDKi), intent(in   )                 :: angle1, angle2
-    real(BDKi), intent(in   ), dimension(3)   :: n1, n2
-    integer   , intent(in   )                 :: flag
-    real(BDKi),                dimension(3)   :: params1, params2, composedparams
+    @assertEqual(testvector, baselinevector, tolerance, testname)
     
-    ! get the rotation parameters
-    call calcWMParameters(params1, angle1, n1)
-    call calcWMParameters(params2, angle2, n2)
     
-    ! compose the rotations
-    call BD_CrvCompose(composedparams, params1, params2, flag)
-
-    ! get the rotation matrix
-    call BD_CrvMatrixR(composedparams, r)
-end subroutine
-
-subroutine standardRotation(r, angle1, angle2, axis1, axis2)
-    use BeamDyn_Subs
-    use test_tools
-    implicit none
+    ! --------------------------------------------------------------------------
+    testname = "delta2 > 0, transpose of second rotation (flag = 2)"
+    angle1 = PiBy2_D ! 90 degrees
+    angle2 = PiBy2_D ! 90 degrees
+    flag = 2
     
-    real(BDKi), intent(  out), dimension(3,3) :: r
-    real(BDKi), intent(in   )                 :: angle1, angle2
-    integer   , intent(in   )                 :: axis1, axis2
-    real(BDKi),                dimension(3,3) :: rotationmat1, rotationmat2
+    ! a point initially at 0,0,1 is at
+    ! 0,-1,0 after rotation 1
+    ! -1,0,0 after rotation 2 <- transpose of the second rotation
     
-    call calcRotationMatrix(rotationmat1, angle1, axis1)
-    call calcRotationMatrix(rotationmat2, angle2, axis2)
-    r = matmul(rotationmat1, rotationmat2)
+    r1 = calcRotationMatrix(angle1, n1)
+    r2 = calcRotationMatrix(-angle2, n2)
+    baselinevector = matmul(r1, initialvector)
+    baselinevector = matmul(r2, baselinevector)
+    
+    call BD_CrvCompose(composedparams, 4*tan(angle1/4)*n1, 4*tan(angle2/4)*n2, flag)
+    call BD_CrvMatrixR(composedparams, testrotation)
+    testvector = matmul(testrotation, initialvector)
+    
+    @assertEqual(testvector, baselinevector, tolerance, testname)
+    
+    
+    ! --------------------------------------------------------------------------
+    testname = "delta2 > 0, transpose of both rotations (flag = 3)"
+    angle1 = PiBy2_D ! 90 degrees
+    angle2 = PiBy2_D ! 90 degrees
+    flag = 3
+    
+    ! a point initially at 0,0,1 is at
+    ! 0,1,0 after rotation 1 <- transpose of the first rotation
+    ! 1,0,0 after rotation 2 <- transpose of the second rotation
+    
+    r1 = calcRotationMatrix(-angle1, n1)
+    r2 = calcRotationMatrix(-angle2, n2)
+    baselinevector = matmul(r1, initialvector)
+    baselinevector = matmul(r2, baselinevector)
+    
+    call BD_CrvCompose(composedparams, 4*tan(angle1/4)*n1, 4*tan(angle2/4)*n2, flag)
+    call BD_CrvMatrixR(composedparams, testrotation)
+    testvector = matmul(testrotation, initialvector)
+    
+    @assertEqual(testvector, baselinevector, tolerance, testname)
+    
 end subroutine
