8f25b9340e3e9c7280b827dfcc01e7d8a639acb0
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-09-19 09:48:33 -0600

    generalize the rotation matrix method, add a specific method for x axis rotations

--- modules-local/beamdyn/tests/test_tools.F90
+++ modules-local/beamdyn/tests/test_tools.F90
@@ -14,27 +14,33 @@ contains
         
         params = 4.0 * tan(angle/4.0) * n
     end subroutine
-
-    subroutine calcRotationMatrix(r, angle, axis)
+    
+    function calcRotationMatrix(angle, axis)
         use BeamDyn_Subs
         implicit none
         
-        real(BDKi), intent(  out), dimension(3,3) :: r
-        real(BDKi), intent(in   )                 :: angle
-        integer,    intent(in   )                 :: axis
+        real(BDKi),             dimension(3,3) :: calcRotationMatrix
+        real(BDKi), intent(in)                 :: angle
+        real(BDKi), intent(in), dimension(3)   :: axis
+        real(BDKi),             dimension(3,3) :: r
+
+        r(1,:) = (/              cos(angle) + (1-cos(angle))*axis(1)**2, axis(1)*axis(2)*(1-cos(angle)) - axis(3)*sin(angle), axis(1)*axis(3)*(1-cos(angle)) + axis(2)*sin(angle) /)
+        r(2,:) = (/ axis(2)*axis(1)*(1-cos(angle)) + axis(3)*sin(angle),              cos(angle) + (1-cos(angle))*axis(2)**2, axis(2)*axis(3)*(1-cos(angle)) - axis(1)*sin(angle) /)
+        r(3,:) = (/ axis(3)*axis(1)*(1-cos(angle)) - axis(2)*sin(angle), axis(3)*axis(2)*(1-cos(angle)) + axis(1)*sin(angle),              cos(angle) + (1-cos(angle))*axis(3)**2 /)
         
-        if (axis == 1) then
-            r(:,1) = (/ 1.0,        0.0,         0.0 /)
-            r(:,2) = (/ 0.0, cos(angle), -sin(angle) /)
-            r(:,3) = (/ 0.0, sin(angle),  cos(angle) /)
-        else if (axis == 2) then
-            r(:,1) = (/ cos(angle), 0.0, -sin(angle) /)
-            r(:,2) = (/       0.0,  1.0,         0.0 /)
-            r(:,3) = (/ sin(angle), 0.0,  cos(angle) /)
-        else if (axis ==3) then
-            r(:,1) = (/ cos(angle), -sin(angle), 0.0 /)
-            r(:,2) = (/ sin(angle),  cos(angle), 0.0 /)
-            r(:,3) = (/        0.0,         0.0, 1.0 /)
-        end if
-    end subroutine
+        calcRotationMatrix = r
+        
+    end function
+
+    function RonXAxis(angle)
+        use BeamDyn_Subs
+        implicit none
+        
+        real(BDKi) :: angle, r(3,3), RonXAxis(3,3)
+        
+        r(1,:) = (/  1.0,         0.0,         0.0 /)
+        r(2,:) = (/  0.0,  cos(angle), -sin(angle) /)
+        r(3,:) = (/  0.0,  sin(angle),  cos(angle) /)
+        RonXAxis = r
+    end function  
 end module
