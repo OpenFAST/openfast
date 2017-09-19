075cc9102b4ec273eecc1452372ea402d255f20e
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-09-19 09:47:40 -0600

    remove a deprecated method to initialize random seeds

--- modules-local/beamdyn/tests/test_tools.F90
+++ modules-local/beamdyn/tests/test_tools.F90
@@ -3,18 +3,6 @@ module test_tools
 implicit none
   
 contains  
-    subroutine init_random_seed()
-        ! subroutine to initialize the random number generator seed from clock time
-        integer :: i, n, clock
-        integer, dimension(:), allocatable :: seed
-
-        call random_seed(size = n)
-        allocate(seed(n))
-        call system_clock(count = clock)
-        seed = clock + 37 * (/ (i - 1, i = 1, n) /)
-        call random_seed(put = seed)
-        deallocate(seed)
-    end subroutine init_random_seed
     
     subroutine calcWMParameters(params, angle, n)
         use BeamDyn_Subs
