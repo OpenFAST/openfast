835bd1d35386d0ace2ea36c3869b9ef23b6cb751
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-09-19 09:50:38 -0600

    expand the test envelope
    test branches
     - simple rotation with known parameters: Pi on xaxis
     - 0 rotation
     - small rotation with baseline WM parameters calculated

--- modules-local/beamdyn/tests/test_BD_CrvMatrixR.F90
+++ modules-local/beamdyn/tests/test_BD_CrvMatrixR.F90
@@ -1,9 +1,13 @@
 @test
 subroutine test_BD_CrvMatrixR()
-    ! branches to test
-    ! rotation matrix from analytically calculated WM parameters for simple rotation
-    ! invalid rotation matrix
-    ! does the WM formulation have any boundaries?
+    ! test branches
+    ! - simple rotation with known parameters: Pi on xaxis
+    ! - 0 rotation
+    ! - small rotation with baseline WM parameters calculated
+    
+    ! TODO
+    ! invalid wm parameters (if thats a thing)
+    ! does the implemented WM formulation have any boundaries?
     
     use pFUnit_mod
     use BeamDyn_Subs
@@ -14,7 +18,7 @@ subroutine test_BD_CrvMatrixR()
         
     real(BDKi), dimension(3,3) :: testR, baselineR
     real(BDKi), dimension(3)   :: wmparams
-    real(BDKi)                 :: rotationangle
+    real(BDKi)                 :: angle, n(3)
     character(1024)            :: testname
     real(BDKi)                 :: tolerance
     integer(IntKi)             :: ErrStat
@@ -25,19 +29,46 @@ subroutine test_BD_CrvMatrixR()
     
     tolerance = 1e-14
     
-    testname = "rotation matrix from analytically calculated WM parameters for simple rotation"
-    rotationangle = Pi_D
-
-    call calcWMParameters(wmparams, rotationangle, (/ 1.0, 0.0, 0.0 /))
+    ! set the rotation axis for all tests
+    n = (/ 1, 0, 0 /) ! x axis
+    
+    
+    ! --------------------------------------------------------------------------
+    testname = "simple rotation with known parameters: Pi on xaxis"
+    angle = Pi_D
+    
+    baselineR = RonXAxis(angle)
+    
+    ! Wiener-Milenkovic parameters are <4.0, 0.0, 0.0>
+    wmparams = (/ 4.0, 0.0, 0.0 /)
     call BD_CrvMatrixR(wmparams, testR)
     
-    ! baseline rotation matrix is rotation of pi around x-axis
-    ! result:
-    ! 1.0   0.0  0.0
-    ! 0.0  -1.0  0.0
-    ! 0.0   0.0 -1.0
-    call calcRotationMatrix(baselineR, rotationangle, 1)    
+    @assertEqual(baselineR, testR, tolerance, testname)
     
-    @assertEqual(testR, baselineR, tolerance, testname)
-
+    
+    ! --------------------------------------------------------------------------
+    testname = "0 rotation"
+    angle = 0
+    
+    baselineR = RonXAxis(angle)
+    
+    ! Wiener-Milenkovic parameters are <0.0, 0.0, 0.0>
+    wmparams = (/ 0.0, 0.0, 0.0 /)
+    call BD_CrvMatrixR(wmparams, testR)
+    
+    @assertEqual(baselineR, testR, tolerance, testname)
+    
+    
+    ! --------------------------------------------------------------------------
+    testname = "small rotation with baseline WM parameters calculated"
+    angle = 0.1*Pi_D
+    
+    baselineR = RonXAxis(angle)
+    
+    ! Wiener-Milenkovic parameters are calculated; note tangent is asymptotic at +/- pi/2
+    wmparams = 4*tan(angle/4)*n
+    call BD_CrvMatrixR(wmparams, testR)
+    
+    @assertEqual(baselineR, testR, tolerance, testname)
+      
 end subroutine test_BD_CrvMatrixR
