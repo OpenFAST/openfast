e90380a21826b396ce218af8e533e578fcb896f7
Rafael M Mudafort <rafael.mudafort@nrel.gov>
2017-09-20 13:26:50 -0600

    add helper methods for generating simple custom types
    based on a simple cantilevered beam under gravity, create
    BD_ParameterType
    BD_MiscVarType
    BD_InputType

--- modules-local/beamdyn/tests/test_tools.F90
+++ modules-local/beamdyn/tests/test_tools.F90
@@ -1,5 +1,7 @@
 module test_tools
   
+use BeamDyn_Types
+
 implicit none
   
 contains  
@@ -87,5 +89,101 @@ contains
         
         real(BDKi), dimension(3) :: getGravityInZ
         getGravityInZ = (/ 0.0, 0.0, -9.806 /)
-    end function    
+    end function
+    
+    type(BD_ParameterType) function simpleParameterType()
+        
+        type(BD_ParameterType) :: p
+        integer                :: i, j
+        integer                :: ErrStat
+        character(1024)        :: ErrMsg
+        
+        ! scalars
+        p%elem_total = 1
+        p%nqp = 16
+        p%qp_indx_offset = 0
+        
+        ! fixed size arrays
+        p%Glb_crv = (/ 0.0, 0.0, 0.0 /)
+        p%GlbRot = identity()
+        
+        ! allocate arrays
+        call AllocAry(p%qp%mmm, p%nqp, p%elem_total, 'qp_mmm', ErrStat, ErrMsg)
+        call AllocAry(p%qp%mEta, 3, p%nqp, p%elem_total, 'qp_RR0mEta', ErrStat, ErrMsg)
+        call AllocAry(p%Mass0_QP, 6, 6, p%nqp*p%elem_total, 'Mass0_QP', ErrStat, ErrMsg)
+        
+        ! construct arrays
+        p%qp%mmm = getMassMatrix()
+        
+        do j=1, p%elem_total
+            do i=1, p%nqp
+                p%qp%mEta(:,i,j) = (/ 0.0, 0.0, 0.0 /)
+                p%Mass0_QP(:,:,(i-1)*p%elem_total+j) = getMassMatrix()
+            end do
+        end do
+        
+        ! set the return value
+        simpleParameterType = p
+        
+    end function
+    
+    type(BD_MiscVarType) function simpleMiscVarType(nqp, nelem)
+        
+        type(BD_MiscVarType) :: m
+        integer, intent(in)  :: nqp, nelem
+        integer              :: i, j
+        integer              :: ErrStat
+        character(1024)      :: ErrMsg
+        
+        ! scalars
+        
+        ! fixed size arrays
+        
+        ! allocate arrays
+        call AllocAry(m%qp%Fg, 6, nqp, nelem, 'qp_Fg', ErrStat, ErrMsg)
+        call AllocAry(m%qp%RR0, 3, 3, nqp, nelem, 'qp_RR0', ErrStat, ErrMsg)
+        call AllocAry(m%qp%RR0mEta, 3, nqp, nelem, 'qp_RR0mEta', ErrStat, ErrMsg)
+        call AllocAry(m%DistrLoad_QP, 6, nqp, nelem, 'DistrLoad_QP', ErrStat, ErrMsg)
+        call AllocAry(m%qp%rho, 3, 3, nqp, nelem, 'qp_rho', ErrStat, ErrMsg)
+        
+        ! construct arrays
+        do j=1, nelem
+            do i=1, nqp
+                m%qp%RR0(:,:,i,j) = identity()
+                m%qp%RR0mEta(:,i,j) = (/ 0.0, 0.0, 0.0 /)
+            end do
+        end do
+        
+        ! set the return value
+        simpleMiscVarType = m
+        
+    end function
+
+    type(BD_InputType) function simpleInputType(nqp, nelem)
+        
+        type(BD_InputType)   :: i
+        integer, intent(in)  :: nqp, nelem
+        integer              :: j
+        integer              :: ErrStat
+        character(1024)      :: ErrMsg
+        
+        ! scalars
+        
+        ! fixed size arrays
+        
+        ! allocate arrays
+        call AllocAry(i%DistrLoad%Force, 3, nqp*nelem, 'DistrLoadForce', ErrStat, ErrMsg)
+        call AllocAry(i%DistrLoad%Moment, 3, nqp*nelem, 'DistrLoadMoment', ErrStat, ErrMsg)
+        
+        ! construct arrays
+        do j = 1, nqp*nelem
+            i%DistrLoad%Force(:,j)  = (/  3*(j-1)+1,  3*(j-1)+2,  3*(j-1)+3 /)
+            i%DistrLoad%Moment(:,j) = (/ -3*(j-1)-1, -3*(j-1)-2, -3*(j-1)-3 /)
+        end do
+        
+        ! set the return value
+        simpleInputType = i
+        
+    end function
+    
 end module
