930ab413d63d2b733fd50d292356d36d3d94e957
Bonnie Jonkman <bonnie.jonkman@envision-energy.com>
2018-01-07 23:00:32 -0700

    bug fix: linearization index from AD tower aero
    dUdy submatrix: ED tower translational displacement to ED tower aero moment block was incorrectly set as the ED platform translational velocity to ED tower moment block (when AD tower aero was used).

--- modules-local/fast-library/src/FAST_Lin.f90
+++ modules-local/fast-library/src/FAST_Lin.f90
@@ -1711,7 +1711,7 @@ SUBROUTINE Linear_ED_InputSolve_dy( p_FAST, u_ED, y_ED, y_AD, u_AD, MeshMapData,
                ED_Out_Start_tmp = ED_Out_Start_tmp + y_ED%BladeLn2Mesh(k)%NNodes*18 ! 6 fields with 3 components on each blade
             end do
          end if
-         ED_Out_Start_tmp = ED_Out_Start_tmp + y_ED%PlatformPtMesh%NNodes*6 ! 6 fields with 3 components
+         ED_Out_Start_tmp = ED_Out_Start_tmp + y_ED%PlatformPtMesh%NNodes*18 ! 6 fields with 3 components
             
             
             ! force equation:
