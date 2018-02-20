41ccddc37b0df76d61729b3195608ea2498b1f3e
Bonnie Jonkman <bonnie.jonkman@envision-energy.com>
2018-02-20 11:25:00 -0700

    Updated math for logMap routines when close to theta=pi (singularity)
    This math is a little cleaner and should give better numerical results (it doesn't rely on as many sqrt() calculations)

--- modules-local/nwtc-library/src/NWTC_Num.f90
+++ modules-local/nwtc-library/src/NWTC_Num.f90
@@ -1072,11 +1072,7 @@ CONTAINS
 !! \f}
 !! where \f$v\f$ is the skew-symmetric matrix associated with the unit-length eigenvector of \f$\Lambda\f$ associated with the eigenvalue 1.
 !! However, this equation has numerical issues near \f$\theta = \pi\f$, so for \f$\theta > 3.1\f$  we instead implement
-!! \f{equation}{
-!!\lambda_1 = \pm \theta\sqrt{ \frac{\left(1 + \Lambda_{11} - \Lambda_{22} - \Lambda_{33}\right)}{2\left(1-\cos\theta\right)}   } \\
-!!\lambda_2 = \pm \theta\sqrt{ \frac{\left(1 + \Lambda_{22} - \Lambda_{11} - \Lambda_{33}\right)}{2\left(1-\cos\theta\right)}   } \\
-!!\lambda_3 = \pm \theta\sqrt{ \frac{\left(1 + \Lambda_{33} - \Lambda_{11} - \Lambda_{22}\right)}{2\left(1-\cos\theta\right)}   }
-!! \f}
+!! a separate equation to find lambda * sign(lambda(indx_max))
 !! and use \f$\Lambda - \Lambda^T\f$ to choose the appropriate signs. 
 !!   
 !! This routine is the inverse of DCM_exp (nwtc_num::dcm_exp). \n
@@ -1109,47 +1105,56 @@ CONTAINS
          thetaOut = theta
       END IF      
       
+      !> Note that \f$ DCM = \begin{bmatrix}
+      !!     1-\frac{1-\cos\theta}{\theta^2}\left( \lambda_3^2 + \lambda_2^2\right) 
+      !!   &   \frac{\sin\theta}{\theta}\lambda_3+\frac{1-\cos\theta}{\theta^2}\lambda_1\lambda_2  
+      !!   &  -\frac{\sin\theta}{\theta}\lambda_2+\frac{1-\cos\theta}{\theta^2}\lambda_1\lambda_3 \\
+      !!      -\frac{\sin\theta}{\theta}\lambda_3+\frac{1-\cos\theta}{\theta^2}\lambda_1\lambda_2
+      !!   &  1-\frac{1-\cos\theta}{\theta^2}\left( \lambda_3^2 + \lambda_1^2\right) 
+      !!   &  \frac{\sin\theta}{\theta}\lambda_1+\frac{1-\cos\theta}{\theta^2}\lambda_2\lambda_3  \\
+      !!      \frac{\sin\theta}{\theta}\lambda_2+\frac{1-\cos\theta}{\theta^2}\lambda_1\lambda_3 
+      !!   & -\frac{\sin\theta}{\theta}\lambda_1+\frac{1-\cos\theta}{\theta^2}\lambda_2\lambda_3 
+      !!   &  1-\frac{1-\cos\theta}{\theta^2}\left( \lambda_2^2 + \lambda_1^2\right)                    \\
+      !!    \end{bmatrix} \f$
+      
       
       !IF ( EqualRealNos( pi_D, theta )  ) THEN
       IF ( theta > 3.1_DbKi ) THEN  ! theta/(2*sin(theta)) blows up quickly as theta approaches pi, 
          ! so I'm putting a pretty large tolerance on pi here, and using a different equation to find the solution near pi
                      
-         !d11 = 1 - (1-cos(theta))/theta^2 * (logMap3^2 + logMap2^2)
-         !d22 = 1 - (1-cos(theta))/theta^2 * (logMap3^2 + logMap1^2)
-         !d33 = 1 - (1-cos(theta))/theta^2 * (logMap2^2 + logMap1^2)
-                  
-         logMap(1) = theta * sqrt(abs( 0.5_DbKi * ( 1.0_DbKi + DCM(1,1) - DCM(2,2) - DCM(3,3) ) / (1.0_DbKi-cosTheta) ))
-         logMap(2) = theta * sqrt(abs( 0.5_DbKi * ( 1.0_DbKi - DCM(1,1) + DCM(2,2) - DCM(3,3) ) / (1.0_DbKi-cosTheta) ))
-         logMap(3) = theta * sqrt(abs( 0.5_DbKi * ( 1.0_DbKi - DCM(1,1) - DCM(2,2) + DCM(3,3) ) / (1.0_DbKi-cosTheta) ))
-               
-         ! we choose logMap1 positive then we get the signs for logMap2 and logMap3:
-         if ( .not. EqualRealNos( logMap(1), 0.0_DbKi ) ) then
-            !d12+d21=2*(1-cos(theta))/theta**2 * logMap(1)*logMap(2); 2*(1-cos(theta))/theta**2 * logMap(1)>0 so logMap(2) is sign(logMap(2),d12+d21)
-            !d13+d31=2*(1-cos(theta))/theta**2 * logMap(1)*logMap(3); 2*(1-cos(theta))/theta**2 * logMap(1)>0 so logMap(3) is sign(logMap(3),d13+d31)
-            
-            logMap(2) = sign( logMap(2), DCM(1,2)+DCM(2,1) )
-            logMap(3) = sign( logMap(3), DCM(1,3)+DCM(3,1) )            
+         logMap(1) = 1.0_DbKi + DCM(1,1) - DCM(2,2) - DCM(3,3);
+         logMap(2) = 1.0_DbKi - DCM(1,1) + DCM(2,2) - DCM(3,3);
+         logMap(3) = 1.0_DbKi - DCM(1,1) - DCM(2,2) + DCM(3,3);
+             
+         indx_max = maxloc( abs(logMap), 1 )
+             
+         divisor = sqrt(abs( logMap(indx_max) *  2.0_DbKi*(1.0_DbKi - cosTheta)  )) / theta  ! 2*(1-cosTheta)/theta^2 * abs(lambda(indx_max))
+         if (indx_max == 1) then
+           !logMap(1) = 1.0 + DCM(1,1) - DCM(2,2) - DCM(3,3)               ! 2*(1-cosTheta)/theta^2 * lambda(1) * lambda(1)
+            logMap(2) = DCM(1,2) + DCM(2,1)                                ! 2*(1-cosTheta)/theta^2 * lambda(1) * lambda(2)
+            logMap(3) = DCM(1,3) + DCM(3,1)                                ! 2*(1-cosTheta)/theta^2 * lambda(1) * lambda(3)
+         elseif (indx_max == 2) then
+            logMap(1) = DCM(1,2) + DCM(2,1)                                ! 2*(1-cosTheta)/theta^2 * lambda(2) * lambda(1)
+           !logMap(2) = 1.0 - DCM(1,1) + DCM(2,2) - DCM(3,3)               ! 2*(1-cosTheta)/theta^2 * lambda(2) * lambda(2)
+            logMap(3) = DCM(2,3) + DCM(3,2)                                ! 2*(1-cosTheta)/theta^2 * lambda(2) * lambda(3)
          else
-            ! because logMap1 is zero, we can choose logMap2 positive:
-            
-            !d23+d32=2*(1-cos(theta))/theta**2 * logMap(2)*logMap(3); 2*(1-cos(theta))/theta**2 * logMap(2)>0 so logMap(3) is sign(logMap(3),d23+d32)
-            logMap(3) = sign( logMap(3), DCM(2,3)+DCM(3,2) )            
-            
+            logMap(1) = DCM(1,3) + DCM(3,1)                                ! 2*(1-cosTheta)/theta^2 * lambda(3) * lambda(1)
+            logMap(2) = DCM(2,3) + DCM(3,2)                                ! 2*(1-cosTheta)/theta^2 * lambda(3) * lambda(2)
+           !logMap(3) = 1.0 - DCM(1,1) - DCM(2,2) + DCM(3,3)               ! 2*(1-cosTheta)/theta^2 * lambda(3) * lambda(3)
          end if
+         logMap = logMap / divisor                                         ! lambda * sign(lambda(indx_max))
+
                     
          ! at this point we may have the wrong sign for logMap (though if theta==pi, it doesn't matter because we can change it in the DCM_setLogMapforInterp() routines)
          ! we'll do a little checking to see if we should change the sign:
          
          IF ( EqualRealNos( pi_D, theta )  ) RETURN
          
-         v(1) = -DCM(3,2) + DCM(2,3) !-skewSym(3,2)
-         v(2) =  DCM(3,1) - DCM(1,3) ! skewSym(3,1)
-         v(3) = -DCM(2,1) + DCM(1,2) !-skewSym(2,1)
+         v(1) = -DCM(3,2) + DCM(2,3) !-skewSym(3,2) = 2*sin(theta)/theta * lambda(1) = (small positive value with theta near pi) * lambda(1)
+         v(2) =  DCM(3,1) - DCM(1,3) ! skewSym(3,1) = 2*sin(theta)/theta * lambda(2) = (small positive value with theta near pi) * lambda(2)
+         v(3) = -DCM(2,1) + DCM(1,2) !-skewSym(2,1) = 2*sin(theta)/theta * lambda(3) = (small positive value with theta near pi) * lambda(3)
  
-         indx_max = 1
-         do i=2,3
-            if ( abs(v(i)) > abs(v(indx_max)) ) indx_max = i
-         end do
+         indx_max = maxloc( abs(v), 1 )  ! find component with largest magnitude
          
          if ( .not. EqualRealNos( sign(1.0_DbKi,v(indx_max)), sign(1.0_DbKi,logMap(indx_max)) )) logMap = -logMap
          
@@ -1202,6 +1207,7 @@ CONTAINS
    REAL(ReKi)                        :: theta
    REAL(ReKi)                        :: TwoSinTheta
    REAL(ReKi)                        :: v(3)
+   REAL(ReKi)                        :: divisor
    INTEGER(IntKi)                    :: indx_max, i
       
          ! initialization
@@ -1218,29 +1224,28 @@ CONTAINS
       IF ( theta > 3.1_ReKi ) THEN  ! theta/(2*sin(theta)) blows up quickly as theta approaches pi, 
          ! so I'm putting a pretty large tolerance on pi here, and using a different equation to find the solution near pi
                      
-         !d11 = 1 - (1-cos(theta))/theta^2 * (logMap3^2 + logMap2^2)
-         !d22 = 1 - (1-cos(theta))/theta^2 * (logMap3^2 + logMap1^2)
-         !d33 = 1 - (1-cos(theta))/theta^2 * (logMap2^2 + logMap1^2)
-         
-         logMap(1) = theta * sqrt(abs( 0.5_ReKi * ( 1.0_ReKi + DCM(1,1) - DCM(2,2) - DCM(3,3) ) / (1.0_ReKi-cosTheta) ))
-         logMap(2) = theta * sqrt(abs( 0.5_ReKi * ( 1.0_ReKi - DCM(1,1) + DCM(2,2) - DCM(3,3) ) / (1.0_ReKi-cosTheta) ))
-         logMap(3) = theta * sqrt(abs( 0.5_ReKi * ( 1.0_ReKi - DCM(1,1) - DCM(2,2) + DCM(3,3) ) / (1.0_ReKi-cosTheta) ))
-               
-         ! we choose logMap1 positive then we get the signs for logMap2 and logMap3:
-         if ( .not. EqualRealNos( logMap(1), 0.0_ReKi ) ) then
-            !d12+d21=2*(1-cos(theta))/theta**2 * logMap(1)*logMap(2); 2*(1-cos(theta))/theta**2 * logMap(1)>0 so logMap(2) is sign(logMap(2),d12+d21)
-            !d13+d31=2*(1-cos(theta))/theta**2 * logMap(1)*logMap(3); 2*(1-cos(theta))/theta**2 * logMap(1)>0 so logMap(3) is sign(logMap(3),d13+d31)
-            
-            logMap(2) = sign( logMap(2), DCM(1,2)+DCM(2,1) )
-            logMap(3) = sign( logMap(3), DCM(1,3)+DCM(3,1) )            
+         logMap(1) = 1.0_ReKi + DCM(1,1) - DCM(2,2) - DCM(3,3);
+         logMap(2) = 1.0_ReKi - DCM(1,1) + DCM(2,2) - DCM(3,3);
+         logMap(3) = 1.0_ReKi - DCM(1,1) - DCM(2,2) + DCM(3,3);
+             
+         indx_max = maxloc( abs(logMap), 1 )
+             
+         divisor = sqrt(abs( logMap(indx_max) *  2.0_ReKi*(1.0_ReKi - cosTheta)  )) / theta  ! 2*(1-cosTheta)/theta^2 * abs(lambda(indx))
+         if (indx_max == 1) then
+           !logMap(1) = 1.0 + DCM(1,1) - DCM(2,2) - DCM(3,3)               ! 2*(1-cosTheta)/theta^2 * lambda(1) * lambda(1)
+            logMap(2) = DCM(1,2) + DCM(2,1)                                ! 2*(1-cosTheta)/theta^2 * lambda(1) * lambda(2)
+            logMap(3) = DCM(1,3) + DCM(3,1)                                ! 2*(1-cosTheta)/theta^2 * lambda(1) * lambda(3)
+         elseif (indx_max == 2) then
+            logMap(1) = DCM(1,2) + DCM(2,1)                                ! 2*(1-cosTheta)/theta^2 * lambda(2) * lambda(1)
+           !logMap(2) = 1.0 - DCM(1,1) + DCM(2,2) - DCM(3,3)               ! 2*(1-cosTheta)/theta^2 * lambda(2) * lambda(2)
+            logMap(3) = DCM(2,3) + DCM(3,2)                                ! 2*(1-cosTheta)/theta^2 * lambda(2) * lambda(3)
          else
-            ! because logMap1 is zero, we can choose logMap2 positive:
-            
-            !d23+d32=2*(1-cos(theta))/theta**2 * logMap(2)*logMap(3); 2*(1-cos(theta))/theta**2 * logMap(2)>0 so logMap(3) is sign(logMap(3),d23+d32)
-            logMap(3) = sign( logMap(3), DCM(2,3)+DCM(3,2) )            
-            
+            logMap(1) = DCM(1,3) + DCM(3,1)                                ! 2*(1-cosTheta)/theta^2 * lambda(3) * lambda(1)
+            logMap(2) = DCM(2,3) + DCM(3,2)                                ! 2*(1-cosTheta)/theta^2 * lambda(3) * lambda(2)
+           !logMap(3) = 1.0 - DCM(1,1) - DCM(2,2) + DCM(3,3)               ! 2*(1-cosTheta)/theta^2 * lambda(3) * lambda(3)
          end if
-                    
+         logMap = logMap / divisor                                         ! lambda * sign(lambda(indx_max))
+         
          ! at this point we may have the wrong sign for logMap (though if theta==pi, it doesn't matter because we can change it in the DCM_setLogMapforInterp() routines)
          ! we'll do a little checking to see if we should change the sign:
          
@@ -1250,10 +1255,7 @@ CONTAINS
          v(2) =  DCM(3,1) - DCM(1,3) ! skewSym(3,1)
          v(3) = -DCM(2,1) + DCM(1,2) !-skewSym(2,1)
  
-         indx_max = 1
-         do i=2,3
-            if ( abs(v(i)) > abs(v(indx_max)) ) indx_max = i
-         end do
+         indx_max = maxloc( abs(v), 1 )  ! find component with largest magnitude
          
          if ( .not. EqualRealNos( sign(1.0_ReKi,v(indx_max)), sign(1.0_ReKi,logMap(indx_max)) )) logMap = -logMap
          
