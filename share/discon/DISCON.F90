821115a841426597d6cff8aad745fc9ee9a1c945
Rafael M Mudafort <rafmudaf@gmail.com>
2018-02-20 18:15:03 -0700

    add license and fix dll export attribute warning

--- share/discon/DISCON.F90
+++ share/discon/DISCON.F90
@@ -1,7 +1,21 @@
-!=======================================================================
-!SUBROUTINE DISCON ( avrSWAP, from_SC, to_SC, aviFAIL, accINFILE, avcOUTNAME, avcMSG ) BIND (C, NAME='DISCON')
+!**********************************************************************************************************************************
+! LICENSING
+! Copyright (C) 2015-2016  National Renewable Energy Laboratory
+! Copyright (C) 2016-2017  Envision Energy USA, LTD
+!
+! Licensed under the Apache License, Version 2.0 (the "License");
+! you may not use this file except in compliance with the License.
+! You may obtain a copy of the License at
+!
+!     http://www.apache.org/licenses/LICENSE-2.0
+!
+! Unless required by applicable law or agreed to in writing, software
+! distributed under the License is distributed on an "AS IS" BASIS,
+! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+! See the License for the specific language governing permissions and
+! limitations under the License.
+!**********************************************************************************************************************************
 SUBROUTINE DISCON ( avrSWAP, aviFAIL, accINFILE, avcOUTNAME, avcMSG ) BIND (C, NAME='DISCON')
-!DEC$ ATTRIBUTES DLLEXPORT :: DISCON
 
    ! This Bladed-style DLL controller is used to implement a variable-speed
    ! generator-torque controller and PI collective blade pitch controller for
@@ -20,6 +34,7 @@ USE, INTRINSIC :: ISO_C_Binding
 
 IMPLICIT                        NONE
 #ifndef IMPLICIT_DLLEXPORT
+!DEC$ ATTRIBUTES DLLEXPORT :: DISCON
 !GCC$ ATTRIBUTES DLLEXPORT :: DISCON
 #endif
 
