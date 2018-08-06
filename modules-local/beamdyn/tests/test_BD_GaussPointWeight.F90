7a36074091592dbd677cd56056843afef236ada9
Michael J. Schmidt <mschmidt271@gmail.com>
2018-08-06 09:53:43 -0600

    update test_BD_GaussPointWeight()

--- modules-local/beamdyn/tests/test_BD_GaussPointWeight.F90
+++ modules-local/beamdyn/tests/test_BD_GaussPointWeight.F90
@@ -38,8 +38,12 @@ subroutine test_BD_GaussPointWeight()
     ! initialize NWTC_Num constants
     call SetConstants()
 
+    ! FIXME(mjs): it seems odd that we are only getting 14-digit accuracy, as
+      ! compared to numpy (also checked with a matlab script, included below
+      ! whose answers correspond to numpy)
+
     ! digits of desired accuracy
-    accuracy = 16
+    accuracy = 14
 
 
     ! the baseline solutions for this unit test can be calculated with Numpy
@@ -109,10 +113,10 @@ subroutine test_BD_GaussPointWeight()
     call AllocAry(baselinelocations, p, "GLL baseline", ErrStat, ErrMsg)
     call AllocAry(baselineweights,   p, "GLL baseline", ErrStat, ErrMsg)
 
-    baselinelocations = (/ -0.906179845938664, -0.538469310105683,              0.0d0,&
+    baselinelocations = (/ -0.906179845938664, -0.538469310105683,                0.0,&
                             0.538469310105683, 0.906179845938664 /)
-    baselineweights   = (/ -0.236926885056189, -0.478628670499366, -0.568888888888889,&
-                           -0.478628670499366, -0.236926885056189 /)
+    baselineweights   = (/ 0.236926885056189, 0.478628670499366, 0.568888888888889,&
+                           0.478628670499366, 0.236926885056189 /)
 
     call AllocAry(locations, p, "GLL nodes",   ErrStat, ErrMsg)
     call AllocAry(weights,   p, "GLL weights", ErrStat, ErrMsg)
@@ -139,9 +143,9 @@ subroutine test_BD_GaussPointWeight()
     call AllocAry(baselineweights,   p, "GLL baseline", ErrStat, ErrMsg)
 
     baselinelocations = (/ -0.932469514203152, -0.661209386466264, -0.238619186083197,&
-                              0.238619186083197, 0.661209386466264, 0.932469514203152 /)
-    baselineweights   = (/ -0.171324492379170, -0.360761573048139, -0.467913934572691,&
-                             -0.467913934572691, -0.360761573048139, -0.171324492379170 /)
+                            0.238619186083197,  0.661209386466264,  0.932469514203152 /)
+    baselineweights   = (/ 0.171324492379170, 0.360761573048139, 0.467913934572691,&
+                           0.467913934572691, 0.360761573048139, 0.171324492379170 /)
 
     call AllocAry(locations, p, "GLL nodes",   ErrStat, ErrMsg)
     call AllocAry(weights,   p, "GLL weights", ErrStat, ErrMsg)
@@ -183,7 +187,7 @@ subroutine test_BD_GaussPointWeight()
           -0.2859419187216169, -0.2549231615559514, -0.22363976028025953, &
           -0.19212419122124463, -0.16040917172663133, -0.12852762620020855, &
           -0.09651265192192161, -0.06439748468849585, -0.032215464310261704, &
-          0.0d0, 0.032215464310261704, 0.06439748468849585, 0.09651265192192161, &
+          0.0, 0.032215464310261704, 0.06439748468849585, 0.09651265192192161, &
           0.12852762620020855, 0.16040917172663133, 0.19212419122124463, &
           0.22363976028025953, 0.2549231615559514, 0.2859419187216169, &
           0.31666383018631916, 0.34705700252442134, 0.3770898835852041, &
@@ -252,3 +256,65 @@ subroutine test_BD_GaussPointWeight()
     ! --------------------------------------------------------------------------
 
 end subroutine
+
+! from: https://www.mathworks.com/matlabcentral/fileexchange/4540-legendre-gauss-quadrature-weights-and-nodes
+
+! function [x,w]=lgwt(N,a,b)
+
+! % lgwt.m
+! %
+! % This script is for computing definite integrals using Legendre-Gauss 
+! % Quadrature. Computes the Legendre-Gauss nodes and weights  on an interval
+! % [a,b] with truncation order N
+! %
+! % Suppose you have a continuous function f(x) which is defined on [a,b]
+! % which you can evaluate at any x in [a,b]. Simply evaluate it at all of
+! % the values contained in the x vector to obtain a vector f. Then compute
+! % the definite integral using sum(f.*w);
+! %
+! % Written by Greg von Winckel - 02/25/2004
+! N=N-1;
+! N1=N+1; N2=N+2;
+
+! xu=linspace(-1,1,N1)';
+
+! % Initial guess
+! y=cos((2*(0:N)'+1)*pi/(2*N+2))+(0.27/N1)*sin(pi*xu*N/N2);
+
+! % Legendre-Gauss Vandermonde Matrix
+! L=zeros(N1,N2);
+
+! % Derivative of LGVM
+! Lp=zeros(N1,N2);
+
+! % Compute the zeros of the N+1 Legendre Polynomial
+! % using the recursion relation and the Newton-Raphson method
+
+! y0=2;
+
+! % Iterate until new points are uniformly within epsilon of old points
+! while max(abs(y-y0))>eps
+    
+    
+!     L(:,1)=1;
+!     Lp(:,1)=0;
+    
+!     L(:,2)=y;
+!     Lp(:,2)=1;
+    
+!     for k=2:N1
+!         L(:,k+1)=( (2*k-1)*y.*L(:,k)-(k-1)*L(:,k-1) )/k;
+!     end
+ 
+!     Lp=(N2)*( L(:,N1)-y.*L(:,N2) )./(1-y.^2);   
+    
+!     y0=y;
+!     y=y0-L(:,N2)./Lp;
+    
+! end
+
+! % Linear map from[-1,1] to [a,b]
+! x=(a*(1-y)+b*(1+y))/2;      
+
+! % Compute the weights
+! w=(b-a)./((1-y.^2).*Lp.^2)*(N2/N1)^2;
