4cb2a01aae4b0da0204968bfa8179178e635e43b
Michael J. Schmidt <mschmidt271@gmail.com>
2018-08-06 09:54:30 -0600

    update test_BD_GyroForce()

--- modules-local/beamdyn/tests/test_BD_GyroForce.F90
+++ modules-local/beamdyn/tests/test_BD_GyroForce.F90
@@ -1,8 +1,22 @@
 @test
 subroutine test_BD_GyroForce()
     ! test branches
-    ! - inputs from bd_5MW_dynamic reg test--simple case
-    ! - inputs from bd_5MW_dynamic reg test--more complex case
+    ! - single quad pt/element--all zero inputs/outputs
+    ! - simulate 2 quad pts/elements to ensure proper indexing--all zero inputs/outputs
+    ! - single quad pt/element--integer-valued inputs
+    ! - simulate 2 quad pts/elements to ensure proper indexing--integer-valued inputs
+    ! - single quad pt/element--randomly-chosen real-valued inputs
+
+    ! --------------------------------------------------------------------------
+    ! --------------------------------------------------------------------------
+    ! In BD_GyroForce(), the element's gyroscopic forces (Fb) are computed for all
+    ! quadrature points, using rotational velocity [vvv(4 : 6)], the tensor of
+    ! inertia (in the inertial frame) [rho], and RR0mEta.
+    ! This test verifies that indexing occurs properly and that the calculations
+    ! are done properly for all zero inputs, integer-valued inputs, and
+    ! randomly-chosen real-valued inputs.
+    ! --------------------------------------------------------------------------
+    ! --------------------------------------------------------------------------
 
     use pFUnit_mod
     use BeamDyn
@@ -11,13 +25,13 @@ subroutine test_BD_GyroForce()
 
     implicit none
 
-    integer(IntKi)  :: nelem            !< index of current element in loop
-    integer(IntKi)  :: nqp              !< Number of quadrature points (per element)
-    real(BDKi)      :: vvv(6, 1, 1)     !< Translational velocity and rotational parameter velocity (at current QP)
-    real(BDKi)      :: RR0mEta(3, 1, 1) !< RR0 times Center of mass location times mass: (m*X_cm, m*Y_cm, m*Z_cm) where X_cm = 0
-    real(BDKi)      :: rho(3, 3, 1, 1)  !< Tensor of inertia resolved in inertia frame at quadrature point. 3x3
-    real(BDKi)      :: Fb(6, 1, 1)      !< Gyroscopic forces at current QP. 6
-    real(BDKi)      :: base_Fb(6)
+    integer(IntKi)          :: nelem
+    integer(IntKi)          :: nqp
+    real(BDKi), allocatable :: vvv(:, :, :)
+    real(BDKi), allocatable :: RR0mEta(:, :, :)
+    real(BDKi), allocatable :: rho(:, :, :, :)
+    real(BDKi), allocatable :: Fb(:, :, :)
+    real(BDKi)              :: base_Fb(6)
 
 
     integer(IntKi)  :: ErrStat ! Error status of the operation
@@ -33,48 +47,120 @@ subroutine test_BD_GyroForce()
     ! digits of desired accuracy
     accuracy = 16
 
+
+    ! --------------------------------------------------------------------------
+    testname = "single quad pt/element--all zero inputs/outputs:"
+
     nelem = 1
     nqp   = 1
 
-
-    ! --------------------------------------------------------------------------
-    testname = "inputs from bd_5MW_dynamic reg test--simple case:"
+    allocate(vvv(6, nqp, nelem), RR0mEta(3, nqp, nelem), rho(3, 3, nqp, nelem), Fb(6, nqp, nelem))
 
     call initialize_vars_base()
-
-    vvv(2, 1, nelem)    = -1.0005999999999999
-    vvv(4, 1, nelem)    = 1.0005999999999999
-
-    rho(1, :, 1, nelem) = (/ 973.03046262476562, -4.0320788880971827E-002, 0.0000000000000000 /)
-    rho(2, :, 1, nelem) = (/ -4.0320788880975254E-002, 972.86953737523640, 0.0000000000000000 /)
-    rho(3, :, 1, nelem) = (/ 0.0000000000000000, 0.0000000000000000, 1945.9000000000019 /)
-
-    base_Fb(6) = -4.0369188343116418E-002
 
     call BD_GyroForce( nelem, nqp, vvv, RR0mEta, rho, Fb )
 
     tolerance = AdjustTol(accuracy, base_Fb)
-    @assertEqual(base_Fb, Fb(:, 1, nelem), tolerance, testname)
+    @assertEqual(base_Fb, Fb(:, nqp, nelem), tolerance, testname)
+
+    deallocate(vvv, RR0mEta, rho, Fb)
 
     ! --------------------------------------------------------------------------
-    testname = "inputs from bd_5MW_dynamic reg test--more complex case:"
+    testname = "simulate 2 quad pts/elements to ensure proper indexing--all zero inputs/outputs:"
+
+    nelem = 2
+    nqp   = 2
+
+    allocate(vvv(6, nqp, nelem), RR0mEta(3, nqp, nelem), rho(3, 3, nqp, nelem), Fb(6, nqp, nelem))
 
     call initialize_vars_base()
-
-    vvv(:, 1, nelem)    = (/ 4.8634616692904533, -79.210164482472834, -5.9402757208341166,&
-                             1.2485338548481626, 0.32267175947647336, -0.14961683098643255 /)
-
-    rho(1, :, 1, nelem) = (/ 0.67999999212185536, 7.9564004709822399E-004, 1.8716266598721497E-004 /)
-    rho(2, :, 1, nelem) = (/ 7.9564004709822399E-004, 2.2394786809757369E-002, -4.0274704407683264E-002 /)
-    rho(3, :, 1, nelem) = (/ 1.8716266598721502E-004, -4.0274704407683264E-002, 0.69760522106838729 /)
-
-    base_Fb = (/ 0.0000000000000000, 0.0000000000000000, 0.0000000000000000,&
-                -3.5664917627997797E-002, 1.9188042594539323E-002, -0.25623733219903078 /)
 
     call BD_GyroForce( nelem, nqp, vvv, RR0mEta, rho, Fb )
 
     tolerance = AdjustTol(accuracy, base_Fb)
-    @assertEqual(base_Fb, Fb(:, 1, nelem), tolerance, testname)
+    @assertEqual(base_Fb, Fb(:, nqp, nelem), tolerance, testname)
+
+    deallocate(vvv, RR0mEta, rho, Fb)
+
+    ! --------------------------------------------------------------------------
+    testname = "single quad pt/element--integer-valued inputs:"
+
+    nelem = 1
+    nqp   = 1
+
+    allocate(vvv(6, nqp, nelem), RR0mEta(3, nqp, nelem), rho(3, 3, nqp, nelem), Fb(6, nqp, nelem))
+
+    call initialize_vars_base()
+
+    vvv(:, nqp, nelem)     = (/ 1.0d0,  2.0d0,  3.0d0,  4.0d0,  5.0d0,  6.0d0 /)
+    RR0mEta(:, nqp, nelem) = (/ 1.0d0,  2.0d0,  3.0d0 /)
+
+    rho(1, :, nqp, nelem)  = (/ 1.0d0,  2.0d0,  3.0d0 /)
+    rho(2, :, nqp, nelem)  = (/ 4.0d0,  5.0d0,  6.0d0 /)
+    rho(3, :, nqp, nelem)  = (/ 7.0d0,  8.0d0,  9.0d0 /)
+
+    base_Fb = (/ 51.0d0, 6.0d0, -39.0d0, 148.0d0, -296.0d0, 148.0d0 /)
+
+    call BD_GyroForce( nelem, nqp, vvv, RR0mEta, rho, Fb )
+
+    tolerance = AdjustTol(accuracy, base_Fb)
+    @assertEqual(base_Fb, Fb(:, nqp, nelem), tolerance, testname)
+
+    deallocate(vvv, RR0mEta, rho, Fb)
+
+    ! --------------------------------------------------------------------------
+    testname = "simulate 2 quad pts/elements to ensure proper indexing--integer-valued inputs:"
+
+    nelem = 2
+    nqp   = 2
+
+    allocate(vvv(6, nqp, nelem), RR0mEta(3, nqp, nelem), rho(3, 3, nqp, nelem), Fb(6, nqp, nelem))
+
+    call initialize_vars_base()
+
+    vvv(:, nqp, nelem)     = (/ 1.0d0,  2.0d0,  3.0d0,  4.0d0,  5.0d0,  6.0d0 /)
+    RR0mEta(:, nqp, nelem) = (/ 1.0d0,  2.0d0,  3.0d0 /)
+
+    rho(1, :, nqp, nelem)  = (/ 1.0d0,  2.0d0,  3.0d0 /)
+    rho(2, :, nqp, nelem)  = (/ 4.0d0,  5.0d0,  6.0d0 /)
+    rho(3, :, nqp, nelem)  = (/ 7.0d0,  8.0d0,  9.0d0 /)
+
+    base_Fb = (/ 51.0d0, 6.0d0, -39.0d0, 148.0d0, -296.0d0, 148.0d0 /)
+
+    call BD_GyroForce( nelem, nqp, vvv, RR0mEta, rho, Fb )
+
+    tolerance = AdjustTol(accuracy, base_Fb)
+    @assertEqual(base_Fb, Fb(:, nqp, nelem), tolerance, testname)
+
+    deallocate(vvv, RR0mEta, rho, Fb)
+
+    ! --------------------------------------------------------------------------
+    testname = "single quad pt/element--randomly-chosen real-valued inputs:"
+
+    nelem = 1
+    nqp   = 1
+
+    allocate(vvv(6, nqp, nelem), RR0mEta(3, nqp, nelem), rho(3, 3, nqp, nelem), Fb(6, nqp, nelem))
+
+    call initialize_vars_base()
+
+    vvv(:, nqp, nelem)     = (/ 0.944310599276061, -7.227511143426417, -7.014119888818851,&
+                               -4.849834917525271,  6.814345119673252, -4.914356420569379 /)
+    RR0mEta(:, nqp, nelem) = (/ 6.285696521376327, -5.129500625500214,  8.585272463744555 /)
+
+    rho(1, :, nqp, nelem)  = (/ -3.000324680303825,  2.320893522932783, 6.616572557925817 /)
+    rho(2, :, nqp, nelem)  = (/ -6.068094991375837, -0.534223021945415, 1.705281823054484 /)
+    rho(3, :, nqp, nelem)  = (/ -4.978322840479379, -2.966809858740065, 0.994472165822790 /)
+
+    base_Fb = (/ -0.695415667344595E02, -2.507046443201556E02, -2.790035425992599E02,&
+                  0.790097936361589E02,  5.908718256043477, -0.697793121305159E02 /)
+
+    call BD_GyroForce( nelem, nqp, vvv, RR0mEta, rho, Fb )
+
+    tolerance = AdjustTol(accuracy, base_Fb)
+    @assertEqual(base_Fb, Fb(:, nqp, nelem), tolerance, testname)
+
+    deallocate(vvv, RR0mEta, rho, Fb)
 
     ! --------------------------------------------------------------------------
 
