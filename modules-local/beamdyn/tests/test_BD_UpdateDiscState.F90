d8b6786b4cbd6e05821de36408de9ba3ee2078b2
Michael J. Schmidt <mschmidt271@gmail.com>
2018-08-09 09:24:23 -0600

    update test_BD_UpdateDiscState()

--- modules-local/beamdyn/tests/test_BD_UpdateDiscState.F90
+++ modules-local/beamdyn/tests/test_BD_UpdateDiscState.F90
@@ -1,9 +1,22 @@
 @test
 subroutine test_BD_UpdateDiscState()
     ! test branches
-    ! - trivial case--UsePitchAct == false => discrete states should not change:
-      ! mjs--FIXME: I don't have a test case with UsePitchAct == true, so I don't
-                    ! have test values
+    ! - trivial case--UsePitchAct == false => discrete states should not change
+    ! - UsePitchAct == true--all zero inputs/outputs (except pitchJ, to avoid division by zero):"
+    ! - UsePitchAct == true--integer-valued inputs with identity rotations
+    ! - UsePitchAct == true--randomly-chosen real-valued inputs
+
+    ! --------------------------------------------------------------------------
+    ! --------------------------------------------------------------------------
+    ! In BD_UpdateDiscState(), the pitch angle state (xd%thetaP) and pitch rate
+    ! state (xd%thetaPD) are updated, using pitch actuator stiffness and inertia
+    ! (pitchK and pitchJ), the pitch actuator matrix (torqM), and the time step.
+    ! This test verifies that when UsePitchAct == false, nothing happens, and 
+    ! when UsePitchAct == true, it  verifies that the calculations are done
+    ! properly for all zero inputs, integer-valued inputs, and randomly-chosen
+    ! real-valued inputs.
+    ! --------------------------------------------------------------------------
+    ! --------------------------------------------------------------------------
 
     use pFUnit_mod
     use BeamDyn
@@ -21,8 +34,7 @@ subroutine test_BD_UpdateDiscState()
     real(BDKi)                 :: pitchJ
     type(BD_DiscreteStateType) :: xd
     real(BDKi)                 :: base_thetaP, base_thetaPD
-
-
+    real(BDKi)                 :: axis(3), angle
 
     integer(IntKi)  :: ErrStat ! Error status of the operation
     character(1024) :: ErrMsg  ! Error message if ErrStat /= ErrID_None
@@ -51,6 +63,88 @@ subroutine test_BD_UpdateDiscState()
     base_thetaPD = xd%thetaPD
 
     call BD_UpdateDiscState( RM_Orientation, HM_Orientation, UsePitchAct, torqM, pitchK, dt, pitchJ, xd )
+
+    tolerance = AdjustTol(accuracy, base_thetaP)
+    @assertEqual(base_thetaP, xd%thetaP, tolerance, testname)
+    tolerance = AdjustTol(accuracy, base_thetaPD)
+    @assertEqual(base_thetaPD, xd%thetaPD, tolerance, testname)
+
+    ! --------------------------------------------------------------------------
+    testname = "UsePitchAct == true--all zero inputs/outputs (except pitchJ, to avoid division by zero):"
+
+    call initialize_vars_base()
+
+    UsePitchAct = .true.
+
+    pitchJ         = 1.0d0
+
+    call BD_UpdateDiscState( RM_Orientation, HM_Orientation, UsePitchAct, torqM, pitchK, dt, pitchJ, xd )
+
+    tolerance = AdjustTol(accuracy, base_thetaP)
+    @assertEqual(base_thetaP, xd%thetaP, tolerance, testname)
+    tolerance = AdjustTol(accuracy, base_thetaPD)
+    @assertEqual(base_thetaPD, xd%thetaPD, tolerance, testname)
+
+    ! --------------------------------------------------------------------------
+    testname = "UsePitchAct == true--integer-valued inputs with identity rotations:"
+
+    call initialize_vars_base()
+
+    UsePitchAct = .true.
+
+    dt             = 2.0d0
+    pitchK         = 2.0d0
+    pitchJ         = 2.0d0
+
+    xd%thetaP      = 2.0d0
+    xd%thetaPD     = 2.0d0
+
+    RM_Orientation(:, :, 1) = identity()
+    HM_Orientation(:, :, 1) = identity()
+
+    torqM(1, :)    = (/ 1.0d0, 2.0d0 /)
+    torqM(2, :)    = (/ 3.0d0, 4.0d0 /)
+
+    base_thetaP    = 6.0d0
+    base_thetaPD   = 26.0d0
+
+    call BD_UpdateDiscState( RM_Orientation, HM_Orientation, UsePitchAct, torqM, pitchK, dt, pitchJ, xd )
+
+    tolerance = AdjustTol(accuracy, base_thetaP)
+    @assertEqual(base_thetaP, xd%thetaP, tolerance, testname)
+    tolerance = AdjustTol(accuracy, base_thetaPD)
+    @assertEqual(base_thetaPD, xd%thetaPD, tolerance, testname)
+
+    ! --------------------------------------------------------------------------
+    testname = "UsePitchAct == true--randomly-chosen real-valued inputs:"
+
+    call initialize_vars_base()
+
+    UsePitchAct = .true.
+
+    dt             = 0.638530758271838
+    pitchK         = 6.544457077570663
+    pitchJ         = 4.076191970411526
+
+    xd%thetaP      = 8.199812227819406
+    xd%thetaPD     = 7.183589432058836
+
+    axis  = (/ 0.147241215856231, -0.686963543539850, 0.711618657850082 /)
+    angle = -5.099064511970266
+    RM_Orientation(:, :, 1) = transpose(calcRotationMatrix(angle, axis))
+
+    axis  = (/ 0.690439500942647, 0.577880389111208, -0.435140840899527 /)
+    angle = 2.511864803506318
+    HM_Orientation(:, :, 1) = transpose(calcRotationMatrix(angle, axis))
+
+    torqM(1, :)    = (/ 6.109586587462006, 7.788022418240925 /)
+    torqM(2, :)    = (/ 4.234529189627382, 0.908232857874395 /)
+
+    base_thetaP    = 123.78439241760138
+    base_thetaPD   = 532.76193287943408
+
+    call BD_UpdateDiscState( RM_Orientation, HM_Orientation, UsePitchAct, torqM, pitchK, dt, pitchJ, xd )
+
 
     tolerance = AdjustTol(accuracy, base_thetaP)
     @assertEqual(base_thetaP, xd%thetaP, tolerance, testname)
