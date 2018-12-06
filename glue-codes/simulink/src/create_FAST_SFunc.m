3e929d8da127cbf33cd2e101055e54f318c297d2
Nikhar Abbas <nikhar.abbas@colorado.edu>
2018-12-05 18:26:12 -0700

    Clean up the sfunc creation function

--- glue-codes/simulink/src/create_FAST_SFunc.m
+++ glue-codes/simulink/src/create_FAST_SFunc.m
@@ -6,35 +6,34 @@ mexname = 'FAST_SFunc';
 switch computer('arch')
     case 'win64'
         % this is set up for files generated using the x64 configuration of vs-build
-        binDir = '../../../build/bin';
-        libname = 'OpenFAST-Simulink_x64';
-        outDir = binDir;
+        libDir = '../../../build/bin';
+        includeDir = '/usr/local/include';
+        libName = 'OpenFAST-Simulink_Win32';
+        outDir = libDir;
         
     case 'win32'
         % this is set up for files generated using the x86 configuration of vs-build
-        binDir = '../../../build/bin';
-        libname = 'OpenFAST-Simulink_Win32';
-        outDir = binDir;
+        libDir = '../../../build/bin';
+        includeDir = '/usr/local/include';
+        libName = 'OpenFAST-Simulink_Win32';
+        outDir = libDir;
         
     case 'maci64'
-        binDir = '/usr/local/lib';
-        libname = 'openfastlib';
+        libDir = '/usr/local/lib';
+        includeDir = '/usr/local/include';
+        libName = 'openfastlib';
         outDir = '.';
-        
-%         mex -L/usr/local/lib -lopenfastlib ...    
-%             -I/usr/local/include -outdir . COMPFLAGS='$COMPFLAGS -MT' FAST_SFunc.c 
-            % This builds the mex file in the current directory, ./openfast/glue-codes/simulink/src/
+
     otherwise
         error('Unexpected computer architecture type.')
 end
 
-    mex( '-largeArrayDims', ['-L' binDir], ['-l' libname] ,...
-        '-I../../../modules-local/openfast-library/src', '-I../../../build/types-files', ['-I' binDir], '-outdir', outDir, ...
-        ['COMPFLAGS=$COMPFLAGS /MT /D S_FUNCTION_NAME=' mexname], '-output', mexname, ...
-        'FAST_SFunc.c');
-
-
-%%
- %addpath('C:\Users\bjonkman\Documents\CAETools\FASTv8\bin');
- %addpath('C:\Users\bjonkman\Documents\CAETools\FASTv8\Simulink\Samples');
-% use mex -setup to configure a C compiler
+mex('-largeArrayDims', ...
+    ['-L' libDir], ...
+    ['-l' libName], ...
+    ['-I' includeDir], ...
+    '-outdir', outDir, ...
+    'COMPFLAGS=$COMPFLAGS -MT -D', ...
+    ['S_FUNCTION_NAME=' mexname], ...
+    '-output', mexname, ...
+    'FAST_SFunc.c');
