7d48c9c65f31773e6f41229731a8a1add217a053
Rafael M Mudafort <rafmudaf@gmail.com>
2019-09-24 10:20:14 -0600

    Update the README and add GA tracking

--- docs/conf.py
+++ docs/conf.py
@@ -159,7 +159,9 @@ html_logo = '_static/openfastlogo.jpg'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    "analytics_id": "UA-68999653-10"
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
