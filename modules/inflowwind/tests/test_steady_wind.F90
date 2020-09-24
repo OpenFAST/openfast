0de06aa9920dad90a3b933b93adc0411a060e9a7
Jake Nunemaker <jake.d.nunemaker@gmail.com>
2020-09-24 17:39:07 -0600

    Initialize InflowWind with strings or input file

---
+++ modules/inflowwind/tests/test_steady_wind.F90
@@ -0,0 +1,63 @@
+module test_steady_wind
+
+    use pFUnit_mod
+    use ifw_test_tools
+    use InflowWind_Subs
+    use InflowWind_Types
+
+    implicit none
+
+contains
+
+    @test
+    subroutine test_steady_wind_input_single_height()
+
+        TYPE(FileInfoType)              :: InFileInfo
+        TYPE(InflowWind_InputFile)      :: InputFileData
+        CHARACTER(1024)                 :: PriPath 
+        INTEGER(IntKi)                  :: TmpErrStat
+        CHARACTER(ErrMsgLen)            :: TmpErrMsg
+
+        PriPath = ""
+
+        InFileInfo = getInputFileData()
+        CALL InflowWind_ParseInputFileInfo(InputFileData , InFileInfo, PriPath, TmpErrStat, TmpErrMsg)
+
+        @assertEqual(TmpErrStat, 0)
+        @assertEqual(InputFileData%WindType, 1)
+        @assertEqual(InputFileData%NWindVel, 1)
+        @assertEqual(InputFileData%WindVziList(1), 90)
+
+    end subroutine
+
+
+    @test
+    subroutine test_steady_wind_input_mult_heights()
+
+        TYPE(FileInfoType)              :: InFileInfo
+        TYPE(InflowWind_InputFile)      :: InputFileData 
+        CHARACTER(1024)                 :: PriPath
+        INTEGER(IntKi)                  :: TmpErrStat
+        CHARACTER(ErrMsgLen)            :: TmpErrMsg
+
+        PriPath = ""
+
+        InFileInfo = getInputFileData()
+        InFileInfo%Lines(7:10) = (/ &
+            '          2   NWindVel       - Number of points to output the wind velocity    (0 to 9)                                                                        ', &
+            '        0,0   WindVxiList    - List of coordinates in the inertial X direction (m)                                                                             ', &
+            '        0,0   WindVyiList    - List of coordinates in the inertial Y direction (m)                                                                             ', &
+            '     80,100   WindVziList    - List of coordinates in the inertial Z direction (m)                                                                             ' &
+        /)
+
+        CALL InflowWind_ParseInputFileInfo(InputFileData , InFileInfo, PriPath, TmpErrStat, TmpErrMsg)
+
+        @assertEqual(TmpErrStat, 0)
+        @assertEqual(InputFileData%WindType, 1)
+        @assertEqual(InputFileData%NWindVel, 2)
+        @assertEqual(InputFileData%WindVziList(1), 80)
+        @assertEqual(InputFileData%WindVziList(2), 100)
+
+    end subroutine
+
+end module
