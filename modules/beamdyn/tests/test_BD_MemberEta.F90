20c93abedd009e2c0af4f15a7ccc2e7c01247ea6
Mike Sprague <michael.a.sprague@nrel.gov>
2020-10-09 12:12:40 -0600

    adding more comments to test_BD_MemberEta.F90

--- modules/beamdyn/tests/test_BD_MemberEta.F90
+++ modules/beamdyn/tests/test_BD_MemberEta.F90
@@ -47,6 +47,8 @@ contains
         ! fz[u_] = 5 u^2 - u^4;
         ! 
         ! exact (to mp) length is 5.627175237247959
+        ! The Jacobian values below are calculate based on a 5-node Legendre Spectral Element
+        ! While we give baseline Jacobian here, we check "test_BD_InitShpDerJaco.F90" elsewhere
 
         nqp = 5 ! number of quadrature points
         member_total = 1 ! 1 element
@@ -62,11 +64,14 @@ contains
         ! assign baseline jacobian based; these values were calculated in separate mathematica script
         baseline_jac(1:nqp,1) = (/ 0.6715870058501458, 1.509599209717604, 2.861380785564901, 4.097191592895223, 4.880926263217582 /)
 
-        ! total length of beam calculated in mathematica
+        ! total length of beam calculated in mathematica; note that for this curved beam, GL quadrature is APPROXIMATE, but
+        !   converged rapidly; TR quadrature is not nearly as good; commented-out values are useful for understanding quadrature performance
+        !   for curved beams.
         !baseline_total_length = 5.627175237247959   ! this is actual length based on mathematica
+        !baseline_total_length = 5.634413547964786   ! this is approximation with 9-point Trapezoidal quadrature; 0.13% error
         !baseline_total_length = 5.627202424388781   ! this is approximation with 7-point gauss quadrature; 0.0005% error
-        baseline_total_length = 5.626918236484061    ! this is approximation with 5-point gauss quadrature; 0.005% error
-        baseline_member_eta(1) = 1.                  ! just one element
+        baseline_total_length = 5.626918236484061    ! this is approximation with 5-point gauss quadrature; 0.005% error  (tested here)
+        baseline_member_eta(1) = 1.                  ! just one element; so member_length / total_length = 1
 
         call BD_MemberEta(member_total, baseline_QPtW, baseline_jac, test_member_eta, test_total_length)
 
