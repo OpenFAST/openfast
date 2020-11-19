6f78f79f262917683cdb9ca4be6930748667a1bc
Rafael M Mudafort <rafmudaf@gmail.com>
2020-11-19 15:00:27 -0600

    Initialize InflowWind with strings or input file (#596)
    Co-authored-by: Jake Nunemaker <jake.d.nunemaker@gmail.com>

---
+++ modules/inflowwind/tests/test_outputs.F90
@@ -0,0 +1,62 @@
+module test_outputs
+
+    use pFUnit_mod
+    use ifw_test_tools
+    use InflowWind_Subs
+    use InflowWind_Types
+
+    implicit none
+
+contains
+
+    @test
+    subroutine test_outputs_parsing()
+
+        TYPE(FileInfoType)              :: InFileInfo
+        TYPE(InflowWind_InputFile)      :: InputFileData
+        CHARACTER(1024)                 :: PriPath 
+        INTEGER(IntKi)                  :: TmpErrStat
+        CHARACTER(ErrMsgLen)            :: TmpErrMsg
+
+        PriPath = ""
+
+        InFileInfo = getInputFileData()
+        CALL InflowWind_ParseInputFileInfo(InputFileData , InFileInfo, PriPath, TmpErrStat, TmpErrMsg)
+
+        @assertEqual(TmpErrStat, 0)
+        @assertEqual(InputFileData%SumPrint, .FALSE.)
+        @assertEqual(InputFileData%OutList(1), "Wind1VelX")
+        @assertEqual(InputFileData%OutList(2), "Wind1VelY")
+        @assertEqual(InputFileData%OutList(3), "Wind1VelZ")
+
+    end subroutine
+
+
+    @test
+    subroutine test_outputs_parsing_alternate()
+
+        TYPE(FileInfoType)              :: InFileInfo
+        TYPE(InflowWind_InputFile)      :: InputFileData 
+        CHARACTER(1024)                 :: PriPath
+        INTEGER(IntKi)                  :: TmpErrStat
+        CHARACTER(ErrMsgLen)            :: TmpErrMsg
+
+        PriPath = ""
+
+        InFileInfo = getInputFileData()
+        InFileInfo%Lines(50:52) = (/ &
+            'True          SumPrint     - Print summary data to <RootName>.IfW.sum (flag)                                                                                   ', &
+            '              OutList      - The next line(s) contains a list of output parameters.  See OutListParameters.xlsx for a listing of available output channels, (-)', &
+            '"Wind1VelX,Wind1VelY"      - Wind velocity at point WindVxiList(1),WindVyiList(1),WindVziList(1).  X, Y, and Z direction components.                           ' &
+        /)
+
+        CALL InflowWind_ParseInputFileInfo(InputFileData , InFileInfo, PriPath, TmpErrStat, TmpErrMsg)
+
+        @assertEqual(TmpErrStat, 0)
+        @assertEqual(InputFileData%SumPrint, .TRUE.)
+        @assertEqual(InputFileData%OutList(1), "Wind1VelX")
+        @assertEqual(InputFileData%OutList(2), "Wind1VelY")
+
+    end subroutine
+
+end module
