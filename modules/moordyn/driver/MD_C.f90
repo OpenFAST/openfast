269f6e48fb6aa70b919f1f41f2dfdeeec0d95056
andrew-platt <andy.platt@nrel.gov>
2021-04-19 16:51:57 -0600

    MD_C wrap: add waves input for init, and a few other parameters

--- modules/moordyn/driver/MD_C.f90
+++ modules/moordyn/driver/MD_C.f90
@@ -48,7 +48,11 @@ CONTAINS
 !===============================================================================================================
 !---------------------------------------------- MD INIT --------------------------------------------------------
 !===============================================================================================================
+!FIXME: add g_C, rhoW_C, WtrDepth_C, and PtfmInit_C to the interface  -- make PtfmInit_C an array size 6
 SUBROUTINE MD_INIT_C(MD_InputFileName_C, InputFileNameLength_C, DT_C, NumChannels_C, OutputChannelNames_C, OutputChannelUnits_C, ErrStat_C, ErrMsg_C) BIND (C, NAME='MD_INIT_C')
+
+   !TEMPORARY hack until Waves handling is finalized
+   USE WAVES, only: WaveGrid_n, WaveGrid_x0, WaveGrid_dx, WaveGrid_nx, WaveGrid_y0, WaveGrid_dy, WaveGrid_ny, WaveGrid_nz
 
     INTEGER(C_INT)                                 , INTENT(IN   )   :: InputFileNameLength_C
     CHARACTER(KIND=C_CHAR)                         , INTENT(IN   )   :: MD_InputFileName_C(InputFileNameLength_C)
@@ -65,21 +69,21 @@ SUBROUTINE MD_INIT_C(MD_InputFileName_C, InputFileNameLength_C, DT_C, NumChannel
     CHARACTER(CHANLEN+1), ALLOCATABLE, TARGET                        :: tmp_OutputChannelNames_C(:)
     CHARACTER(CHANLEN+1), ALLOCATABLE, TARGET                        :: tmp_OutputChannelUnits_C(:)
     REAL(DbKi)                                                       :: DTcoupling
-    INTEGER                                                          :: ErrStat
+    INTEGER(IntKi)                                                   :: ErrStat
     CHARACTER(ErrMsgLen)                                             :: ErrMsg
     INTEGER                                                          :: I
 
-    ! Additional Inputs
-    REAL(ReKi)                                                       :: g = -9.806       !< gravity constant [[m/s^2]]
-    REAL(ReKi)                                                       :: rhoW = -1000.    !< sea density [[kg/m^3]]
-    REAL(ReKi)                                                       :: WtrDepth = -100. !< depth of water [[m]]
-    REAL(ReKi) , DIMENSION(:), ALLOCATABLE                           :: PtfmInit         !< initial position of platform(s) originally size 6 [-]
-    REAL(ReKi) , DIMENSION(:,:,:), ALLOCATABLE                       :: WaveVel          !<  [-]
-    REAL(ReKi) , DIMENSION(:,:,:), ALLOCATABLE                       :: WaveAcc          !<  [-]
-    REAL(ReKi) , DIMENSION(:,:), ALLOCATABLE                         :: WavePDyn         !<  [-]
-    REAL(ReKi) , DIMENSION(:,:), ALLOCATABLE                         :: WaveElev         !<  [-]
-    REAL(DbKi) , DIMENSION(:), ALLOCATABLE                           :: WaveTime         !<  [-]
-    
+    integer(IntKi)                                                   :: ErrStat2       ! temporary error status of the operation
+    character(ErrMsgLen)                                             :: ErrMsg2        ! temporary error message
+
+    ! NOTE: Wave info will be handled differently in the future.  So the following is a temporary hack until that is finalized
+    ! Hard coded for 10 wave steps.  Doesn't actually matter since it will get zeroed
+    integer(IntKi)                                                   :: NStepWave = 10
+
+    ! Initialize ErrStat
+    ErrStat = ErrID_None
+    ErrMsg  = ""
+
     ! Convert the MD input filename from C to Fortran
     MD_InputFileName = TRANSFER(MD_InputFileName_C, MD_InputFileName)
     I = INDEX(MD_InputFileName,C_NULL_CHAR) - 1            ! if this has a c null character at the end...
@@ -87,29 +91,41 @@ SUBROUTINE MD_INIT_C(MD_InputFileName_C, InputFileNameLength_C, DT_C, NumChannel
     PRINT *, 'Inside MD_INIT_C: the passed input filename is ', MD_InputFileName
 
     ! Set other inputs for calling MD_Init
-    ALLOCATE(PtfmInit(6))
-    ALLOCATE(WaveVel())
-    ALLOCATE(WaveAcc())
-    ALLOCATE(WavePDyn())
-    ALLOCATE(WaveElev())
-    ALLOCATE(WaveTime())
-
     DTcoupling               = REAL(DT_C, DbKi)
     InitInp%FileName         = MD_InputFileName
     InitInp%RootName         = 'MDroot'
-    InitInp%g                = g
-    InitInp%rhoW             = rhoW
-    InitInp%WtrDepth         = WtrDepth
-    InitInp%PtfmInit         = PtfmInit
-    InitInp%WaveVel          = WaveVel
-    InitInp%WaveAcc          = WaveAcc
-    InitInp%WavePDyn         = WavePDyn
-    InitInp%WaveElev         = WaveElev
-    InitInp%WaveTime         = WaveTime
+
+    ! Environment variables -- These should be passed in from C.
+    InitInp%g                = -9.806        ! Set this from a value passed in from C   -- check with Matt Hall on sign
+    InitInp%rhoW             = 1000.0_ReKi   ! Set this from a value passed in from C
+    InitInp%WtrDepth         = -100.0_ReKi   ! Set this from a value passed in from C   -- check with Matt Hall on sign
+
+    ! Platform position (x,y,z,Rx,Ry,Rz) where rotations are small angle assumption in radians.
+    ! This data is used to set the CoupledKinematics mesh that will be used at each timestep call
+    call AllocAry (InitInp%PtfmInit, 6, 'InitInp%PtfmInit', ErrStat2, ErrMsg2 ); if (Failed()) return
+    InitInp%PtfmInit         = (/ 0.0_ReKi, 0.0_ReKi, 0.0_ReKi, 0.0_ReKi, 0.0_ReKi, 0.0_ReKi /)
+
+    ! THIS IS A SHORT TERM HACK
+    ! Fake wave info -- completely still, with no dynamic pressure terms
+    ! Set wave info to zeros -- assume 10 timesteps for now (doesn't really matter since it isn't getting used)
+    call AllocAry ( InitInp%WaveVel  ,NStepWave, WaveGrid_n, 3, 'InitInp%WaveVel' , ErrStat2, ErrMsg2 );    if (Failed()) return
+    call AllocAry ( InitInp%WaveAcc  ,NStepWave, WaveGrid_n, 3, 'InitInp%WaveAcc' , ErrStat2, ErrMsg2 );    if (Failed()) return
+    call AllocAry ( InitInp%WavePDyn ,NStepWave, WaveGrid_n,    'InitInp%WavePDyn', ErrStat2, ErrMsg2 );    if (Failed()) return
+    call AllocAry ( InitInp%WaveElev ,NStepWave, WaveGrid_n,    'InitInp%WaveElev', ErrStat2, ErrMsg2 );    if (Failed()) return
+    call AllocAry ( InitInp%WaveTime ,NStepWave,                'InitInp%WaveTime', ErrStat2, ErrMsg2 );    if (Failed()) return
+    do i=1,NStepWave
+       InitInp%WaveTime(i) = DTcoupling * REAL(i-1, DbKi)
+    enddo
+    InitInp%WaveVel          = 0.0_ReKi
+    InitInp%WaveAcc          = 0.0_ReKi
+    InitInp%WavePDyn         = 0.0_ReKi
+    InitInp%WaveElev         = 0.0_ReKi
+
 
     ! Call the main subroutine MD_Init
     CALL MD_Init(InitInp, u, p, x, xd, z, other, y, m, DTcoupling, InitOutData, ErrStat, ErrMsg)
-    IF (ErrStat .NE. 0) THEN 
+!FIXME: this may catch messages labelled as Info as fatal errors.  You probably don't want that.
+    IF (ErrStat /= ErrID_None) THEN
         PRINT *, "MD_INIT_C: Main MD_Init subroutine failed!"
         PRINT *, ErrMsg
      ELSE
@@ -137,6 +153,19 @@ SUBROUTINE MD_INIT_C(MD_InputFileName_C, InputFileNameLength_C, DT_C, NumChannel
      ErrMsg_C = TRANSFER( ErrMsg//C_NULL_CHAR, ErrMsg_C )
 
     PRINT*, "DONE WITH MD_INIT_C!"
+
+contains
+
+   subroutine Cleanup()
+      ErrMsg_C = TRANSFER( ErrMsg//C_NULL_CHAR, ErrMsg_C )
+      ErrStat_C = ErrStat
+   end subroutine Cleanup
+
+   logical function Failed()
+      call SetErrStat(ErrStat2, ErrMsg2, ErrStat, ErrMsg, 'MD_Init_C')
+      Failed =  ErrStat >= AbortErrLev
+      if (Failed) call CleanUp()
+   end function Failed
 
 END SUBROUTINE MD_INIT_C
 
