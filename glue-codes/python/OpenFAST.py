262c1b79b7d54b3e9ce497759e5fd2d690bdc3b6
Rafael M Mudafort <rafmudaf@gmail.com>
2021-05-05 17:10:17 -0400

    Python driver clean up
    # Conflicts:
    #	glue-codes/python/OpenFAST.py

--- glue-codes/python/OpenFAST.py
+++ glue-codes/python/OpenFAST.py
@@ -1,48 +1,49 @@
 
-from ctypes import (
-    byref,
-    create_string_buffer,
-    pointer,
-    c_int,
-    c_double,
-    c_char,
-    c_bool
-)
 import openfast_library
 
 project_root = '/Users/rmudafor/Development/weis'
 library_path = project_root + '/build/modules/openfast-library/libopenfastlib.dylib'
+library_path = '/Users/rmudafor/Downloads/libopenfastlib.dylib'
 
-# ## Serial
-# input_file_name = "/Users/rmudafor/Development/weis/reg_tests/r-test/glue-codes/openfast/AOC_YFix_WSt/AOC_YFix_WSt.fst"
-# openfastlib = openfast_library.FastLibAPI(library_path, input_file_name)
-# openfastlib.fast_run()
+def serial(input_file):
+    input_file_name = input_file # "/Users/rmudafor/Development/weis/reg_tests/r-test/glue-codes/openfast/AOC_YFix_WSt/AOC_YFix_WSt.fst"
+    openfastlib = openfast_library.FastLibAPI(library_path, input_file_name)
+    openfastlib.fast_run()
 
-# ## Display the outputs
-# for i, c in enumerate(openfastlib.output_channel_names):
-#     print(i, c)
-# print(openfastlib.output_channel_names)
-# print(openfastlib.output_values)
-# print(openfastlib.output_values[:,0])   # Prints the time steps
+    # Display the outputs
+    # for i, c in enumerate(openfastlib.output_channel_names):
+    #     print(i, c)
+    # print(openfastlib.output_channel_names)
+    # print(openfastlib.output_values)
+    # print(openfastlib.output_values[:,0])   # Prints the time steps
 
-## Parallel with MPI
-from mpi4py import MPI
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
+def parallel():
+    ## Parallel with MPI
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
 
-if rank == 0:
-    input_file_name = "{}/reg_tests/r-test/glue-codes/openfast/AOC_WSt/AOC_WSt.fst".format(project_root)
-    openfastlib = openfast_library.FastLibAPI(library_path, create_string_buffer(input_file_name.encode('utf-8')))
-    openfastlib.fast_run()
-elif rank == 1:
-    input_file_name = "{}/reg_tests/r-test/glue-codes/openfast/AOC_YFix_WSt/AOC_YFix_WSt.fst".format(project_root)
-    openfastlib = openfast_library.FastLibAPI(library_path, create_string_buffer(input_file_name.encode('utf-8')))
-    openfastlib.fast_run()
-elif rank == 2:
-    input_file_name = "{}/reg_tests/r-test/glue-codes/openfast/AOC_YFree_WTurb/AOC_YFree_WTurb.fst".format(project_root)
-    openfastlib = openfast_library.FastLibAPI(library_path, create_string_buffer(input_file_name.encode('utf-8')))
-    openfastlib.fast_run()
-elif rank == 3:
-    input_file_name = "{}/reg_tests/r-test/glue-codes/openfast/AWT_YFix_WSt/AWT_YFix_WSt.fst".format(project_root)
-    openfastlib = openfast_library.FastLibAPI(library_path, create_string_buffer(input_file_name.encode('utf-8')))
-    openfastlib.fast_run()
+    if rank == 0:
+        input_file_name = "{}/reg_tests/r-test/glue-codes/openfast/AOC_WSt/AOC_WSt.fst".format(project_root)
+        openfastlib = openfast_library.FastLibAPI(library_path, input_file_name)
+        openfastlib.fast_run()
+    elif rank == 1:
+        input_file_name = "{}/reg_tests/r-test/glue-codes/openfast/AOC_YFix_WSt/AOC_YFix_WSt.fst".format(project_root)
+        openfastlib = openfast_library.FastLibAPI(library_path, input_file_name)
+        openfastlib.fast_run()
+    elif rank == 2:
+        input_file_name = "{}/reg_tests/r-test/glue-codes/openfast/AOC_YFree_WTurb/AOC_YFree_WTurb.fst".format(project_root)
+        openfastlib = openfast_library.FastLibAPI(library_path, input_file_name)
+        openfastlib.fast_run()
+    elif rank == 3:
+        input_file_name = "{}/reg_tests/r-test/glue-codes/openfast/AWT_YFix_WSt/AWT_YFix_WSt.fst".format(project_root)
+        openfastlib = openfast_library.FastLibAPI(library_path, input_file_name)
+        openfastlib.fast_run()
+
+if __name__=="__main__":
+    import sys
+    if len(sys.argv) > 1:
+        input_file = sys.argv[1]
+        serial(input_file)
+    else:
+        print("No cases run. Check the driver code.")
