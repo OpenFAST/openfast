42ff2fad12d80c4f9d2cb5f0d955a98c4f196607
andrew-platt <andy.platt@nrel.gov>
2021-05-18 16:27:02 -0600

    NWTC lib: add unit test for InitFileInfo_FromNullCString subroutine and fix broken test
    Also reorder part of the InitFileInfo_FromStringArray routine

--- modules/nwtc-library/tests/test_NWTC_IO_FileInfo.F90
+++ modules/nwtc-library/tests/test_NWTC_IO_FileInfo.F90
@@ -18,7 +18,7 @@ subroutine test_initfileinfo()
 
     integer(IntKi) :: error_status
     character(ErrMsgLen) :: error_message
-    character(1024) :: input_strings(num_lines)
+    character(MaxFileInfoLineLen) :: input_strings(num_lines)
     type(FileInfoType) :: file_info_type
     integer :: i
 
@@ -62,7 +62,8 @@ subroutine test_initfileinfo2()
 
     integer(IntKi) :: error_status
     character(ErrMsgLen) :: error_message
-    character(1025) :: input_strings(num_lines)
+    character(MaxFileInfoLineLen*2) :: input_strings(num_lines)
+    character(MaxFileInfoLineLen*2) :: tmpstring
     type(FileInfoType) :: file_info_type
     integer :: i
 
@@ -73,11 +74,45 @@ subroutine test_initfileinfo2()
         "line 3",  &
         "line 4"   &
     /)
+    ! make the last character a + so a trim does not reduce this string length
+    tmpstring=input_strings(5)
+    tmpstring(MaxFileInfoLineLen+1:MaxFileInfoLineLen+1) = 'a'
+    input_strings(5)=tmpstring
     call InitFileInfo( input_strings, file_info_type, error_status, error_message )
-
     @assertEqual(num_lines, file_info_type%NumLines)
     @assertEqual(num_files, file_info_type%NumFiles)
     @assertEqual( 4, error_status )
+    
+end subroutine
+
+@test
+subroutine test_initfileinfo3()
+    USE ISO_C_BINDING, only: C_CHAR, C_NULL_CHAR
+
+    ! This case should result in zero error status.
+    ! It attempts to initialize FileInfoType with a C_NULL_CHAR delimited string 
+
+    integer, parameter :: num_lines = 7
+    integer, parameter :: num_files = 1
+
+    integer(IntKi) :: error_status
+    character(ErrMsgLen) :: error_message
+    character(kind=C_CHAR,len=MaxFileInfoLineLen*2) :: input_string
+    type(FileInfoType) :: file_info_type
+    integer :: i
+
+    ! Note: the rest of the input string is empty.  That should pass ok
+    input_string =   "line  0"//C_NULL_CHAR//   &
+                     "line  1"//C_NULL_CHAR//   &
+                     "line  2"//C_NULL_CHAR//   &
+                     "line  3"//C_NULL_CHAR//   &
+                     "line  4"//C_NULL_CHAR//   &
+                     "line  5"//C_NULL_CHAR//   &
+                     "line  6"//C_NULL_CHAR
+    call InitFileInfo( input_string, file_info_type, error_status, error_message )
+    @assertEqual(num_lines, file_info_type%NumLines)
+    @assertEqual(num_files, file_info_type%NumFiles)
+    @assertEqual( 0, error_status )
     
 end subroutine
 
