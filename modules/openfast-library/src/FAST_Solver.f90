207059dbc05e16ac2cf55f71987fec912bfd4715
Martinez <tonyinme@gmail.com>
2021-06-30 10:19:32 -0600

    Curl: First implementation of curled wake model

--- modules/openfast-library/src/FAST_Solver.f90
+++ modules/openfast-library/src/FAST_Solver.f90
@@ -3291,6 +3291,7 @@ CONTAINS
    !...............................................................................................................................
    SUBROUTINE CleanUp()
       INTEGER(IntKi)             :: ErrStat3    ! The error identifier (ErrStat)
+      INTEGER(IntKi)             :: nb 
       CHARACTER(ErrMsgLen)       :: ErrMsg3     ! The error message (ErrMsg)
          
       IF ( calcJacobian ) THEN
