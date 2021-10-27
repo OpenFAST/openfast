cf57a85a66e9caca5f903127d9b879523851c759
andrew-platt <andy.platt@nrel.gov>
2021-10-27 13:14:44 -0600

    CMake+Intel+Win32+Debug compiler flag consistency

--- cmake/OpenfastFortranOptions.cmake
+++ cmake/OpenfastFortranOptions.cmake
@@ -221,7 +221,7 @@ macro(set_fast_intel_fortran_windows)
 
   # debug flags
   if(CMAKE_BUILD_TYPE MATCHES Debug)
-    set( CMAKE_Fortran_FLAGS_DEBUG "${CMAKE_Fortran_FLAGS_DEBUG} /check:all /traceback /Qinit=huge,infinity" )
+    set( CMAKE_Fortran_FLAGS_DEBUG "${CMAKE_Fortran_FLAGS_DEBUG} /check:all,noarg_temp_created /traceback /Qinit=huge,infinity" )
   endif()
 
   # OPENMP
