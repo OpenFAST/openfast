d6225f48e515a6504cf136ca605d17a538eb9480
Hannah Ross <hannah.ross@nrel.gov>
2021-11-03 10:43:10 -0600

    Fix uninitialized variables in FAST_Solver

--- modules/openfast-library/src/FAST_Solver.f90
+++ modules/openfast-library/src/FAST_Solver.f90
@@ -376,9 +376,6 @@ SUBROUTINE ED_InputSolve( p_FAST, u_ED, y_ED, p_AD14, y_AD14, y_AD, y_SrvD, u_AD
          END IF
    END IF
 
-   u_ED%HubPtLoad%Force = 0.0_ReKi
-   u_ED%HubPtLoad%Moment = 0.0_ReKi
-
    IF ( p_FAST%CompAero == Module_AD ) THEN
       IF ( u_AD%rotors(1)%HubMotion%Committed ) THEN
          CALL Transfer_Point_to_Point( y_AD%rotors(1)%HubLoad, MeshMapData%u_ED_HubPtLoad, MeshMapData%AD_P_2_ED_P_H, ErrStat2, ErrMsg2, u_AD%rotors(1)%HubMotion, y_ED%HubPtMotion )
@@ -3011,6 +3008,10 @@ CONTAINS
             
          ! Transfer BD loads to ED hub input:
          ! we're mapping loads, so we also need the sibling meshes' displacements:
+         if ( p_FAST%CompAero /= Module_AD ) then
+            MeshMapData%u_ED_HubPtLoad%Force  = 0.0_ReKi
+            MeshMapData%u_ED_HubPtLoad%Moment = 0.0_ReKi
+         end if
          do i=1,p_FAST%nBeams            
             CALL Transfer_Point_to_Point( y_BD2(i)%ReactionForce, MeshMapData%u_ED_HubPtLoad_2, MeshMapData%BD_P_2_ED_P(i), ErrStat2, ErrMsg2, MeshMapData%u_BD_RootMotion(i), y_ED2%HubPtMotion) !u_BD_RootMotion and y_ED2%HubPtMotion contain the displaced positions for load calculations
                CALL SetErrStat(ErrStat2,ErrMsg2, ErrStat, ErrMsg, RoutineName)
@@ -4887,6 +4888,9 @@ SUBROUTINE InitModuleMappings(p_FAST, ED, BD, AD14, AD, HD, SD, ExtPtfm, SrvD, M
          .or. p_FAST%CompMooring == Module_Orca) THEN
                   
          ! Temporary meshes for transfering inputs to ED, HD, BD, Orca, and SD
+      CALL MeshCopy( ED%Input(1)%HubPtLoad, MeshMapData%u_ED_HubPtLoad, MESH_NEWCOPY, ErrStat2, ErrMsg2 )
+         CALL SetErrStat( ErrStat2, ErrMsg2, ErrStat, ErrMsg, RoutineName//':u_ED_HubPtLoad' )
+
       CALL MeshCopy ( ED%Input(1)%PlatformPtMesh, MeshMapData%u_ED_PlatformPtMesh, MESH_NEWCOPY, ErrStat2, ErrMsg2 )      
          CALL SetErrStat( ErrStat2, ErrMsg2, ErrStat, ErrMsg, RoutineName//':u_ED_PlatformPtMesh' )                 
 
