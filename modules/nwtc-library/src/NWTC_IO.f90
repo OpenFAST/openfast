cd0a6c15f5cb9a3926a05c1737fca67389a70b1a
andrew-platt <andy.platt@nrel.gov>
2022-02-21 12:19:00 -0700

    NWTC_IO: ParseVarWDefault was putting double entries in echo files if not default value

--- modules/nwtc-library/src/NWTC_IO.f90
+++ modules/nwtc-library/src/NWTC_IO.f90
@@ -4611,7 +4611,7 @@ END SUBROUTINE CheckR16Var
       CALL Conv2UC( defaultStr )
       IF ( INDEX(defaultStr, "DEFAULT" ) /= 1 ) THEN ! If it's not "default", read this variable
          LineNum = LineNum - 1  ! back up a line
-         CALL ParseVar ( FileInfo, LineNum, ExpVarName, Var, ErrStatLcl, ErrMsg2, UnEc )
+         CALL ParseVar ( FileInfo, LineNum, ExpVarName, Var, ErrStatLcl, ErrMsg2 )
             CALL SetErrStat( ErrStatLcl, ErrMsg2, ErrStat, ErrMsg, RoutineName )
       ELSE
          Var = VarDefault  ! "DEFAULT" value
