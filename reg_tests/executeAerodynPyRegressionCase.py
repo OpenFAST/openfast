061d13ccca70c9055aa46c60bda44d84c9b409fa
andrew-platt <andy.platt@nrel.gov>
2022-10-24 15:32:59 -0600

    ADI: update py_ad_ case

--- reg_tests/executeAerodynPyRegressionCase.py
+++ reg_tests/executeAerodynPyRegressionCase.py
@@ -24,9 +24,10 @@
 
 import os
 import sys
-basepath = os.path.dirname(__file__)
+basepath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.sep.join([basepath, "lib"]))
 import argparse
+import numpy as np
 import shutil
 import glob
 import subprocess
@@ -46,9 +47,8 @@ parser.add_argument("caseName", metavar="Case-Name", type=str, nargs=1, help="Th
 parser.add_argument("executable", metavar="AeroDyn-Inflow-Python", type=str, nargs=1, help="The path to the AeroDyn driver executable.")
 parser.add_argument("sourceDirectory", metavar="path/to/openfast_repo", type=str, nargs=1, help="The path to the OpenFAST repository.")
 parser.add_argument("buildDirectory", metavar="path/to/openfast_repo/build", type=str, nargs=1, help="The path to the OpenFAST repository build directory.")
-parser.add_argument("tolerance", metavar="Test-Tolerance", type=float, nargs=1, help="Tolerance defining pass or failure in the regression test.")
-parser.add_argument("systemName", metavar="System-Name", type=str, nargs=1, help="The current system\'s name: [Darwin,Linux,Windows]")
-# parser.add_argument("compilerId", metavar="Compiler-Id", type=str, nargs=1, help="The compiler\'s id: [Intel,GNU]")
+parser.add_argument("rtol", metavar="Relative-Tolerance", type=float, nargs=1, help="Relative tolerance to allow the solution to deviate; expressed as order of magnitudes less than baseline.")
+parser.add_argument("atol", metavar="Absolute-Tolerance", type=float, nargs=1, help="Absolute tolerance to allow small values to pass; expressed as order of magnitudes less than baseline.")
 parser.add_argument("-p", "-plot", dest="plot", action='store_true', help="bool to include plots in failed cases")
 parser.add_argument("-n", "-no-exec", dest="noExec", action='store_true', help="bool to prevent execution of the test cases")
 parser.add_argument("-v", "-verbose", dest="verbose", action='store_true', help="bool to include verbose system output")
@@ -59,7 +59,8 @@ caseName = args.caseName[0]
 executable = args.executable[0]
 sourceDirectory = args.sourceDirectory[0]
 buildDirectory = args.buildDirectory[0]
-tolerance = args.tolerance[0]
+rtol = args.rtol[0]
+atol = args.atol[0]
 plotError = args.plot if args.plot is False else True
 noExec = args.noExec if args.noExec is False else True
 verbose = args.verbose if args.verbose is False else True
@@ -78,7 +79,6 @@ moduleDirectory = os.path.join(rtest, "modules", "aerodyn")
 inputsDirectory = os.path.join(moduleDirectory, caseName)
 targetOutputDirectory = os.path.join(inputsDirectory)
 testBuildDirectory = os.path.join(buildDirectory, caseName)
-print('TargetOuput',targetOutputDirectory)
     
 # verify all the required directories exist
 if not os.path.isdir(rtest):
@@ -89,20 +89,7 @@ if not os.path.isdir(inputsDirectory):
     rtl.exitWithError("The test data inputs directory, {}, does not exist. Verify your local repository is up to date.".format(inputsDirectory))
 
 
-# Special case, copy the BAR Baseline files
-dst = os.path.join(buildDirectory, "BAR_Baseline")
-src = os.path.join(moduleDirectory, "BAR_Baseline")
-try:
-    rtl.copyTree(src, dst)
-except:
-    # This can fail if two processes are copying the file at the same time
-    print('>>> Copy failed')
-    import time
-    time.sleep(1)
-
 # create the local output directory and initialize it with input files 
-print('>>>> Input',inputsDirectory)
-print('>>>> Input',testBuildDirectory)
 rtl.copyTree(inputsDirectory, testBuildDirectory, renameDict={'py_ad_driver.out':'py_ad_driver_ref.out'})
        # , excludeExt=['.out','.outb'])
 
@@ -111,7 +98,7 @@ if not noExec:
     caseInputFile = os.path.join(testBuildDirectory, "py_ad_driver.py")
     returnCode = openfastDrivers.runAerodynDriverCase(caseInputFile, executable, verbose=verbose)
     if returnCode != 0:
-        rtl.exitWithError("")
+        sys.exit(returnCode*10)
     
 ###Build the filesystem navigation variables for running the regression test
 # For multiple turbines, test turbine 2, for combined cases, test case 4 
@@ -120,31 +107,31 @@ baselineOutFile = os.path.join(targetOutputDirectory, os.path.basename(localOutF
 rtl.validateFileOrExit(localOutFile)
 rtl.validateFileOrExit(baselineOutFile)
 
-testData, testInfo, testPack = pass_fail.readFASTOut(localOutFile)
+testData, testInfo, _ = pass_fail.readFASTOut(localOutFile)
 baselineData, baselineInfo, _ = pass_fail.readFASTOut(baselineOutFile)
-performance = pass_fail.calculateNorms(testData, baselineData)
-normalizedNorm = performance[:, 1]
+
+passing_channels = pass_fail.passing_channels(testData.T, baselineData.T, rtol, atol)
+passing_channels = passing_channels.T
+
+norms = pass_fail.calculateNorms(testData, baselineData)
 
 # export all case summaries
-results = list(zip(testInfo["attribute_names"], [*performance]))
-results_max = performance.max(axis=0)
-exportCaseSummary(testBuildDirectory, caseName, results, results_max, tolerance)
+channel_names = testInfo["attribute_names"]
+exportCaseSummary(testBuildDirectory, caseName, channel_names, passing_channels, norms)
+
+# passing case
+if np.all(passing_channels):
+    sys.exit(0)
 
 # failing case
-if not pass_fail.passRegressionTest(normalizedNorm, tolerance):
-    if plotError:
-        from errorPlotting import finalizePlotDirectory, plotOpenfastError
-        ixFailChannels = [i for i in range(len(testInfo["attribute_names"])) if normalizedNorm[i] > tolerance]
-        failChannels = [channel for i, channel in enumerate(testInfo["attribute_names"]) if i in ixFailChannels]
-        failResults = [res for i, res in enumerate(results) if i in ixFailChannels]
-        for channel in failChannels:
-            try:
-                plotOpenfastError(localOutFile, baselineOutFile, channel)
-            except:
-                error = sys.exc_info()[1]
-                print("Error generating plots: {}".format(error.msg))
-        finalizePlotDirectory(localOutFile, failChannels, caseName)
-    sys.exit(1)
+if plotError:
+    from errorPlotting import finalizePlotDirectory, plotOpenfastError
+    for channel in testInfo["attribute_names"]:
+        try:
+            plotOpenfastError(localOutFile, baselineOutFile, channel, rtol, atol)
+        except:
+            error = sys.exc_info()[1]
+            print("Error generating plots: {}".format(error))
+    finalizePlotDirectory(localOutFile, testInfo["attribute_names"], caseName)
     
-# passing case
-sys.exit(0)
+sys.exit(1)
