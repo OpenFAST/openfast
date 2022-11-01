9a05e983d7bb275f5afe46880367310f736f461b
andrew-platt <andy.platt@nrel.gov>
2022-11-01 16:55:32 -0600

    ROM: fix regression tests and update

--- reg_tests/executeAerodiskRegressionCase.py
+++ reg_tests/executeAerodiskRegressionCase.py
@@ -24,9 +24,10 @@
 
 import os
 import sys
-basepath = os.path.sep.join(sys.argv[0].split(os.path.sep)[:-1]) if os.path.sep in sys.argv[0] else "."
+basepath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.sep.join([basepath, "lib"]))
 import argparse
+import numpy as np
 import shutil
 import glob
 import subprocess
@@ -46,9 +47,8 @@ parser.add_argument("caseName", metavar="Case-Name", type=str, nargs=1, help="Th
 parser.add_argument("executable", metavar="AeroDisk-Driver", type=str, nargs=1, help="The path to the AeroDisk driver executable.")
 parser.add_argument("sourceDirectory", metavar="path/to/openfast_repo", type=str, nargs=1, help="The path to the OpenFAST repository.")
 parser.add_argument("buildDirectory", metavar="path/to/openfast_repo/build", type=str, nargs=1, help="The path to the OpenFAST repository build directory.")
-parser.add_argument("tolerance", metavar="Test-Tolerance", type=float, nargs=1, help="Tolerance defining pass or failure in the regression test.")
-parser.add_argument("systemName", metavar="System-Name", type=str, nargs=1, help="The current system\'s name: [Darwin,Linux,Windows]")
-parser.add_argument("compilerId", metavar="Compiler-Id", type=str, nargs=1, help="The compiler\'s id: [Intel,GNU]")
+parser.add_argument("rtol", metavar="Relative-Tolerance", type=float, nargs=1, help="Relative tolerance to allow the solution to deviate; expressed as order of magnitudes less than baseline.")
+parser.add_argument("atol", metavar="Absolute-Tolerance", type=float, nargs=1, help="Absolute tolerance to allow small values to pass; expressed as order of magnitudes less than baseline.")
 parser.add_argument("-p", "-plot", dest="plot", action='store_true', help="bool to include plots in failed cases")
 parser.add_argument("-n", "-no-exec", dest="noExec", action='store_true', help="bool to prevent execution of the test cases")
 parser.add_argument("-v", "-verbose", dest="verbose", action='store_true', help="bool to include verbose system output")
@@ -59,10 +59,11 @@ caseName = args.caseName[0]
 executable = args.executable[0]
 sourceDirectory = args.sourceDirectory[0]
 buildDirectory = args.buildDirectory[0]
-tolerance = args.tolerance[0]
-plotError = args.plot if args.plot is False else True
-noExec = args.noExec if args.noExec is False else True
-verbose = args.verbose if args.verbose is False else True
+rtol = args.rtol[0]
+atol = args.atol[0]
+plotError = args.plot
+noExec = args.noExec
+verbose = args.verbose
 
 # validate inputs
 rtl.validateExeOrExit(executable)
@@ -87,26 +88,27 @@ if not os.path.isdir(targetOutputDirectory):
 if not os.path.isdir(inputsDirectory):
     rtl.exitWithError("The test data inputs directory, {}, does not exist. Verify your local repository is up to date.".format(inputsDirectory))
 
-# create the local output directory if it does not already exist
-# and initialize it with input files for all test cases
-if not os.path.isdir(testBuildDirectory):
-    os.makedirs(testBuildDirectory, exist_ok = True)
-    for file in glob.glob(os.path.join(inputsDirectory,"*inp")):
-        filename = file.split(os.path.sep)[-1]
-        shutil.copy(os.path.join(inputsDirectory,filename), os.path.join(testBuildDirectory,filename))
-    for file in glob.glob(os.path.join(inputsDirectory,"*dat")):
-        filename = file.split(os.path.sep)[-1]
-        shutil.copy(os.path.join(inputsDirectory,filename), os.path.join(testBuildDirectory,filename))
-    for file in glob.glob(os.path.join(inputsDirectory,"*csv")):
-        filename = file.split(os.path.sep)[-1]
-        shutil.copy(os.path.join(inputsDirectory,filename), os.path.join(testBuildDirectory,filename))
+## create the local output directory if it does not already exist
+## and initialize it with input files for all test cases
+#if not os.path.isdir(testBuildDirectory):
+#    os.makedirs(testBuildDirectory, exist_ok = True)
+#    for file in glob.glob(os.path.join(inputsDirectory,"*inp")):
+#        filename = file.split(os.path.sep)[-1]
+#        shutil.copy(os.path.join(inputsDirectory,filename), os.path.join(testBuildDirectory,filename))
+#    for file in glob.glob(os.path.join(inputsDirectory,"*dat")):
+#        filename = file.split(os.path.sep)[-1]
+#        shutil.copy(os.path.join(inputsDirectory,filename), os.path.join(testBuildDirectory,filename))
+#    for file in glob.glob(os.path.join(inputsDirectory,"*csv")):
+#        filename = file.split(os.path.sep)[-1]
+#        shutil.copy(os.path.join(inputsDirectory,filename), os.path.join(testBuildDirectory,filename))
+rtl.copyTree(inputsDirectory, testBuildDirectory, renameDict={'adsk_driver.outb':'adsk_driver_ref.outb'})
 
 ### Run aerodisk on the test case
 if not noExec:
-    caseInputFile = os.path.join(testBuildDirectory, "adsk_driver.inp")
-    returnCode = openfastDrivers.runAerodiskDriverCase(caseInputFile, executable)
+    caseInputFile = os.path.join(testBuildDirectory, "adsk_driver.dvr")
+    returnCode = openfastDrivers.runAerodiskDriverCase(caseInputFile, executable, verbose=verbose)
     if returnCode != 0:
-        rtl.exitWithError("")
+        sys.exit(returnCode*10)
     
 ### Build the filesystem navigation variables for running the regression test
 localOutFile = os.path.join(testBuildDirectory, "adsk_driver.out")
@@ -114,31 +116,31 @@ baselineOutFile = os.path.join(targetOutputDirectory, "adsk_driver.out")
 rtl.validateFileOrExit(localOutFile)
 rtl.validateFileOrExit(baselineOutFile)
 
-testData, testInfo, testPack = pass_fail.readFASTOut(localOutFile)
+testData, testInfo, _ = pass_fail.readFASTOut(localOutFile)
 baselineData, baselineInfo, _ = pass_fail.readFASTOut(baselineOutFile)
-performance = pass_fail.calculateNorms(testData, baselineData)
-normalizedNorm = performance[:, 1]
+
+passing_channels = pass_fail.passing_channels(testData.T, baselineData.T, rtol, atol)
+passing_channels = passing_channels.T
+
+norms = pass_fail.calculateNorms(testData, baselineData)
 
 # export all case summaries
-results = list(zip(testInfo["attribute_names"], [*performance]))
-results_max = performance.max(axis=0)
-exportCaseSummary(testBuildDirectory, caseName, results, results_max, tolerance)
+channel_names = testInfo["attribute_names"]
+exportCaseSummary(testBuildDirectory, caseName, channel_names, passing_channels, norms)
+
+# passing case
+if np.all(passing_channels):
+    sys.exit(0)
 
 # failing case
-if not pass_fail.passRegressionTest(normalizedNorm, tolerance):
-    if plotError:
-        from errorPlotting import finalizePlotDirectory, plotOpenfastError
-        ixFailChannels = [i for i in range(len(testInfo["attribute_names"])) if normalizedNorm[i] > tolerance]
-        failChannels = [channel for i, channel in enumerate(testInfo["attribute_names"]) if i in ixFailChannels]
-        failResults = [res for i, res in enumerate(results) if i in ixFailChannels]
-        for channel in failChannels:
-            try:
-                plotOpenfastError(localOutFile, baselineOutFile, channel)
-            except:
-                error = sys.exc_info()[1]
-                print("Error generating plots: {}".format(error.msg))
-        finalizePlotDirectory(localOutFile, failChannels, caseName)
-    sys.exit(1)
+if plotError:
+    from errorPlotting import finalizePlotDirectory, plotOpenfastError
+    for channel in testInfo["attribute_names"]:
+        try:
+            plotOpenfastError(localOutFile, baselineOutFile, channel, rtol, atol)
+        except:
+            error = sys.exc_info()[1]
+            print("Error generating plots: {}".format(error))
+    finalizePlotDirectory(localOutFile, testInfo["attribute_names"], caseName)
     
-# passing case
-sys.exit(0)
+sys.exit(1)
