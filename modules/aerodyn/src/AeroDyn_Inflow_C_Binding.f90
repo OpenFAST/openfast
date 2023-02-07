45ba20f45fcc0e363263c19c2133c3b2722698ec
andrew-platt <andy.platt@nrel.gov>
2023-02-06 23:34:43 -0700

    ADI_C: call to ADI_ADIW_Solve for IfW setting values, update regression tests

--- modules/aerodyn/src/AeroDyn_Inflow_C_Binding.f90
+++ modules/aerodyn/src/AeroDyn_Inflow_C_Binding.f90
@@ -1052,6 +1052,10 @@ SUBROUTINE AeroDyn_Inflow_C_CalcOutput(Time_C, &
             ErrStat2, ErrMsg2 )  ! transfer input motion mesh to u(1) meshes
       if (Failed())  return
 
+   ! call IfW and set inputs for AD
+   call ADI_ADIW_Solve(Time, ADI%u(1)%AD, ADI%OtherState(STATE_CURR)%AD, ADI%m%IW%u, ADI%m%IW, .false., ErrStat2, ErrMsg2)
+      if (Failed())  return
+
    ! Call the main subroutine ADI_CalcOutput to get the resulting forces and moments at time T
    CALL ADI_CalcOutput( Time, ADI%u(1), ADI%p, ADI%x(STATE_CURR), ADI%xd(STATE_CURR), ADI%z(STATE_CURR), ADI%OtherState(STATE_CURR), ADI%y, ADI%m, ErrStat2, ErrMsg2 )
       if (Failed())  return
