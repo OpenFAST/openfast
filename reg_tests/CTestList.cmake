a5afd1f9e4469da61ef061fdb74272eda9d424c9
Emmanuel Branlard <emmanuel.branlard@nrel.gov>
2023-06-27 15:12:05 -0600

    Adding tailfin testcase

--- reg_tests/CTestList.cmake
+++ reg_tests/CTestList.cmake
@@ -282,6 +282,7 @@ of_regression("EllipticalWing_OLAF"                    "openfast;aerodyn15;olaf"
 of_regression("StC_test_OC4Semi"                       "openfast;servodyn;hydrodyn;moordyn;offshore;stc")
 of_regression("MHK_RM1_Fixed"                          "openfast;elastodyn;aerodyn15;mhk")
 of_regression("MHK_RM1_Floating"                       "openfast;elastodyn;aerodyn15;hydrodyn;moordyn;mhk")
+of_regression("Tailfin_FreeYaw1DOF_PolarBased"         "openfast;elastodyn;aerodyn15")
 
 # OpenFAST C++ API test
 if(BUILD_OPENFAST_CPP_API)
