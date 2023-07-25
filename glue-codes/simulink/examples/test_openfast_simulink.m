ab24635df6ab95d59d2fc006ea2e2f66a5b803ff
reos-rcrozier <91322120+reos-rcrozier@users.noreply.github.com>
2023-07-26 00:45:15 +0100

    Update test_openfast_simulink.m
    Use environment variable to get workspace root

--- glue-codes/simulink/examples/test_openfast_simulink.m
+++ glue-codes/simulink/examples/test_openfast_simulink.m
@@ -6,12 +6,14 @@ classdef OpenFASTSimulinkTest < matlab.unittest.TestCase
         
         function testOpenLoopRuns(testCase)
 
+            workspace_root = getenv("GITHUB_WORKSPACE");
+
             this_file_path = fileparts(which(mfilename()));
 
             cd(this_file_path);
             
             % these variables are defined in the OpenLoop model's FAST_SFunc block:
-            FAST_InputFileName = fullfile(this_file_path, '..', '..', '..', 'reg_tests', 'r-test', 'glue-codes', 'openfast', 'AOC_WSt/AOC_WSt.fst');
+            FAST_InputFileName = fullfile(workspace_root, 'reg_tests', 'r-test', 'glue-codes', 'openfast', 'AOC_WSt', 'AOC_WSt.fst');
             TMax               = 5; % seconds
             
             sim('OpenLoop.mdl',[0,TMax]);
