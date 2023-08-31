e9c646d184d8f90f2aa4d035ca2be8f8242f016a
andrew-platt <andy.platt@nrel.gov>
2023-08-31 17:23:30 -0600

    BD RefChange: code cleanup.  Add flag to get old behaviour

--- modules/beamdyn/src/BeamDyn.f90
+++ modules/beamdyn/src/BeamDyn.f90
@@ -50,9 +50,11 @@ MODULE BeamDyn
                                                !   states(z)
    PUBLIC :: BD_GetOP                          !< Routine to pack the operating point values (for linearization) into arrays
 
-   PUBLIC :: UpdateBeamDynGlobalReference      !< update the BeamDyn reference.  The reference for the calculations follows u%RootMotionMesh
+   PUBLIC :: BD_UpdateGlobalRef      !< update the BeamDyn reference.  The reference for the calculations follows u%RootMotionMesh
                                                !  and therefore x%q must be updated from T -> T+DT to include the root motion from T->T+DT
 
+   ! do we change the reference frame at each State update?
+   LOGICAL, PARAMETER :: ChangeRefFrame=.true.
 
 CONTAINS
 
@@ -1920,9 +1922,11 @@ SUBROUTINE BD_UpdateStates( t, n, u, utimes, p, x, xd, z, OtherState, m, ErrStat
       CALL BD_GA2( t, n, u, utimes, p, x, xd, z, OtherState, m, ErrStat2, ErrMsg2 )
       call SetErrStat(ErrStat2,ErrMsg2,ErrStat,ErrMsg,'');  if (ErrStat >= AbortErrLev) return
 
-      ! change reference frame to root motion at t=T+DT (u(1)%RootMotionMesh)
-     call UpdateBeamDynGlobalReference(u(1),p,x,OtherState,ErrStat2,ErrMsg2)
-      call SetErrStat(ErrStat2,ErrMsg2,ErrStat,ErrMsg,'')
+      if (ChangeRefFrame) then
+         ! change reference frame to root motion at t=T+DT (u(1)%RootMotionMesh)
+         call BD_UpdateGlobalRef(u(1),p,x,OtherState,ErrStat2,ErrMsg2)
+         call SetErrStat(ErrStat2,ErrMsg2,ErrStat,ErrMsg,'')
+      endif
    ELSE !IF(p%analysis_type == BD_STATIC_ANALYSIS) THEN
       CALL BD_Static( t, u, utimes, p, x, OtherState, m, ErrStat, ErrMsg )
    ENDIF
@@ -2124,10 +2128,6 @@ SUBROUTINE BD_CalcContStateDeriv( t, u, p, x, xd, z, OtherState, m, dxdt, ErrSta
    CHARACTER(*),                 INTENT(  OUT)  :: ErrMsg      !< Error message if ErrStat /= ErrID_None
 
       ! LOCAL variables
-   real(R8Ki)                                   :: GlbPos_new(3)
-   real(R8Ki)                                   :: GlbRot_new(3,3)
-   real(R8Ki)                                   :: GlbWM_new(3)
-   real(R8Ki)                                   :: GlbWM_diff(3)
    INTEGER(IntKi)                         :: ErrStat2          ! The error status code
    CHARACTER(ErrMsgLen)                   :: ErrMsg2           ! The error message, if an error occurred
    CHARACTER(*), PARAMETER                :: RoutineName = 'BD_CalcContStateDeriv'
@@ -2159,19 +2159,13 @@ SUBROUTINE BD_CalcContStateDeriv( t, u, p, x, xd, z, OtherState, m, dxdt, ErrSta
    CALL BD_CopyContState(x, dxdt, MESH_UPDATECOPY, ErrStat2, ErrMsg2)
       CALL SetErrStat( ErrStat2, ErrMsg2, ErrStat, ErrMsg, RoutineName )
       
-   ! Find the root position/rotation information at extrapolated u -- u must be in the global frame for the SetRefFrame routine
-   call SetRefFrame(u, GlbPos_new, GlbRot_new, GlbWM_new, ErrStat2,ErrMsg2)
-      CALL SetErrStat( ErrStat2, ErrMsg2, ErrStat, ErrMsg, RoutineName )
-      if (ErrStat >= AbortErrLev) return
-
    ! Root displacement is relative to the GlbPos at time T, which is simply the difference between
    ! the previous root position (GlbPos) and the new extrapolated position (Pos+TransDisp)
    dxdt%q(1:3,1) = m%u%RootMotion%TranslationDisp(:,1) + &
                     matmul(m%u%RootMotion%Position(:,1) - OtherState%GlbPos, OtherState%GlbRot)
-   CALL ExtractRelativeRotation(m%u%RootMotion%Orientation(:,:,1), p, OtherState, dxdt%q(   4:6,1), ErrStat2, ErrMsg2)
+   CALL ExtractRelativeRotation(m%u%RootMotion%Orientation(:,:,1), p, OtherState, dxdt%q(4:6,1), ErrStat2, ErrMsg2)
       CALL SetErrStat(ErrStat2, ErrMsg2, ErrStat, ErrMsg, RoutineName)
       if (ErrStat >= AbortErrLev) return
-
    dxdt%dqdt(1:3,1) = m%u%RootMotion%TranslationVel(:,1)
    dxdt%dqdt(4:6,1) = m%u%Rootmotion%RotationVel(:,1)
 
@@ -4616,10 +4610,6 @@ SUBROUTINE BD_BoundaryGA2(x,p,u,OtherState, ErrStat, ErrMsg)
    INTEGER(IntKi),               INTENT(  OUT)  :: ErrStat     !< Error status of the operation
    CHARACTER(*),                 INTENT(  OUT)  :: ErrMsg      !< Error message if ErrStat /= ErrID_None
 
-   real(R8Ki)                                   :: GlbPos_new(3)
-   real(R8Ki)                                   :: GlbRot_new(3,3)
-   real(R8Ki)                                   :: GlbWM_new(3)
-   real(R8Ki)                                   :: GlbWM_diff(3)
    INTEGER(IntKi)                               :: ErrStat2    ! Temporary Error status
    CHARACTER(ErrMsgLen)                         :: ErrMsg2     ! Temporary Error message
    CHARACTER(*), PARAMETER                      :: RoutineName = 'BD_BoundaryGA2'
@@ -4629,9 +4619,13 @@ SUBROUTINE BD_BoundaryGA2(x,p,u,OtherState, ErrStat, ErrMsg)
    ErrMsg  = ""
 
    ! NOTE: u is in a BD local frame.  So cannot use SetRefFrame routine (note there are differences here)
-   x%q(1:3,1) = u%RootMotion%TranslationDisp(:,1) + &
+
+      ! Root displacements
+   x%q(1:3,1) = u%RootMotion%TranslationDisp(1:3,1) + &
                     matmul(u%RootMotion%Position(:,1) - OtherState%GlbPos, OtherState%GlbRot)
-   CALL ExtractRelativeRotation(u%RootMotion%Orientation(:,:,1), p, OtherState, x%q(   4:6,1), ErrStat2, ErrMsg2)
+
+      ! Root rotations
+   CALL ExtractRelativeRotation(u%RootMotion%Orientation(:,:,1),p, OtherState, x%q(4:6,1), ErrStat2, ErrMsg2)
       CALL SetErrStat(ErrStat2, ErrMsg2, ErrStat, ErrMsg, RoutineName)
       if (ErrStat >= AbortErrLev) return
 
@@ -6834,16 +6828,17 @@ END SUBROUTINE BD_WriteMassStiffInFirstNodeFrame
 !!    - move the state information in x from the previous reference frame at time T (u(2)%rootmotion) to the new reference frame at T+dt (u(1)%rootmation)
 !!    - the GlbRot, GlbPos, and Glb_crv values are stored as otherstates and updated
 !!    - 
-subroutine UpdateBeamDynGlobalReference(u,p,x,OtherState,ErrStat,ErrMsg)
+subroutine BD_UpdateGlobalRef(u, p, x, OtherState, ErrStat, ErrMsg)
    type(BD_InputType),           intent(in   ) :: u          !< Inputs at utimes
    type(BD_ParameterType),       intent(in   ) :: p          !< Parameters
    type(BD_ContinuousStateType), intent(inout) :: x          !< Input: Continuous states at t;
    type(BD_OtherStateType),      intent(inout) :: OtherState !< Other states: Other states at t;
    integer(IntKi),               intent(  out) :: ErrStat    !< Error status of the operation
    character(*),                 intent(  out) :: ErrMsg     !< Error message if ErrStat /=
+
+   character(*), parameter       :: RoutineName = 'BD_UpdateGlobalRef'
    integer(IntKi)                :: ErrStat2
    character(ErrMsgLen)          :: ErrMsg2    ! Temporary Error message
-   character(*), parameter       :: RoutineName = 'UpdateBeamDynGlobalReference'
    real(R8Ki)                    :: GlbWM_old(3), GlbWM_new(3), GlbWM_diff(3)
    real(R8Ki)                    :: GlbRot_old(3, 3), GlbRot_new(3, 3), GlbRot_diff(3, 3)
    real(R8Ki)                    :: GlbPos_old(3), GlbPos_new(3)
@@ -6867,16 +6862,18 @@ subroutine UpdateBeamDynGlobalReference(u,p,x,OtherState,ErrStat,ErrMsg)
    GlbWM_new  = OtherState%Glb_crv
 
    ! Calculate differences between old and new reference
+!   GlbRot_diff = matmul(transpose(GlbRot_old), GlbRot_new)
    GlbRot_diff = matmul(transpose(GlbRot_new), GlbRot_old)
-   !GlbWM_diff = wm_inv(wm_compose(wm_inv(GlbWM_new), GlbWM_old))
-   call BD_CrvCompose(GlbWM_diff, GlbWM_new, GlbWM_old, FLAG_R1TR2)
+!   GlbWM_diff = wm_from_dcm(GlbRot_diff)
+   call BD_CrvExtractCrv( GlbRot_diff, GlbWM_diff, ErrStat2, ErrMsg2)
+      CALL SetErrStat( ErrStat2, ErrMsg2, ErrStat, ErrMsg, RoutineName )
+      if (ErrStat >= AbortErrLev) return
 
 
    do i = 1, p%elem_total
       do j = 1, p%nodes_per_elem
 
          temp_id = (i - 1)*(p%nodes_per_elem - 1) + j ! The last node of the first element is used as the first node in the second element.
-         temp_id2 = (i - 1)*p%nodes_per_elem + j      ! Index to a node within element i
 
          ! Calculate displacement in terms of new root motion mesh position
          x%q(1:3, temp_id) =  matmul(transpose(GlbRot_new), &
@@ -6896,21 +6893,24 @@ subroutine UpdateBeamDynGlobalReference(u,p,x,OtherState,ErrStat,ErrMsg)
 
          ! Update translational acceleration
          OtherState%acc(1:3, :) = matmul(GlbRot_diff, OtherState%acc(1:3, :))
+         OtherState%xcc(1:3, :) = matmul(GlbRot_diff, OtherState%xcc(1:3, :))
 
          ! Update rotational acceleration
          OtherState%acc(4:6, :) = matmul(GlbRot_diff, OtherState%acc(4:6, :))
+         OtherState%xcc(4:6, :) = matmul(GlbRot_diff, OtherState%xcc(4:6, :))
 
       end do
    end do
 
    ! Root node is always aligned with root motion mesh 
-   x%q(1:3, 1) = 0.0_R8Ki 
-   x%q(4:6, 1) = 0.0_R8Ki
+   x%q(:, 1) = 0.0_R8Ki
    x%dqdt(1:3, 1) = matmul(u%RootMotion%TranslationVel(:, 1), GlbRot_new)
    x%dqdt(4:6, 1) = matmul(u%RootMotion%RotationVel(:, 1), GlbRot_new)
    OtherState%acc(1:3, 1) = matmul(u%RootMotion%TranslationAcc(:, 1), GlbRot_new)
    OtherState%acc(4:6, 1) = matmul(u%RootMotion%RotationAcc(:, 1), GlbRot_new)
 
+!   OtherState%acc(:,:) = 0.0_R8Ki
+    OtherState%xcc(:,:) = 0.0_R8Ki
 
 end subroutine
 
