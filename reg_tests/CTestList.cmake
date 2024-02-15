231afd9ac9e44ca31e0588c56ebea5ece528b391
andrew-platt <andy.platt@nrel.gov>
2024-02-14 17:05:56 -0700

    Add 3 linearization regression tests:
    - 5MW_Land_BD_Linear_Aero: BD + AD15 (based on 5MW_Land_BD_Linear, but with aero)
    - 5MW_Land_Linear_Aero: ED + AD15 (based on 5MW_Land_BD_Linear_Aero, but with ED)
    - 5MW_OC4Semi_MD_Linear: MD equivalent of 5MW_OC4Semi_Linear

--- reg_tests/CTestList.cmake
+++ reg_tests/CTestList.cmake
@@ -314,8 +314,11 @@ of_regression_linear("Fake5MW_AeroLin_B3_UA6"        "openfast;linear;elastodyn"
 of_regression_linear("WP_Stationary_Linear"         "openfast;linear;elastodyn")
 of_regression_linear("Ideal_Beam_Fixed_Free_Linear" "openfast;linear;beamdyn")
 of_regression_linear("Ideal_Beam_Free_Free_Linear"  "openfast;linear;beamdyn")
+of_regression_linear("5MW_Land_Linear_Aero"         "openfast;linear;elastodyn;servodyn;aerodyn")
 of_regression_linear("5MW_Land_BD_Linear"           "openfast;linear;beamdyn;servodyn")
-of_regression_linear("5MW_OC4Semi_Linear"           "openfast;linear;hydrodyn;servodyn")
+of_regression_linear("5MW_Land_BD_Linear_Aero"      "openfast;linear;beamdyn;servodyn;aerodyn")
+of_regression_linear("5MW_OC4Semi_Linear"           "openfast;linear;hydrodyn;servodyn;map")
+of_regression_linear("5MW_OC4Semi_MD_Linear"        "openfast;linear;hydrodyn;servodyn;moordyn")
 of_regression_linear("StC_test_OC4Semi_Linear_Nac"  "openfast;linear;servodyn;stc")
 of_regression_linear("StC_test_OC4Semi_Linear_Tow"  "openfast;linear;servodyn;stc")
 
