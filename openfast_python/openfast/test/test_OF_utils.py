000b6aadcb1cd0820cb5452836e6c3bf159f81b9
Mayank Chetan <mayankchetan@gmail.com>
2024-05-20 16:06:34 -0600

    Pointing test to one r-test case, removing test_data

--- openfast_python/openfast/test/test_OF_utils.py
+++ openfast_python/openfast/test/test_OF_utils.py
@@ -1,6 +1,6 @@
 import unittest
 import os.path as osp
-import subprocess
+import subprocess, sys
 import platform
 
 from openfast.FAST_reader import InputReader_OpenFAST
@@ -8,8 +8,10 @@ from openfast.FAST_writer import InputWriter_OpenFAST
 from openfast.FAST_output_reader import FASTOutputFile
 
 REPOSITORY_ROOT = osp.dirname(osp.dirname(osp.dirname(osp.dirname(__file__))))
-TEST_DATA_DIR = osp.join(REPOSITORY_ROOT, "openfast_python", "openfast", "test", "test_data")
+RTESTS_DIR = osp.join(REPOSITORY_ROOT, "reg_tests","r-test")
+TEST_DATA_DIR = osp.join(RTESTS_DIR, "glue-codes", "openfast", "5MW_Land_DLL_WTurb")
 
+# looking up OS for the correct executable extension
 mactype = platform.system().lower()
 if mactype in ["linux", "linux2", "darwin"]:
     exeExt = ""
@@ -18,19 +20,24 @@ elif mactype in ["win32", "windows", "cygwin"]: #NOTE: platform.system()='Window
 else:
     raise ValueError('Unknown platform type: '+mactype)
 
-of_path = osp.join("../../../build/glue-codes/openfast",f"openfast{exeExt}")
-bin_dir  = osp.dirname(of_path)
+# Path to the OpenFAST executable
+of_path = osp.join(REPOSITORY_ROOT,"build/glue-codes/openfast",f"openfast{exeExt}")
 
 class TestOFutils(unittest.TestCase):
-
     def testOF_Inputs(self):
+
+        # Check if r-tests are available
+        if not osp.isdir(RTESTS_DIR):
+            Exception("The test data directory, {}, does not exist. If you haven't already, run `git submodule update --init --recursive`".format(RTESTS_DIR))
+
+
         # Read input deck
         fast_reader = InputReader_OpenFAST()
         fast_writer = InputWriter_OpenFAST()
-        fast_reader.FAST_InputFile =  'IEA-15-240-RWT-UMaineSemi.fst'   # FAST input file (ext=.fst)
-        fast_reader.FAST_directory = osp.join(TEST_DATA_DIR, 'OpenFAST_models', 'IEA-15-240-RWT', 'IEA-15-240-RWT-UMaineSemi')   # Path to fst directory files
+        fast_reader.FAST_InputFile =  '5MW_Land_DLL_WTurb.fst'   # FAST input file (ext=.fst)
+        fast_reader.FAST_directory = TEST_DATA_DIR   # Path to fst directory files
         fast_writer.FAST_runDirectory = osp.join('temp','OpenFAST')
-        fast_writer.FAST_namingOut    = 'iea15'
+        fast_writer.FAST_namingOut    = 'nrel5mw'
 
         with self.subTest('Reading', i=0):
             try:
@@ -46,6 +53,7 @@ class TestOFutils(unittest.TestCase):
         fst_vt['AeroDyn15', 'TwrAero'] = False
         fst_vt['Fst','CompMooring'] = 0
         fst_vt['Fst','CompServo'] = 0
+        fst_vt['Fst','OutFileFmt'] = 3
         fast_writer.update(fst_update=fst_vt)
 
         with self.subTest('Writing', i=1):
@@ -64,8 +72,8 @@ class TestOFutils(unittest.TestCase):
 
     def testOF_Outputs(self):
         # Read output deck
-        asciiOutput = osp.join('temp','OpenFAST',f'iea15.out')
-        binaryOutput = osp.join('temp','OpenFAST',f'iea15.outb')
+        asciiOutput = osp.join('temp','OpenFAST',f'nrel5mw.out')
+        binaryOutput = osp.join('temp','OpenFAST',f'nrel5mw.outb')
         
         with self.subTest('Reading ASCII output', i=0):
             try:
