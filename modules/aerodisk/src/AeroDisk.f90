4a5660a87cb535030fd4996922e352479b526448
Andy Platt <andrew-platt@users.noreply.github.com>
2024-08-19 12:11:00 -0600

    Update modules/aerodisk/src/AeroDisk.f90
    Co-authored-by: Derek Slaughter <deslaughter@gmail.com>

--- modules/aerodisk/src/AeroDisk.f90
+++ modules/aerodisk/src/AeroDisk.f90
@@ -6,7 +6,7 @@
 !!
 ! ..................................................................................................................................
 !! ## LICENSING
-!! Copyright (C) 2022  National Renewable Energy Laboratory
+!! Copyright (C) 2024  National Renewable Energy Laboratory
 !!
 !!    This file is part of AeroDisk.
 !!
