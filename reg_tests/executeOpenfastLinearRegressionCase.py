6156ee169bd8143717fa98dedab0719c8e8a23fc
Derek Slaughter <deslaughter@gmail.com>
2024-09-13 15:20:06 +0000

    Copy .outb file when doing linear regression test

--- reg_tests/executeOpenfastLinearRegressionCase.py
+++ reg_tests/executeOpenfastLinearRegressionCase.py
@@ -39,7 +39,7 @@ from fast_linearization_file import FASTLinearizationFile
 # from weio.fast_linearization_file import FASTLinearizationFile
 
 ##### Helper functions
-excludeExt=['.out','.outb','.ech','.yaml','.sum','.log','.md']
+excludeExt=['.ech','.yaml','.sum','.log','.md']
 
 def file_line_count(filename):
     file_handle = open(filename, 'r')
@@ -164,7 +164,8 @@ else:
 # 
 # Copying the actual test directory
 # if not os.path.isdir(testBuildDirectory):
-rtl.copyTree(inputsDirectory, testBuildDirectory, excludeExt=excludeExt, renameExtDict={'.lin':'.ref_lin', '.out': '.ref.out', '.outb': '.ref.outb'})
+rtl.copyTree(inputsDirectory, testBuildDirectory, excludeExt=excludeExt, 
+             renameExtDict={'.lin':'.ref_lin', '.out': '.ref.out', '.outb': '.ref.outb'})
 
 ### Run openfast on the test case
 if not noExec:
