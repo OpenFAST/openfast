7257fd4b138a9f6bec409715d61abed5355acee1
Derek Slaughter <deslaughter@gmail.com>
2024-09-13 18:22:14 +0000

    FAST_SolverTC: Disable AD in Option 1

--- modules/openfast-library/src/FAST_SolverTC.f90
+++ modules/openfast-library/src/FAST_SolverTC.f90
@@ -114,7 +114,7 @@ subroutine FAST_SolverInit(p_FAST, p, m, GlueModData, GlueModMaps, Turbine, ErrS
 
    ! Indices of Option 1 modules
    p%iModOpt1 = [pack(modInds, ModIDs == Module_SED), &
-                 pack(modInds, ModIDs == Module_AD .and. p_FAST%MHK /= MHK_None), &
+               ! TODO: pack(modInds, ModIDs == Module_AD .and. p_FAST%MHK /= MHK_None), &
                  pack(modInds, ModIDs == Module_ExtPtfm), &
                  pack(modInds, ModIDs == Module_HD), &
                  pack(modInds, ModIDs == Module_MD), &
@@ -128,7 +128,8 @@ subroutine FAST_SolverInit(p_FAST, p, m, GlueModData, GlueModMaps, Turbine, ErrS
                  pack(modInds, ModIDs == Module_SD), &
                  pack(modInds, ModIDs == Module_IfW), &
                  pack(modInds, ModIDs == Module_SeaSt), &
-                 pack(modInds, ModIDs == Module_AD .and. p_FAST%MHK == MHK_None), &
+               ! TODO: pack(modInds, ModIDs == Module_AD .and. p_FAST%MHK == MHK_None), &
+                 pack(modInds, ModIDs == Module_AD), &
                  pack(modInds, ModIDs == Module_ADsk), &
                  pack(modInds, ModIDs == Module_ExtLd), &
                  pack(modInds, ModIDs == Module_FEAM), &
