e0b8d01a0a82bfa62531949a395d6f45ab768b5f
Derek Slaughter <deslaughter@gmail.com>
2025-02-13 19:05:58 +0000

    Debugging version info on CI

--- .github/scripts/build_windows_executables.bat
+++ .github/scripts/build_windows_executables.bat
@@ -5,37 +5,39 @@ for /f "tokens=* usebackq" %%f in (`dir /b "C:\Program Files (x86)\Intel\oneAPI\
 
 @REM Make git ignore changes to Types files (line endings) and 
 @REM vs-build directory so it doesn't append -dirty to version
-ECHO *_Types.f90>>".gitignore"
-ECHO vs-build>>".gitignore"
+ECHO *_Types.f90>>".git/info/exclude"
+ECHO vs-build>>".git/info/exclude"
 
 echo on
 
 @REM Build all solutions
-devenv vs-build/AeroDisk/AeroDisk_Driver.sln /Build "Release|x64"
-devenv vs-build/AeroDyn/AeroDyn_Driver.sln /Build "Release|x64"
-devenv vs-build/AeroDyn/AeroDyn_Driver.sln /Build "Release_OpenMP|x64"
-devenv vs-build/AeroDyn_Inflow_c_binding/AeroDyn_Inflow_c_binding.sln /Build "Release|x64"
-devenv vs-build/AeroDyn_Inflow_c_binding/AeroDyn_Inflow_c_binding.sln /Build "Release_OpenMP|x64"
-devenv vs-build/BeamDyn/BeamDyn-w-registry.sln /Build "Release|x64"
-devenv vs-build/Discon/Discon.sln /Build "Release|x64" 
-devenv vs-build/FAST-farm/FAST-Farm.sln /Build "Release|x64"
-devenv vs-build/FAST-farm/FAST-Farm.sln /Build "Release_OpenMP|x64"
-devenv vs-build/HydroDyn/HydroDynDriver.sln /Build "Release|x64"
-devenv vs-build/HydroDyn_c_binding/HydroDyn_c_binding.sln /Build "Release|x64"
-devenv vs-build/InflowWind_c_binding/InflowWind_c_binding.sln /Build "Release|x64"
-devenv vs-build/InflowWind/InflowWind_driver.sln /Build "Release|x64"
-devenv vs-build/InflowWind/InflowWind_driver.sln /Build "Release_OpenMP|x64"
-devenv vs-build/MoorDyn/MoorDynDriver.sln /Build "Release|x64"
-devenv vs-build/MoorDyn_c_binding/MoorDyn_c_binding.sln /Build "Release|x64"
-devenv vs-build/FAST/FAST.sln /Build "Release|x64"
-devenv vs-build/FAST/FAST.sln /Build "Release_Matlab|x64"
-devenv vs-build/SeaState/SeaStateDriver.sln /Build "Release|x64"
-devenv vs-build/SimpleElastoDyn/SimpleElastoDyn_Driver.sln /Build "Release|x64"
-devenv vs-build/SubDyn/SubDyn.sln /Build "Release|x64"
+@REM devenv vs-build/AeroDisk/AeroDisk_Driver.sln /Build "Release|x64"
+@REM devenv vs-build/AeroDyn/AeroDyn_Driver.sln /Build "Release|x64"
+@REM devenv vs-build/AeroDyn/AeroDyn_Driver.sln /Build "Release_OpenMP|x64"
+@REM devenv vs-build/AeroDyn_Inflow_c_binding/AeroDyn_Inflow_c_binding.sln /Build "Release|x64"
+@REM devenv vs-build/AeroDyn_Inflow_c_binding/AeroDyn_Inflow_c_binding.sln /Build "Release_OpenMP|x64"
+@REM devenv vs-build/BeamDyn/BeamDyn-w-registry.sln /Build "Release|x64"
+@REM devenv vs-build/Discon/Discon.sln /Build "Release|x64" 
+@REM devenv vs-build/FAST-farm/FAST-Farm.sln /Build "Release|x64"
+@REM devenv vs-build/FAST-farm/FAST-Farm.sln /Build "Release_OpenMP|x64"
+@REM devenv vs-build/HydroDyn/HydroDynDriver.sln /Build "Release|x64"
+@REM devenv vs-build/HydroDyn_c_binding/HydroDyn_c_binding.sln /Build "Release|x64"
+@REM devenv vs-build/InflowWind_c_binding/InflowWind_c_binding.sln /Build "Release|x64"
+@REM devenv vs-build/InflowWind/InflowWind_driver.sln /Build "Release|x64"
+@REM devenv vs-build/InflowWind/InflowWind_driver.sln /Build "Release_OpenMP|x64"
+@REM devenv vs-build/MoorDyn/MoorDynDriver.sln /Build "Release|x64"
+@REM devenv vs-build/MoorDyn_c_binding/MoorDyn_c_binding.sln /Build "Release|x64"
+@REM devenv vs-build/FAST/FAST.sln /Build "Release|x64"
+@REM devenv vs-build/SeaState/SeaStateDriver.sln /Build "Release|x64"
+@REM devenv vs-build/SimpleElastoDyn/SimpleElastoDyn_Driver.sln /Build "Release|x64"
+@REM devenv vs-build/SubDyn/SubDyn.sln /Build "Release|x64"
 devenv vs-build/TurbSim/TurbSim.vfproj /Build "Release|x64"
-devenv vs-build/UnsteadyAero/UnsteadyAero.sln /Build "Release|x64"
+@REM devenv vs-build/UnsteadyAero/UnsteadyAero.sln /Build "Release|x64"
+
+@REM Build MATLAB solution last
+@REM devenv vs-build/FAST/FAST.sln /Build "Release_Matlab|x64"
 
 @REM Copy controllers to bin directory
-xcopy .\reg_tests\r-test\glue-codes\openfast\5MW_Baseline\ServoData\*.dll .\build\bin\ /y
+@REM xcopy .\reg_tests\r-test\glue-codes\openfast\5MW_Baseline\ServoData\*.dll .\build\bin\ /y
 
 exit /b %ERRORLEVEL%
