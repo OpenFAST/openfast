25e27179f262c8ef7f2b1c715d9994e8d278f6b7
Rafael M Mudafort <rafmudaf@gmail.com>
2025-04-21 23:42:07 -0500

    Hard code IfW OutputAccel flag

--- modules/aerodyn/src/AeroDyn_Inflow.f90
+++ modules/aerodyn/src/AeroDyn_Inflow.f90
@@ -388,6 +388,7 @@ subroutine ADI_InitInflowWind(Root, i_IW, u_AD, o_AD, IW, dt, InitOutData, errSt
       endif
       InitInData%RootName         = trim(Root)//'.IfW'
       InitInData%MHK              = i_IW%MHK
+      InitInData%OutputAccel      = .TRUE.            ! TODO: Set via input file or c driver flag
       ! OLAF might be used in AD, in which case we need to allow out of bounds for some calcs. To do that
       ! the average values for the entire wind profile must be calculated and stored (we don't know if OLAF
       ! is used until after AD_Init below).
