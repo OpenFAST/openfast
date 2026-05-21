d6198e08826d49d6325df94b2b9c7416d8ba66a7
andrew-platt <andy.platt@nrel.gov>
2026-05-21 15:29:45 -0600

    FF: update MaxPlanes calculation for cartesian/curl

--- glue-codes/fast-farm/src/FAST_Farm_Subs.f90
+++ glue-codes/fast-farm/src/FAST_Farm_Subs.f90
@@ -211,7 +211,11 @@ SUBROUTINE Farm_Initialize( farm, InputFile, ErrStat, ErrMsg )
    call AllocAry( farm%p%MaxNumPlanes, farm%p%NumTurbines, 'farm%p%MaxNumPlanes', ErrStat2, ErrMsg2);  CALL SetErrStat( ErrStat2, ErrMsg2, ErrStat, ErrMsg, RoutineName); if (Failed()) return
    do i=1,farm%p%NumTurbines
       ! Eventually, we will have different settings for different rotors
-      farm%p%MaxNumPlanes(i) = ceiling( 15.0 * ( WD_InitInput%InputFileData%NumDFull + WD_InitInput%InputFileData%NumDBuff ) / AWAE_InitInput%InputFileData%C_Meander )
+      if (farm%WD(nt)%p%Mod_Wake == Mod_Wake_Polar) then
+         farm%p%MaxNumPlanes(i) = ceiling( 15.0 * ( WD_InitInput%InputFileData%NumDFull + WD_InitInput%InputFileData%NumDBuff ) / AWAE_InitInput%InputFileData%C_Meander )
+      else
+         farm%p%MaxNumPlanes(i) = ceiling( 45.0 * ( WD_InitInput%InputFileData%NumDFull + WD_InitInput%InputFileData%NumDBuff ) / AWAE_InitInput%InputFileData%C_Meander )
+      endif
       farm%p%MaxNumPlanes(i) = max( 2, min( farm%p%MaxNumPlanes(i) , farm%p%n_TMax + 2 ) )
    end do
 
