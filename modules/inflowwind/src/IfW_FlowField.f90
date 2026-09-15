d937bfb73e25114496151cca8a744b204f53bf2d
Roger Bergua <roger.bergua@nrel.gov>
2026-09-15 11:17:02 -0600

    GetBoundsT: lower bound with grid tolerance

--- modules/inflowwind/src/IfW_FlowField.f90
+++ modules/inflowwind/src/IfW_FlowField.f90
@@ -1289,6 +1289,11 @@ contains
       ! Adjust indices and interpolant
       if (IT_LO >= 1 .and. IT_HI <= G3D%NSteps) then
          ! Point is within grid
+      else if (IT_LO == 0 .and. T_GRID >= -GridTol) then
+	      ! Within tolerance of the first time step
+	      IT_LO = 1
+	      IT_HI = 1
+         DT = -1.0_ReKi
       else if (IT_LO == G3D%NSteps) then
          if (G3D%Periodic) then
             ! Time wraps back to beginning
